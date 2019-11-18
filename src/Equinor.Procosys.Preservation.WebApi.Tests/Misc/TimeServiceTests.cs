5d52ef0c6a819a5426d988b652b03d520ed175f0
Henning Bjørgo <HHBJ@equinor.com>
2019-11-18 12:50:58 +0100

    Added test projects

---
+++ src/Equinor.Procosys.Preservation.WebApi.Tests/Misc/TimeServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Equinor.Procosys.Preservation.WebApi.Misc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Equinor.Procosys.Preservation.WebApi.Tests.Misc
+{
+    [TestClass]
+    public class TimeServiceTests
+    {
+        [TestMethod]
+        public void ReturnsUtcTime()
+        {
+            DateTime time = (new TimeService()).GetCurrentTimeUTC();
+            Assert.AreEqual(DateTimeKind.Utc, time.Kind);
+        }
+    }
+}
