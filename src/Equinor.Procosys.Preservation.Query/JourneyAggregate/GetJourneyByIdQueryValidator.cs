1495b577dc9d8dc014e738a9071626e5392f075b
Henning Bjørgo <HHBJ@equinor.com>
2019-12-17 11:41:29 +0100

    Code cleanup

--- src/Equinor.Procosys.Preservation.Query/JourneyAggregate/GetJourneyByIdQueryValidator.cs
+++ src/Equinor.Procosys.Preservation.Query/JourneyAggregate/GetJourneyByIdQueryValidator.cs
@@ -4,6 +4,8 @@ namespace Equinor.Procosys.Preservation.Query.JourneyAggregate
 {
     public class GetJourneyByIdQueryValidator : AbstractValidator<GetJourneyByIdQuery>
     {
-        public GetJourneyByIdQueryValidator() => RuleFor(x => x.Id).GreaterThan(0);
+        public GetJourneyByIdQueryValidator()
+        {
+        }
     }
 }
