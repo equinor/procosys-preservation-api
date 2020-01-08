4fd83ed71390d3b4a423b648e8a32dfd42c3698b
Henning Bjørgo <HHBJ@equinor.com>
2020-01-08 10:06:54 +0100

    Fixed tests.

--- src/tests/Equinor.Procosys.Preservation.Domain.Tests/AggregateModels/TagAggregate/TagTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Domain.Tests/AggregateModels/TagAggregate/TagTests.cs
@@ -9,29 +9,34 @@ namespace Equinor.Procosys.Preservation.Domain.Tests.AggregateModels.TagAggregat
     [TestClass]
     public class TagTests
     {
-        /*
         [TestMethod]
         public void ConstructorSetsPropertiesTest()
         {
             var step = new Mock<Step>();
             step.SetupGet(x => x.Id).Returns(3);
-            var dut = new Tag("SchemaA", "TagNoA", "ProjectNoA", step.Object, "DescA");
+            var dut = new Tag("SchemaA", "TagNumberA", "ProjectNumberA", "DescriptionA", "AreaCodeA", "CalloffA", "DisciplineA", "McPkgA", "PurchaseOrderA", "TagFunctionCodeA", step.Object);
 
             Assert.AreEqual("SchemaA", dut.Schema);
-            Assert.AreEqual("TagNoA", dut.TagNo);
-            Assert.AreEqual("ProjectNoA", dut.ProjectNo);
+            Assert.AreEqual("TagNumberA", dut.TagNumber);
+            Assert.AreEqual("ProjectNumberA", dut.ProjectNumber);
+            Assert.AreEqual("DescriptionA", dut.Description);
+            Assert.AreEqual("AreaCodeA", dut.AreaCode);
+            Assert.AreEqual("CalloffA", dut.CalloffNumber);
+            Assert.AreEqual("DisciplineA", dut.DisciplineCode);
+            Assert.AreEqual("McPkgA", dut.McPkcNumber);
+            Assert.AreEqual("PurchaseOrderA", dut.PurchaseOrderNumber);
+            Assert.AreEqual("TagFunctionCodeA", dut.TagFunctionCode);
             Assert.AreEqual(step.Object.Id, dut.StepId);
-            Assert.AreEqual("DescA", dut.Description);
         }
 
         [TestMethod]
-        public void ConstructorThrowsExceptionIsStepIsNotSetTest() => Assert.ThrowsException<ArgumentNullException>(() => new Tag("", "", "", null, ""));
+        public void ConstructorThrowsExceptionIsStepIsNotSetTest() => Assert.ThrowsException<ArgumentNullException>(() => new Tag("", "", "", "", "", "", "", "", "", "", null));
 
         [TestMethod]
         public void SetStepSetsStepIdTest()
         {
             var step = new Mock<Step>();
-            var dut = new Tag("", "", "", step.Object, "");
+            var dut = new Tag("", "", "", "", "", "", "", "", "", "", step.Object);
 
             var newStep = new Mock<Step>();
             newStep.SetupGet(x => x.Id).Returns(3);
@@ -44,10 +49,9 @@ namespace Equinor.Procosys.Preservation.Domain.Tests.AggregateModels.TagAggregat
         public void SetStepThrowsExceptionIfStepIsNullTest()
         {
             var step = new Mock<Step>();
-            var dut = new Tag("", "", "", step.Object, "");
+            var dut = new Tag("", "", "", "", "", "", "", "", "", "", step.Object);
 
             Assert.ThrowsException<ArgumentNullException>(() => dut.SetStep(null));
         }
-        */
     }
 }
