89abcb0f467a475b2c6d65217b13132b6b97eb8d
Henning Bjørgo <HHBJ@equinor.com>
2020-01-14 09:17:19 +0100

    Expr. bodies.

--- src/tests/Equinor.Procosys.Preservation.MainApi.Tests/Client/HttpHelper.cs
+++ src/tests/Equinor.Procosys.Preservation.MainApi.Tests/Client/HttpHelper.cs
@@ -12,15 +12,11 @@ namespace Equinor.Procosys.Preservation.MainApi.Tests.Client
     {
         private HttpResponseMessage _fakeResponse;
 
-        public FakeHttpMessageHandler(HttpResponseMessage responseMessage)
-        {
+        public FakeHttpMessageHandler(HttpResponseMessage responseMessage) =>
             _fakeResponse = responseMessage;
-        }
 
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            return await Task.FromResult(_fakeResponse);
-        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) =>
+            await Task.FromResult(_fakeResponse);
     }
 
     internal static class HttpHelper
