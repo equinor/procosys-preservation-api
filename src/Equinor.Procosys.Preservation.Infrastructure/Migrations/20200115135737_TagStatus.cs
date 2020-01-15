896b94b71628d33607a7e76eec8f09ff8fade1f4
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-15 14:59:49 +0100

    Migration
    (cherry picked from commit ea13291141d8c07c861f7d5a871284e61ec2eaab)

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200115135737_TagStatus.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class TagStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Tags",
+                nullable: false,
+                defaultValue: "NotStarted");
+
+            migrationBuilder.CreateCheckConstraint(
+                name: "constraint_tag_check_valid_status",
+                table: "Tags",
+                sql: "Status in ('NotStarted','Active','Completed')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_tag_check_valid_status",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tags");
+        }
+    }
+}
