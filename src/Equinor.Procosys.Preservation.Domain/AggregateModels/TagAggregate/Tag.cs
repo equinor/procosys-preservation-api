0e4872f8a7c1be6f0760dc9575c873c1a9f23e82
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-15 08:53:02 +0100

    Props after ctors for Tag
    (cherry picked from commit 651ae91e586ace6e9ec002c90360f79e9a0bc740)

--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/TagAggregate/Tag.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/TagAggregate/Tag.cs
@@ -12,19 +12,6 @@ namespace Equinor.Procosys.Preservation.Domain.AggregateModels.TagAggregate
         public const int TagNoLengthMax = 255;
         public const int ProjectNumberLengthMax = 255;
 
-        public string AreaCode { get; private set; }
-        public string CalloffNumber { get; private set; }
-        public string CommPkgNumber { get; private set; }
-        public string DisciplineCode { get; private set; }
-        public bool IsAreaTag { get; private set; }
-        public string McPkcNumber { get; private set; }
-        public string ProjectNumber { get; private set; }
-        public string PurchaseOrderNumber { get; private set; }
-        public int StepId { get; private set; }
-        public string TagFunctionCode { get; private set; }
-        public string TagNo { get; private set; }
-        public IReadOnlyCollection<Requirement> Requirements => _requirements.AsReadOnly();
-
         protected Tag()
             : base(null)
         {
@@ -71,6 +58,19 @@ namespace Equinor.Procosys.Preservation.Domain.AggregateModels.TagAggregate
             StepId = step.Id;
             _requirements.AddRange(reqList);
         }
+        
+        public string AreaCode { get; private set; }
+        public string CalloffNumber { get; private set; }
+        public string CommPkgNumber { get; private set; }
+        public string DisciplineCode { get; private set; }
+        public bool IsAreaTag { get; private set; }
+        public string McPkcNumber { get; private set; }
+        public string ProjectNumber { get; private set; }
+        public string PurchaseOrderNumber { get; private set; }
+        public int StepId { get; private set; }
+        public string TagFunctionCode { get; private set; }
+        public string TagNo { get; private set; }
+        public IReadOnlyCollection<Requirement> Requirements => _requirements.AsReadOnly();
 
         public void SetStep(Step step)
         {
