9e3094ea8ab543f250103cccd67a301e420355db
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-21 12:38:14 +0100

    More tests

--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/TagAggregate/Tag.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/TagAggregate/Tag.cs
@@ -78,7 +78,7 @@ namespace Equinor.Procosys.Preservation.Domain.AggregateModels.TagAggregate
         public void Void() => IsVoided = true;
         public void UnVoid() => IsVoided = false;
 
-        public void SetStep(Step step)
+        public virtual void SetStep(Step step)
         {
             if (step == null)
             {
--- src/tests/Equinor.Procosys.Preservation.Command.Tests/TagCommands/SetStep/SetStepCommandHandlerTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/TagCommands/SetStep/SetStepCommandHandlerTests.cs
@@ -1,7 +1,70 @@
-﻿namespace Equinor.Procosys.Preservation.Command.Tests.TagCommands.SetStep
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.TagCommands.SetStep;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.TagAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.TagCommands.SetStep
 {
+    [TestClass]
     public class SetStepCommandHandlerTests : CommandHandlerTestsBase
     {
-        // todo Henning
+        private const int StepId = 11;
+        private const int TagId = 99;
+
+        private Mock<Tag> _tagMock;
+        private Mock<Step> _stepMock;
+        private Mock<IJourneyRepository> _journeyRepositoryMock;
+        private Mock<ITagRepository> _tagRepositoryMock;
+        private SetStepCommand _command;
+        private SetStepCommandHandler _dut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Arrange
+            _tagMock = new Mock<Tag>();
+            _tagMock.SetupGet(x => x.Id).Returns(TagId);
+            _stepMock = new Mock<Step>();
+            _stepMock.SetupGet(x => x.Id).Returns(StepId);
+
+            _tagRepositoryMock = new Mock<ITagRepository>();
+            _tagRepositoryMock
+                .Setup(x => x.GetByIdAsync(TagId))
+                .Returns(Task.FromResult(_tagMock.Object));
+
+            _journeyRepositoryMock = new Mock<IJourneyRepository>();
+            _journeyRepositoryMock
+                .Setup(x => x.GetStepByStepIdAsync(StepId))
+                .Returns(Task.FromResult(_stepMock.Object));
+
+            _command = new SetStepCommand(TagId, StepId);
+            
+            _dut = new SetStepCommandHandler(
+                _tagRepositoryMock.Object,
+                _journeyRepositoryMock.Object,
+                _unitOfWorkMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandlingSetStepCommand_ShouldAddTagToRepository()
+        {
+            // Act
+            var result = await _dut.Handle(_command, default);
+
+            // Assert
+            _tagMock.Verify(t => t.SetStep(_stepMock.Object), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task HandlingSetStepCommand_ShouldSave()
+        {
+            // Act
+            await _dut.Handle(_command, default);
+            
+            // Assert
+            _unitOfWorkMock.Verify(u => u.SaveChangesAsync(default), Times.Once);
+        }
     }
 }
