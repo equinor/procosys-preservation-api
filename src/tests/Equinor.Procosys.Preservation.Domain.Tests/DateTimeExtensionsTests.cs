ff25b3c417944139a8713d281db8c11f372e9577
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-21 09:18:54 +0100

    DateTimeExtensions GetWeeksUntilDate with unit tests To be used in Requirement dto

--- src/tests/Equinor.Procosys.Preservation.Domain.Tests/DateTimeExtensionsTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Domain.Tests/DateTimeExtensionsTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Equinor.Procosys.Preservation.Domain.Tests
 {
+    [TestClass]
     public class DateTimeExtensionsTests
     {
         [TestMethod]
@@ -14,11 +15,76 @@ namespace Equinor.Procosys.Preservation.Domain.Tests
 
             Assert.AreEqual(2020, dt.Year);
             Assert.AreEqual(1, dt.Month);
-            Assert.AreEqual(21, dt.Date);
+            Assert.AreEqual(21, dt.Day);
             Assert.AreEqual(2, dt.Hour);
             Assert.AreEqual(3, dt.Minute);
             Assert.AreEqual(4, dt.Second);
             Assert.AreEqual(DateTimeKind.Utc, dt.Kind);
+        }
+
+        [TestMethod]
+        public void GetWeeksUntilDate_ADay_ShouldGetZeroWeeks()
+        {
+            var fromTime = new DateTime(2020, 1, 1);
+            var toTime = fromTime.AddDays(1);
+
+            var weeks = fromTime.GetWeeksUntilDate(toTime);
+
+            Assert.AreEqual(0, weeks);
+        }
+
+        [TestMethod]
+        public void GetWeeksUntilDate_OnSameTime_ShouldGetZeroWeeks()
+        {
+            var fromTime = new DateTime(2020, 1, 1);
+
+            var weeks = fromTime.GetWeeksUntilDate(fromTime);
+
+            Assert.AreEqual(0, weeks);
+        }
+
+        [TestMethod]
+        public void GetWeeksUntilDate_AWeek_ShouldGetOneWeek()
+        {
+            var fromTime = new DateTime(2020, 1, 1);
+            var toTime = fromTime.AddDays(7);
+
+            var weeks = fromTime.GetWeeksUntilDate(toTime);
+
+            Assert.AreEqual(1, weeks);
+        }
+
+        [TestMethod]
+        public void GetWeeksUntilDate_OneMinueBeforeAWeek_ShouldGetZeroWeeks()
+        {
+            var fromTime = new DateTime(2020, 1, 1);
+            var toTime = fromTime.AddDays(7).AddMinutes(-1);
+
+            var weeks = fromTime.GetWeeksUntilDate(toTime);
+
+            Assert.AreEqual(0, weeks);
+        }
+
+        [TestMethod]
+        public void GetWeeksUntilDate_AYear_ShouldGet52Weeks()
+        {
+            var fromTime = new DateTime(2019, 1, 1);
+            var toTime = fromTime.AddYears(1);
+
+            var weeks = fromTime.GetWeeksUntilDate(toTime);
+
+            Assert.AreEqual(52, weeks);
+        }
+
+        [TestMethod]
+        public void GetWeeksUntilDate_AYearBackward_ShouldGetMinus52Weeks()
+        {
+            var fromTime = new DateTime(2020, 1, 1);
+            var toTime = fromTime.AddYears(-1);
+
+            var weeks = fromTime.GetWeeksUntilDate(toTime);
+
+            Assert.AreEqual(-52, weeks);
         }
     }
 }
