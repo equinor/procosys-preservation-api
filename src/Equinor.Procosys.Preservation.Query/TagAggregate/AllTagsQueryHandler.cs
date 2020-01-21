2bed3cbd3ca85b1a70e1c71396e32a9e253e5841
Henning Bjørgo <HHBJ@equinor.com>
2020-01-21 10:46:07 +0100

    Returning more properties on Tag. Added Seeding of test data.

--- src/Equinor.Procosys.Preservation.Query/TagAggregate/AllTagsQueryHandler.cs
+++ src/Equinor.Procosys.Preservation.Query/TagAggregate/AllTagsQueryHandler.cs
@@ -19,14 +19,24 @@ namespace Equinor.Procosys.Preservation.Query.TagAggregate
             var tags = await _tagRepository.GetAllAsync();
             return new SuccessResult<IEnumerable<TagDto>>(tags.Select(tag =>
                 new TagDto(tag.Id,
-                    tag.TagNo,
-                    tag.StepId,
-                    tag.Status,
-                    tag.Requirements.Select(r =>
-                        new RequirementDto(r.Id, 
-                            r.RequirementDefinitionId, 
-                            r.IsVoided, 
-                            r.IntervalWeeks)))));
+                tag.AreaCode,
+                tag.CalloffNumber,
+                tag.CommPkgNumber,
+                tag.DisciplineCode,
+                tag.IsAreaTag,
+                tag.IsVoided,
+                tag.McPkcNumber,
+                tag.ProjectNumber,
+                tag.PurchaseOrderNumber,
+                tag.Requirements.Select(r =>
+                        new RequirementDto(r.Id,
+                            r.RequirementDefinitionId,
+                            r.IsVoided,
+                            r.IntervalWeeks)),
+                tag.Status,
+                tag.StepId,
+                tag.TagFunctionCode,
+                tag.TagNo)));
         }
     }
 }
--- src/Equinor.Procosys.Preservation.Query/TagAggregate/TagDto.cs
+++ src/Equinor.Procosys.Preservation.Query/TagAggregate/TagDto.cs
@@ -6,23 +6,54 @@ namespace Equinor.Procosys.Preservation.Query.TagAggregate
 {
     public class TagDto
     {
-        public TagDto(int id, string tagNo, int stepId, PreservationStatus status, IEnumerable<RequirementDto> requirements)
+        public TagDto(
+            int id,
+            string areaCode,
+            string calloffNumber,
+            string commPkgNumber,
+            string disciplineCode,
+            bool isAreaTag,
+            bool isVoided,
+            string mcPkgNumber,
+            string projectNumber,
+            string purchaseOrderNumber,
+            IEnumerable<RequirementDto> requirements,
+            PreservationStatus status,
+            int stepId,
+            string tagFunctionCode,
+            string tagNo)
         {
-            if (requirements == null)
-            {
-                throw new ArgumentNullException(nameof(requirements));
-            }
-            
             Id = id;
+            AreaCode = areaCode;
+            CalloffNumber = calloffNumber;
+            CommPkgNumber = commPkgNumber;
+            DisciplineCode = disciplineCode;
+            IsAreaTag = isAreaTag;
+            IsVoided = isVoided;
+            McPkgNumber = mcPkgNumber;
+            ProjectNumber = projectNumber;
+            PurchaseOrderNumber = purchaseOrderNumber;
             TagNo = tagNo;
             Status = status;
             StepId = stepId;
-            Requirements = requirements;
+            TagFunctionCode = tagFunctionCode;
+            Requirements = requirements ?? throw new ArgumentNullException(nameof(requirements));
         }
+
+        public string AreaCode { get; set; }
+        public string CalloffNumber { get; set; }
+        public string CommPkgNumber { get; set; }
+        public string DisciplineCode { get; set; }
         public int Id { get; }
-        public string TagNo { get; }
-        public int StepId { get; }
-        public PreservationStatus Status { get; }
+        public bool IsAreaTag { get; set; }
+        public bool IsVoided { get; set; }
+        public string McPkgNumber { get; set; }
+        public string ProjectNumber { get; set; }
+        public string PurchaseOrderNumber { get; set; }
         public IEnumerable<RequirementDto> Requirements { get; }
+        public PreservationStatus Status { get; }
+        public int StepId { get; }
+        public string TagFunctionCode { get; set; }
+        public string TagNo { get; }
     }
 }
