04304391617043e5df311b840f5ce6700a27ac65
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-01-22 08:58:55 +0100

    Unit test on all command handlers (#39)
    * Unit tests
    * CommandHandlerTestsBase and more unit tests

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/JourneyCommands/CreateJourney/CreateJourneyCommandHandlerTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/JourneyCommands/CreateJourney/CreateJourneyCommandHandlerTests.cs
@@ -1,7 +1,62 @@
-﻿namespace Equinor.Procosys.Preservation.Command.Tests.JourneyCommands.CreateJourney
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.JourneyCommands.CreateJourney;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.JourneyCommands.CreateJourney
 {
-    public class CreateJourneyCommandHandlerTests
+    [TestClass]
+    public class CreateJourneyCommandHandlerTests : CommandHandlerTestsBase
     {
-        // todo Henning
+        private const string TestJourney = "TestJourney";
+        private Journey _journeyAdded;
+        private Mock<IJourneyRepository> _journeyRepositoryMock;
+        private CreateJourneyCommand _command;
+        private CreateJourneyCommandHandler _dut;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            // Arrange
+            _journeyRepositoryMock = new Mock<IJourneyRepository>();
+            _journeyRepositoryMock
+                .Setup(repo => repo.Add(It.IsAny<Journey>()))
+                .Callback<Journey>(journey =>
+                {
+                    _journeyAdded = journey;
+                });
+
+            _command = new CreateJourneyCommand(TestJourney);
+
+            _dut = new CreateJourneyCommandHandler(
+                _journeyRepositoryMock.Object,
+                UnitOfWorkMock.Object,
+                PlantProviderMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandlingCreateJourneyCommand_ShouldAddJourneyToRepository()
+        {
+            // Act
+            var result = await _dut.Handle(_command, default);
+            
+            // Assert
+            Assert.AreEqual(0, result.Errors.Count);
+            Assert.AreEqual(0, result.Data);
+            Assert.AreEqual(0, _journeyAdded.Id);
+            Assert.AreEqual(TestJourney, _journeyAdded.Title);
+            Assert.AreEqual(TestPlant, _journeyAdded.Schema);
+        }
+
+        [TestMethod]
+        public async Task HandlingCreateJourneyCommand_ShouldSave()
+        {
+            // Act
+            await _dut.Handle(_command, default);
+            
+            // Assert
+            UnitOfWorkMock.Verify(u => u.SaveChangesAsync(default), Times.Once);
+        }
     }
 }
--- src/tests/Equinor.Procosys.Preservation.Command.Tests/JourneyCommands/CreateStep/CreateStepCommandHandlerTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/JourneyCommands/CreateStep/CreateStepCommandHandlerTests.cs
@@ -1,7 +1,89 @@
-﻿namespace Equinor.Procosys.Preservation.Command.Tests.JourneyCommands.CreateStep
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.JourneyCommands.CreateStep;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ModeAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ResponsibleAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.JourneyCommands.CreateStep
 {
-    public class CreateStepCommandHandlerTests
+    [TestClass]
+    public class CreateStepCommandHandlerTests : CommandHandlerTestsBase
     {
-        // todo Henning
+        private const int JourneyId = 1;
+        private const int ModeId = 2;
+        private const int ResponsibleId = 3;
+        private Step _stepAdded;
+        private Mock<IJourneyRepository> _journeyRepositoryMock;
+        private Mock<Journey> _journeyMock;
+        private Mock<Mode> _modeMock;
+        private Mock<Responsible> _responsibleMock;
+        private Mock<IModeRepository> _modeRepositoryMock;
+        private Mock<IResponsibleRepository> _responsibleRepositoryMock;
+        private CreateStepCommand _command;
+        private CreateStepCommandHandler _dut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Arrange
+            _journeyRepositoryMock = new Mock<IJourneyRepository>();
+            _journeyMock = new Mock<Journey>();
+            _journeyRepositoryMock
+                .Setup(r => r.GetByIdAsync(JourneyId))
+                .Returns(Task.FromResult(_journeyMock.Object));
+            _journeyMock
+                .Setup(journey => journey.AddStep(It.IsAny<Step>()))
+                .Callback<Step>(step =>
+                {
+                    _stepAdded = step;
+                });
+
+            _modeRepositoryMock = new Mock<IModeRepository>();
+            _modeMock = new Mock<Mode>();
+            _modeMock.SetupGet(x => x.Id).Returns(ModeId);
+            _modeRepositoryMock
+                .Setup(r => r.GetByIdAsync(ModeId))
+                .Returns(Task.FromResult(_modeMock.Object));
+
+            _responsibleRepositoryMock = new Mock<IResponsibleRepository>();
+            _responsibleMock = new Mock<Responsible>();
+            _responsibleMock.SetupGet(x => x.Id).Returns(ResponsibleId);
+            _responsibleRepositoryMock
+                .Setup(r => r.GetByIdAsync(ResponsibleId))
+                .Returns(Task.FromResult(_responsibleMock.Object));
+
+            _command = new CreateStepCommand(JourneyId, ModeId, ResponsibleId);
+
+            _dut = new CreateStepCommandHandler(_journeyRepositoryMock.Object,
+                _modeRepositoryMock.Object,
+                _responsibleRepositoryMock.Object,
+                UnitOfWorkMock.Object,
+                PlantProviderMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandlingCreateStepCommand_ShouldAddStepToJourney()
+        {
+            // Act
+            var result = await _dut.Handle(_command, default);
+            
+            // Assert
+            _journeyMock.Verify(u => u.AddStep(It.IsAny<Step>()), Times.Once);
+            Assert.AreEqual(ModeId, _stepAdded.ModeId);
+            Assert.AreEqual(ResponsibleId, _stepAdded.ResponsibleId);
+        }
+
+        [TestMethod]
+        public async Task HandlingCreateStepCommand_ShouldSave()
+        {
+            // Act
+            await _dut.Handle(_command, default);
+            
+            // Assert
+            UnitOfWorkMock.Verify(u => u.SaveChangesAsync(default), Times.Once);
+        }
     }
 }
