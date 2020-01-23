26a18a0b8b621688a34515bcc7c1197f6d5d3541
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-23 13:54:57 +0100

    RequirementTypeRepositoryTests

---
+++ src/tests/Equinor.Procosys.Preservation.Infrastructure.Tests/Repositories/RequirementTypeTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Equinor.Procosys.Preservation.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockQueryable.Moq;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Tests.Repositories
+{
+    [TestClass]
+    public class RequirementTypeRepositoryTests : RepositoryTestBase
+    {
+        private const int ReqDefId = 8;
+        private List<RequirementType> _requirementTypes;
+        private Mock<DbSet<RequirementType>> _dbSetMock;
+
+        private RequirementTypeRepository _dut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            
+            var requirementType = new RequirementType(TestPlant, "C1", "T1", 0);
+            var rdMock = new Mock<RequirementDefinition>();
+            rdMock.SetupGet(r => r.Id).Returns(ReqDefId);
+            requirementType.AddRequirementDefinition(rdMock.Object);
+            requirementType.AddRequirementDefinition(new Mock<RequirementDefinition>().Object);
+            requirementType.AddRequirementDefinition(new Mock<RequirementDefinition>().Object);
+            
+            _requirementTypes = new List<RequirementType>
+            {
+                requirementType,
+                new RequirementType(TestPlant, "C2", "T2", 0),
+                new RequirementType(TestPlant, "C3", "T3", 0)
+            };
+            
+            _dbSetMock = _requirementTypes.AsQueryable().BuildMockDbSet();
+
+            ContextHelper
+                .ContextMock
+                .Setup(x => x.RequirementTypes)
+                .Returns(_dbSetMock.Object);
+
+            _dut = new RequirementTypeRepository(ContextHelper.ContextMock.Object);
+        }
+
+        [TestMethod]
+        public async Task GetRequirementDefinitionById_KnownReqDefId_ReturnReqDef()
+        {
+            var result = await _dut.GetRequirementDefinitionByIdAsync(ReqDefId);
+
+            Assert.AreEqual(ReqDefId, result.Id);
+        }
+
+        [TestMethod]
+        public async Task GetRequirementDefinitionsByIds_KnownReqDefId_ReturnReqDef()
+        {
+            var result = await _dut.GetRequirementDefinitionsByIdsAsync(new List<int> {ReqDefId});
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(ReqDefId, result.First().Id);
+        }
+    }
+}
