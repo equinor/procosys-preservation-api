8820248cbad40754ae04599ee26180dc5fbd6133
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-28 11:10:29 +0100

    unit tests

---
+++ src/tests/Equinor.Procosys.Preservation.Domain.Tests/AggregateModels/ProjectAggregate/FieldValueTests.cs
@@ -0,0 +1,22 @@
+﻿using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Domain.Tests.AggregateModels.ProjectAggregate
+{
+    [TestClass]
+    public class FieldValueTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldSetProperties()
+        {
+            var fieldMock = new Mock<Field>();
+            fieldMock.SetupGet(f => f.Id).Returns(54);
+            var f = new FieldValue("SchemaA", fieldMock.Object);
+
+            Assert.AreEqual("SchemaA", f.Schema);
+            Assert.AreEqual(54, f.FieldId);
+        }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Domain.Tests/AggregateModels/ProjectAggregate/NumberValueTests.cs
@@ -0,0 +1,33 @@
+﻿using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Domain.Tests.AggregateModels.ProjectAggregate
+{
+    [TestClass]
+    public class NumberValueTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldSetProperties_WhenNullValue()
+        {
+            var fieldMock = new Mock<Field>();
+            fieldMock.SetupGet(f => f.Id).Returns(54);
+            var f = new NumberValue("SchemaA", fieldMock.Object, null);
+
+            Assert.AreEqual("SchemaA", f.Schema);
+            Assert.AreEqual(54, f.FieldId);
+            Assert.IsFalse(f.Value.HasValue);
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldSetProperties_WhenValue()
+        {
+            var f = new NumberValue("SchemaA", new Mock<Field>().Object, 1.4);
+
+            Assert.AreEqual("SchemaA", f.Schema);
+            Assert.IsTrue(f.Value.HasValue);
+            Assert.AreEqual(1.4, f.Value.Value);
+        }
+    }
+}
