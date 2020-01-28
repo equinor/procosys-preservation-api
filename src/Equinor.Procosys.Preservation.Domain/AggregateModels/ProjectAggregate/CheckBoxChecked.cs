685902ad58611473993f64018ada69826ef899c0
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-01-28 10:25:51 +0100

    Attachment : FieldValue Docs in child classes

--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/ProjectAggregate/CheckBoxChecked.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/ProjectAggregate/CheckBoxChecked.cs
@@ -3,7 +3,10 @@
 namespace Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate
 {
     // A field needing input can be of 3 types: Number, CheckBox and Attachment
-    // This class represent a checked CheckBox
+    // This class represent a CHECKED CheckBox field,
+    // I.e:
+    //      If table row exists -> end user has checked the checkbox for particular field and saved
+    //      If end user uncheck checkbox for particular field, and save, table row will be deleted
     public class CheckBoxChecked : FieldValue
     {
         protected CheckBoxChecked()
--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/ProjectAggregate/NumberValue.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/ProjectAggregate/NumberValue.cs
@@ -3,7 +3,11 @@
 namespace Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate
 {
     // A field needing input can be of 3 types: Number, CheckBox and Attachment
-    // This class represent Number
+    // This class represent a Number field
+    // I.e:
+    //      If table row exists -> end user has either filled a numeric value for particular field, or written NA (or N/A) and saved
+    //      Value == null in an existing row, represent NA
+    //      If end user blank an existing value (or NA) for particular field, and save, table row will be deleted
     public class NumberValue : FieldValue
     {
         protected NumberValue()
@@ -13,6 +17,6 @@ namespace Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate
         public NumberValue(string schema, Field field, double? value)
             : base(schema, field) => Value = value;
 
-        public double? Value { get; private set; } // Value == null means NA
+        public double? Value { get; private set; }
     }
 }
