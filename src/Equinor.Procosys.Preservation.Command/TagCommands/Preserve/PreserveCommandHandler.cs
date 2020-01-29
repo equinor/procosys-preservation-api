ba842fa88fa0c8fea368765ccdeaec7e6352ddec
Henning Bjørgo <HHBJ@equinor.com>
2020-01-29 13:03:23 +0100

    Fixed unit tests.

--- src/Equinor.Procosys.Preservation.Command/TagCommands/Preserve/PreserveCommandHandler.cs
+++ src/Equinor.Procosys.Preservation.Command/TagCommands/Preserve/PreserveCommandHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Domain;
-using Equinor.Procosys.Preservation.Domain.AggregateModels.PersonAggregate;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
 using MediatR;
 using ServiceResult;
@@ -11,20 +10,17 @@ namespace Equinor.Procosys.Preservation.Command.TagCommands.Preserve
     public class PreserveCommandHandler : IRequestHandler<PreserveCommand, Result<Unit>>
     {
         private readonly IProjectRepository _projectRepository;
-        private readonly IPersonRepository _personRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly ICurrentUserProvider _currentUserProvider;
         private readonly ITimeService _timeService;
 
         public PreserveCommandHandler(
             IProjectRepository projectRepository,
-            IPersonRepository personRepository,
             ITimeService timeService,
             IUnitOfWork unitOfWork,
             ICurrentUserProvider currentUserProvider)
         {
             _projectRepository = projectRepository;
-            _personRepository = personRepository;
             _timeService = timeService;
             _unitOfWork = unitOfWork;
             _currentUserProvider = currentUserProvider;
