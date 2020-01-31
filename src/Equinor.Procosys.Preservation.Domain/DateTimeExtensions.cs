c1fafe41badcfec520162fc92373e6dc3d9067b7
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-01-31 09:22:35 +0100

    RequirementDTO.NextDueWeeks  (#63)
    * In RequirementDTO: calculate number of due weeks so the number is the same during the week. I.e the number of due weeks shifts at Monday night regardless of where in week the duedate / current date is
    
    * Rename method

--- src/Equinor.Procosys.Preservation.Domain/DateTimeExtensions.cs
+++ src/Equinor.Procosys.Preservation.Domain/DateTimeExtensions.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 namespace Equinor.Procosys.Preservation.Domain
 {
     public static class DateTimeExtensions
     {
+        private static readonly DayOfWeek firstDayOfWeek = Thread.CurrentThread.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+
         public static DateTime AddWeeks(this DateTime dateTime, int weeks)
             => dateTime.AddDays(7 * weeks);
 
         public static string FormatAsYearAndWeekString(this DateTime dateTime)
             => string.Concat(ISOWeek.GetYear(dateTime).ToString(), "w", ISOWeek.GetWeekOfYear(dateTime).ToString("00"));
+
+        public static DateTime StartOfWeek(this DateTime dt)
+        {
+            var dayOfWeek = GetDayOfWeekMondayAsFirst(dt);
+            var diff = -(dayOfWeek - (int)firstDayOfWeek);
+            return dt.AddDays(diff).Date;
+        }
+
+        public static int GetWeeksUntil(this DateTime fromDateTime, DateTime toDateTime)
+        {
+            var startOfFromWeek = fromDateTime.StartOfWeek();
+            var startOfToWeek = toDateTime.StartOfWeek();
+            var timeSpan = startOfToWeek - startOfFromWeek;
+            
+            return timeSpan.Weeks();
+        }
+
+        private static int GetDayOfWeekMondayAsFirst(DateTime dt)
+        {
+            if (dt.DayOfWeek == DayOfWeek.Sunday)
+            {
+                // treat Sunday as last day of week since enum DayOfWeek has Sunday as 0
+                return 7;
+            }
+
+            return (int)dt.DayOfWeek;
+        }
     }
 }
--- src/Equinor.Procosys.Preservation.Query/ProjectAggregate/RequirementDto.cs
+++ src/Equinor.Procosys.Preservation.Query/ProjectAggregate/RequirementDto.cs
@@ -5,20 +5,27 @@ namespace Equinor.Procosys.Preservation.Query.ProjectAggregate
 {
     public class RequirementDto
     {
-        public RequirementDto(int id, int requirementDefinitionId, DateTime? nextDueTimeUtc, TimeSpan timeUntilNextDueTime)
+        public RequirementDto(int id, int requirementDefinitionId, DateTime currentTimeUtc, DateTime? nextDueTimeUtc)
         {
             Id = id;
             RequirementDefinitionId = requirementDefinitionId;
             NextDueTimeUtc = nextDueTimeUtc;
-            NextDueWeeks = timeUntilNextDueTime.Weeks();
+            
+            if (nextDueTimeUtc.HasValue)
+            {
+                NextDueWeeks = currentTimeUtc.GetWeeksUntil(nextDueTimeUtc.Value);
+            }
+            NextDueAsYearAndWeek = NextDueTimeUtc?.FormatAsYearAndWeekString();
         }
 
         public int Id { get; }
         public int RequirementDefinitionId { get; }
         public DateTime? NextDueTimeUtc { get; }
+        public string NextDueAsYearAndWeek { get; }
 
-        public string NextDueAsYearAndWeek => NextDueTimeUtc?.FormatAsYearAndWeekString();
-
+        /// <summary>
+        /// NextDueWeeks shifts at Monday night regardless of where in week the NextDueTimeUtc / current time is
+        /// </summary>
         public int NextDueWeeks { get; }
     }
 }
