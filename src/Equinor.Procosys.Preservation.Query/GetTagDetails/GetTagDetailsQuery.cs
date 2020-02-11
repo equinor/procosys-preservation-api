e7482e264f0cef2d537e5576f38373e09ec973f3
Henning Bjørgo <HHBJ@equinor.com>
2020-02-11 11:34:33 +0100

    Expr. body.

--- src/Equinor.Procosys.Preservation.Query/GetTagDetails/GetTagDetailsQuery.cs
+++ src/Equinor.Procosys.Preservation.Query/GetTagDetails/GetTagDetailsQuery.cs
@@ -5,10 +5,7 @@ namespace Equinor.Procosys.Preservation.Query.GetTagDetails
 {
     public class GetTagDetailsQuery : IRequest<Result<TagDetailsDto>>
     {
-        public GetTagDetailsQuery(int id)
-        {
-            Id = id;
-        }
+        public GetTagDetailsQuery(int id) => Id = id;
 
         public int Id { get; }
     }
