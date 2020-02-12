4f116d004a2e5fc08cb0fbee00893fcfcdbd4f3c
Henning Bjørgo <HHBJ@equinor.com>
2020-02-12 07:34:22 +0100

    Added tests.

---
+++ src/tests/Equinor.Procosys.Preservation.Query.Tests/GetDisciplines/DisciplineDtoTests.cs
@@ -0,0 +1,18 @@
+﻿using Equinor.Procosys.Preservation.Query.GetDisciplines;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Query.Tests.GetDisciplines
+{
+    [TestClass]
+    public class DisciplineDtoTests
+    {
+        [TestMethod]
+        public void Constructor_SetsProperties()
+        {
+            var dut = new DisciplineDto("CodeA", "DescriptionA");
+
+            Assert.AreEqual("CodeA", dut.Code);
+            Assert.AreEqual("DescriptionA", dut.Description);
+        }
+    }
+}
