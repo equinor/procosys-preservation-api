c6b59320d536534ea0d240589e9605513095309b
Henning Bjørgo <HHBJ@equinor.com>
2020-02-12 07:23:53 +0100

    Functionality.

---
+++ src/Equinor.Procosys.Preservation.Query/GetDisciplines/GetDisciplinesQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.MainApi.Discipline;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Query.GetDisciplines
+{
+    public class GetDisciplinesQueryHandler : IRequestHandler<GetDisciplinesQuery, Result<List<DisciplineDto>>>
+    {
+        private readonly IDisciplineApiService _disciplineApiService;
+        private readonly IPlantProvider _plantProvider;
+
+        public GetDisciplinesQueryHandler(IDisciplineApiService disciplineApiService, IPlantProvider plantProvider)
+        {
+            _disciplineApiService = disciplineApiService;
+            _plantProvider = plantProvider;
+        }
+
+        public async Task<Result<List<DisciplineDto>>> Handle(GetDisciplinesQuery request, CancellationToken cancellationToken)
+        {
+            var disciplines = await _disciplineApiService.GetDisciplines(_plantProvider.Plant);
+            var disciplineDtos = disciplines
+                .Select(ac => new DisciplineDto(ac.Code, ac.Description))
+                .ToList();
+            return new SuccessResult<List<DisciplineDto>>(disciplineDtos);
+        }
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Disciplines/DisciplinesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Query.GetDisciplines;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ServiceResult.ApiExtensions;
+
+namespace Equinor.Procosys.Preservation.WebApi.Controllers.Disciplines
+{
+    [ApiController]
+    [Route("Disciplines")]
+    public class DisciplinesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DisciplinesController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet]
+        public async Task<ActionResult<List<DisciplineDto>>> GetDisciplines()
+        {
+            var result = await _mediator.Send(new GetDisciplinesQuery());
+            return this.FromResult(result);
+        }
+    }
+}
