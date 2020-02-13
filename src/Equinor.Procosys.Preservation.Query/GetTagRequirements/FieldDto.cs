b9aa455ae7d38e9aba0310e9029b19c72a026486
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-13 14:26:29 +0100

    brainfart in last commit

--- src/Equinor.Procosys.Preservation.Query/GetTagRequirements/FieldDto.cs
+++ src/Equinor.Procosys.Preservation.Query/GetTagRequirements/FieldDto.cs
@@ -4,13 +4,15 @@ namespace Equinor.Procosys.Preservation.Query.GetTagRequirements
 {
     public class FieldDto
     {
-        public FieldDto(int id, string label, FieldType fieldType, string unit, bool? showPrevious)
+        public FieldDto(int id, string label, FieldType fieldType, string unit, bool? showPrevious, string currentValue, string previousValue)
         {
             Id = id;
             Label = label;
             FieldType = fieldType;
             Unit = unit;
             ShowPrevious = showPrevious;
+            CurrentValue = currentValue;
+            PreviousValue = previousValue;
         }
 
         public int Id { get; }
@@ -18,5 +20,7 @@ namespace Equinor.Procosys.Preservation.Query.GetTagRequirements
         public FieldType FieldType { get; }
         public string Unit { get; }
         public bool? ShowPrevious { get; }
+        public string CurrentValue { get; }
+        public string PreviousValue { get; }
     }
 }
