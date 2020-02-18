d59746b5d22bda9d614125bc612faa42b9a79e47
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-18 11:19:43 +0100

    Last test

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/TagCommands/Transfer/TransferCommandHandlerTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/TagCommands/Transfer/TransferCommandHandlerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Command.TagCommands.Transfer;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
@@ -10,10 +12,10 @@ namespace Equinor.Procosys.Preservation.Command.Tests.TagCommands.Transfer
     [TestClass]
     public class TransferCommandHandlerTests : CommandHandlerTestsBase
     {
-        private const int StepId1OnJourney1 = 1;
-        private const int StepId2OnJourney1 = 2;
-        private const int StepId1OnJourney2 = 3;
-        private const int StepId2OnJourney2 = 4;
+        private const int Step1OnJourney1Id = 1;
+        private const int Step2OnJourney1Id = 2;
+        private const int Step1OnJourney2Id = 3;
+        private const int Step2OnJourney2Id = 4;
 
         private const int TagId1 = 7;
         private const int TagId2 = 8;
@@ -21,15 +23,52 @@ namespace Equinor.Procosys.Preservation.Command.Tests.TagCommands.Transfer
         private TransferCommand _command;
 
         private TransferCommandHandler _dut;
+        private Mock<Tag> _tag1Mock;
+        private Mock<Tag> _tag2Mock;
 
         [TestInitialize]
         public void Setup()
         {
+            var step1OnJourney1Mock = new Mock<Step>();
+            step1OnJourney1Mock.SetupGet(x => x.Id).Returns(Step1OnJourney1Id);
+            var step2OnJourney1Mock = new Mock<Step>();
+            step2OnJourney1Mock.SetupGet(x => x.Id).Returns(Step2OnJourney1Id);
+
+            var journey1 = new Journey("","");
+            journey1.AddStep(step1OnJourney1Mock.Object);
+            journey1.AddStep(step2OnJourney1Mock.Object);
+
+            var step1OnJourney2Mock = new Mock<Step>();
+            step1OnJourney2Mock.SetupGet(x => x.Id).Returns(Step1OnJourney2Id);
+            var step2OnJourney2Mock = new Mock<Step>();
+            step2OnJourney2Mock.SetupGet(x => x.Id).Returns(Step2OnJourney2Id);
+
+            var journey2 = new Journey("","");
+            journey2.AddStep(step1OnJourney2Mock.Object);
+            journey2.AddStep(step2OnJourney2Mock.Object);
+
             var journeyRepoMock = new Mock<IJourneyRepository>();
+            journeyRepoMock
+                .Setup(r => r.GetJourneysByStepIdsAsync(new List<int> {Step1OnJourney1Id, Step1OnJourney2Id}))
+                .Returns(Task.FromResult(new List<Journey> {journey1, journey2}));
 
+            _tag1Mock = new Mock<Tag>("", TagType.Standard, "", "", "", "", "", "", "", "", "", "", step1OnJourney1Mock.Object,
+                new List<Requirement> {new Mock<Requirement>().Object});
+            _tag1Mock.SetupGet(t => t.Id).Returns(TagId1);
 
+            _tag2Mock = new Mock<Tag>("", TagType.Standard, "", "", "", "", "", "", "", "", "", "", step1OnJourney2Mock.Object,
+                new List<Requirement> {new Mock<Requirement>().Object});
+            _tag2Mock.SetupGet(t => t.Id).Returns(TagId2);
 
             var projectRepoMock = new Mock<IProjectRepository>();
+            
+            var tagIds = new List<int> {TagId1, TagId2};
+            projectRepoMock
+                .Setup(r => r.GetTagsByTagIdsAsync(tagIds))
+                .Returns(Task.FromResult(new List<Tag> {_tag1Mock.Object, _tag2Mock.Object}));
+
+            _command = new TransferCommand(tagIds);
+
             _dut = new TransferCommandHandler(projectRepoMock.Object, journeyRepoMock.Object, UnitOfWorkMock.Object);
         }
 
@@ -37,6 +76,19 @@ namespace Equinor.Procosys.Preservation.Command.Tests.TagCommands.Transfer
         public async Task HandlingTransferCommand_Should()
         {
             await _dut.Handle(_command, default);
+
+            Assert.AreEqual(Step2OnJourney1Id, _tag1Mock.Object.StepId);
+            Assert.AreEqual(Step2OnJourney2Id, _tag2Mock.Object.StepId);
+        }
+        
+        [TestMethod]
+        public async Task HandlingTransferCommand_ShouldThrowException_WhenTransferFromLastStep()
+        {
+            await _dut.Handle(_command, default);
+
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(() =>
+                _dut.Handle(_command, default)
+            );
         }
 
         [TestMethod]
