f4cc48137e4598d6f64a0c5ef5bbe647977a8a60
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-02-18 07:26:24 +0100

    Rename AreaCode stuff to Area (#82)

---
+++ src/Equinor.Procosys.Preservation.Query/GetAreas/GetAreasQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Query.GetAreas
+{
+    public class GetAreasQuery : IRequest<Result<List<AreaDto>>>
+    {
+    }
+}
