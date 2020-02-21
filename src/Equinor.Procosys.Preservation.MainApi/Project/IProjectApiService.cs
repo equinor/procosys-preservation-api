db4b7262fe50722bd1baabbf407fe99319f48bfa
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-21 07:50:31 +0100

    MainApiProjectService with tests

---
+++ src/Equinor.Procosys.Preservation.MainApi/Project/IProjectApiService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Preservation.MainApi.Project
+{
+    public interface IProjectApiService
+    {
+        Task<ProcosysProject> GetProject(string plant, string name);
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.MainApi/Project/MainApiProjectService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.MainApi.Client;
+using Equinor.Procosys.Preservation.MainApi.Plant;
+using Microsoft.Extensions.Options;
+
+namespace Equinor.Procosys.Preservation.MainApi.Project
+{
+    public class MainApiProjectService : IProjectApiService
+    {
+        private readonly string _apiVersion;
+        private readonly Uri _baseAddress;
+        private readonly IBearerTokenApiClient _mainApiClient;
+        private readonly IPlantApiService _plantApiService;
+
+        public MainApiProjectService(IBearerTokenApiClient mainApiClient,
+            IPlantApiService plantApiService,
+            IOptionsMonitor<MainApiOptions> options)
+        {
+            _mainApiClient = mainApiClient;
+            _plantApiService = plantApiService;
+            _apiVersion = options.CurrentValue.ApiVersion;
+            _baseAddress = new Uri(options.CurrentValue.BaseAddress);
+        }
+
+        public async Task<ProcosysProject> GetProject(string plant, string name)
+        {
+            if (!await _plantApiService.IsPlantValidAsync(plant))
+            {
+                throw new ArgumentException($"Invalid plant: {plant}");
+            }
+
+            var url = $"{_baseAddress}ProjectByName" +
+                $"?plantId={plant}" +
+                $"&projectName={name}" +
+                $"&api-version={_apiVersion}";
+
+            return await _mainApiClient.QueryAndDeserialize<ProcosysProject>(url);
+        }
+    }
+}
