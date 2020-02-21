f351d89004529333ebe379a87c4eb8c50cbbb5d0
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-21 11:37:54 +0100

    Tests

---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/RequirementCommands/Preserve/PreserveCommandValidatorTests.cs
@@ -0,0 +1,112 @@
+﻿using Equinor.Procosys.Preservation.Command.RequirementCommands.Preserve;
+using Equinor.Procosys.Preservation.Command.Validators.Tag;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.RequirementCommands.Preserve
+{
+    [TestClass]
+    public class PreserveCommandValidatorTests
+    {
+        private const int TagId = 7;
+        private const int RequirementId = 71;
+        private PreserveCommandValidator _dut;
+        private Mock<ITagValidator> _tagValidatorMock;
+        private PreserveCommand _command;
+
+        [TestInitialize]
+        public void Setup_OkState()
+        {
+            _tagValidatorMock = new Mock<ITagValidator>();
+            _tagValidatorMock.Setup(r => r.Exists(TagId)).Returns(true);
+            _tagValidatorMock.Setup(r => r.HasANonVoidedRequirement(TagId)).Returns(true);
+            _tagValidatorMock.Setup(r => r.VerifyPreservationStatus(TagId, PreservationStatus.Active)).Returns(true);
+            _tagValidatorMock.Setup(r => r.HaveRequirementReadyToBePreserved(TagId, RequirementId)).Returns(true);
+            _command = new PreserveCommand(TagId, RequirementId);
+
+            _dut = new PreserveCommandValidator(_tagValidatorMock.Object);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldBeValid_WhenOkState()
+        {
+            var result = _dut.Validate(_command);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenTagNotExists()
+        {
+            _tagValidatorMock.Setup(r => r.Exists(TagId)).Returns(false);
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag doesn't exists!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenTagIsVoided()
+        {
+            _tagValidatorMock.Setup(r => r.IsVoided(TagId)).Returns(true);
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag is voided!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenProjectForTagIsClosed()
+        {
+            _tagValidatorMock.Setup(r => r.ProjectIsClosed(TagId)).Returns(true);
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Project for tag is closed!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenPreservationIsNotActiveForTag()
+        {
+            _tagValidatorMock.Setup(r => r.VerifyPreservationStatus(TagId, PreservationStatus.Active)).Returns(false);
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith($"Tag must have status {PreservationStatus.Active} to preserve!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenRequirementNotReadyToBePreserved()
+        {
+            _tagValidatorMock.Setup(r => r.HaveRequirementReadyToBePreserved(TagId, RequirementId)).Returns(false);
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag doesn't have this requirement ready to be preserved!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFailWith1Error_When2Errors()
+        {
+            _tagValidatorMock.Setup(r => r.ProjectIsClosed(TagId)).Returns(true);
+            _tagValidatorMock.Setup(r => r.Exists(TagId)).Returns(false);
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag doesn't exists!"));
+        }
+    }
+}
