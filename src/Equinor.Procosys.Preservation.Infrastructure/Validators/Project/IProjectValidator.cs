350d8fc42f752aaea733da92c871844819b1ea93
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-22 10:02:24 +0100

    Moved IProjectValidator and impl from Command to InfraStructure. Impl now uses IReadOnlyContext only .. thin queries and Async methods

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Validators/Project/IProjectValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Validators.Project
+{
+    public interface IProjectValidator
+    {
+        Task<bool> ExistsAsync(string projectName, CancellationToken cancellationToken);
+        
+        Task<bool> IsExistingAndClosedAsync(string projectName, CancellationToken cancellationToken);
+        
+        Task<bool> IsClosedForTagAsync(int tagId, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Validators/Project/ProjectValidator.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Microsoft.EntityFrameworkCore;
+using DomProject = Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate.Project;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Validators.Project
+{
+    public class ProjectValidator : IProjectValidator
+    {
+        private readonly IReadOnlyContext _context;
+
+        public ProjectValidator(IReadOnlyContext context) => _context = context;
+
+        public async Task<bool> ExistsAsync(string projectName, CancellationToken cancellationToken)
+        {
+            var count = await (from p in _context.QuerySet<DomProject>()
+                where p.Name == projectName
+                select p).CountAsync(cancellationToken);
+            return count > 0;
+        }
+
+        public async Task<bool> IsExistingAndClosedAsync(string projectName, CancellationToken cancellationToken)
+        {
+            var project = await (from p in _context.QuerySet<DomProject>()
+                where p.Name == projectName
+                select p).FirstOrDefaultAsync(cancellationToken);
+
+            return project != null && project.IsClosed;
+        }
+
+        public async Task<bool> IsClosedForTagAsync(int tagId, CancellationToken cancellationToken)
+        {
+            var project = await (from tag in _context.QuerySet<Tag>()
+                join p in _context.QuerySet<DomProject>() on EF.Property<int>(tag, "ProjectId") equals p.Id
+                where tag.Id == tagId
+                select p).FirstOrDefaultAsync(cancellationToken);
+
+            return project != null && project.IsClosed;
+        }
+    }
+}
