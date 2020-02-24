5caf2cab5c0c1909b3564160c18d9ec3427c4967
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-24 14:09:08 +0100

    more tests

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/Validators/ProjectValidatorTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/Validators/ProjectValidatorTests.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Command.Validators.ProjectValidators;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
 using Equinor.Procosys.Preservation.Infrastructure;
 using Equinor.Procosys.Preservation.Test.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Equinor.Procosys.Preservation.Command.Tests.Validators
 {
@@ -12,13 +16,19 @@ namespace Equinor.Procosys.Preservation.Command.Tests.Validators
     {
         private const string ProjectNameNotClosed = "Project name";
         private const string ProjectNameClosed = "Project name (closed)";
+        private int _tagInClosedProjectId;
+        private int _tagInNotClosedProjectId;
 
         protected override void SetupNewDatabase(DbContextOptions<PreservationContext> dbContextOptions)
         {
             using (var context = new PreservationContext(dbContextOptions, _eventDispatcher, _plantProvider))
             {
-                AddProject(context, ProjectNameNotClosed, "Project description");
-                AddProject(context, ProjectNameClosed, "Project description", true);
+                var notClosedProject = AddProject(context, ProjectNameNotClosed, "Project description");
+                var closedProject = AddProject(context, ProjectNameClosed, "Project description", true);
+                var t1 = AddTag(context, notClosedProject, "T1", "Tag description", new Mock<Step>().Object, new List<Requirement>{ new Mock<Requirement>().Object});
+                _tagInNotClosedProjectId = t1.Id;
+                var t2 = AddTag(context, closedProject, "T2", "Tag description", new Mock<Step>().Object, new List<Requirement>{ new Mock<Requirement>().Object});
+                _tagInClosedProjectId = t2.Id;
             }
         }
 
@@ -34,7 +44,7 @@ namespace Equinor.Procosys.Preservation.Command.Tests.Validators
         }
 
         [TestMethod]
-        public async Task ValidateExists_UnknownName_ReturnsFalse()
+        public async Task ExistsAsync_UnknownName_ReturnsFalse()
         {
             using (var context = new PreservationContext(_dbContextOptions, _eventDispatcher, _plantProvider))
             {
@@ -73,6 +83,39 @@ namespace Equinor.Procosys.Preservation.Command.Tests.Validators
             {
                 var dut = new ProjectValidator(context);
                 var result = await dut.IsExistingAndClosedAsync("XX", default);
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task IsClosedForTagAsync_KnownTag_ReturnsFalse()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _eventDispatcher, _plantProvider))
+            {
+                var dut = new ProjectValidator(context);
+                var result = await dut.IsClosedForTagAsync(_tagInNotClosedProjectId, default);
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task IsClosedForTagAsync_KnownTag_ReturnsTrue()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _eventDispatcher, _plantProvider))
+            {
+                var dut = new ProjectValidator(context);
+                var result = await dut.IsClosedForTagAsync(_tagInClosedProjectId, default);
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task IsClosedForTagAsync_UnknownTag_ReturnsFalse()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _eventDispatcher, _plantProvider))
+            {
+                var dut = new ProjectValidator(context);
+                var result = await dut.IsClosedForTagAsync(0, default);
                 Assert.IsFalse(result);
             }
         }
---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/Validators/TagValidatorTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.Validators.TagValidators;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.Procosys.Preservation.Infrastructure;
+using Equinor.Procosys.Preservation.Test.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.Validators
+{
+    [TestClass]
+    public class TagValidatorTests : ReadOnlyTestsBase
+    {
+        private const string ProjectName = "P";
+        private const string TagNo = "PA-13";
+
+        protected override void SetupNewDatabase(DbContextOptions<PreservationContext> dbContextOptions)
+        {
+            using (var context = new PreservationContext(dbContextOptions, _eventDispatcher, _plantProvider))
+            {
+                var p = AddProject(context, ProjectName, "Project description");
+                var j = AddJourneyWithStep(context, "J", AddMode(context, "M"), AddResponsible(context, "R"));
+                var rt = AddRequirementTypeWith1DefWithoutField(context, "Rot", "D");
+                var r = new Requirement(_schema, 4, rt.RequirementDefinitions.First());
+                AddTag(context, p, TagNo, "Tag description", j.Steps.First(), new List<Requirement>{r});
+            }
+        }
+
+        [TestMethod]
+        public async Task ExistsAsync_KnownTag_ReturnsTrue()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _eventDispatcher, _plantProvider))
+            {
+                var dut = new TagValidator(context);
+                var result = await dut.ExistsAsync(TagNo, ProjectName, default);
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task ExistsAsync_UnknownTag_ReturnsTrue()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _eventDispatcher, _plantProvider))
+            {
+                var dut = new TagValidator(context);
+                var result = await dut.ExistsAsync("X", ProjectName, default);
+                Assert.IsFalse(result);
+            }
+        }
+    }
+}
