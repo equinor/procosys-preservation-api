a5e84ce0984d0c72046f5796adf352c501d979f9
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-25 13:34:15 +0100

    Migration for set correct value into Requirement.[InitialPreservationPeriodStatus]

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200225123159_InitialPreservationPeriodStatusUpdate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class InitialPreservationPeriodStatusUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+            => migrationBuilder.Sql(@"update Requirements
+                        set InitialPreservationPeriodStatus = 'ReadyToBePreserved'
+                        where Requirements.RequirementDefinitionId not in (  select rd.Id from Fields f
+                         join RequirementDefinitions rd on rd.Id =  f.RequirementDefinitionId
+                          where f.FieldType in ('CheckBox','Number'))");
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
