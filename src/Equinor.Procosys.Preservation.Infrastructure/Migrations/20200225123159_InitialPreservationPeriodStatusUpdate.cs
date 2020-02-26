d18f9606ec8cefcef458c74cc05b23a549932972
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-02-26 15:11:04 +0100

    yet a migration

--- src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200225123159_InitialPreservationPeriodStatusUpdate.cs
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200225123159_InitialPreservationPeriodStatusUpdate.cs
@@ -7,9 +7,11 @@ namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
             => migrationBuilder.Sql(@"update Requirements
                         set InitialPreservationPeriodStatus = 'ReadyToBePreserved'
-                        where Requirements.RequirementDefinitionId not in (  select rd.Id from Fields f
-                         join RequirementDefinitions rd on rd.Id =  f.RequirementDefinitionId
-                          where f.FieldType in ('CheckBox','Number'))");
+                        where Requirements.RequirementDefinitionId not in (
+                            select rd.Id from Fields f
+                            join RequirementDefinitions rd on rd.Id = f.RequirementDefinitionId
+                            where f.FieldType in ('CheckBox','Number')
+                        )");
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200226135751_PreservationPeriodStatusUpdate.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class PreservationPeriodStatusUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+            => migrationBuilder.Sql(@"update PreservationPeriods 
+                        set Status='ReadyToBePreserved'
+                        where id in (  
+                            select pp.id from PreservationPeriods pp 
+                            join Requirements r on pp.RequirementId=r.Id
+                            where pp.Status = 'NeedsUserInput' and r.InitialPreservationPeriodStatus='ReadyToBePreserved'
+                        )");
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
