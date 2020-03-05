8a0e75efdfd60e3e077ee497925eb454c8725a0d
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-05 15:00:08 +0100

    Added Empty migration to create correct PreservationContextModelSnapshot

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200305135702_Empty.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class Empty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
