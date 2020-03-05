f5a314b5a2849e4a123d698a32947427e6950db5
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-05 15:04:33 +0100

    Migration

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200305140127_TagNextDueTimeUtc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class TagNextDueTimeUtc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NextDueTimeUtc",
+                table: "Tags",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NextDueTimeUtc",
+                table: "Tags");
+        }
+    }
+}
