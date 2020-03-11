25c80c1890e879c2a47a9a3d2d04a03354373015
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-11 15:47:53 +0100

    Refactored GetAllModesQueryHandler and GetAllRequirementTypesQueryHandler to use IReadOnlyContext

--- src/Equinor.Procosys.Preservation.Query/ModeAggregate/GetAllModesQueryHandler.cs
+++ src/Equinor.Procosys.Preservation.Query/ModeAggregate/GetAllModesQueryHandler.cs
@@ -2,21 +2,24 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.ModeAggregate;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using ServiceResult;
 
 namespace Equinor.Procosys.Preservation.Query.ModeAggregate
 {
     public class GetAllModesQueryHandler : IRequestHandler<GetAllModesQuery, Result<IEnumerable<ModeDto>>>
     {
-        private readonly IModeRepository _modeRepository;
+        private readonly IReadOnlyContext _context;
 
-        public GetAllModesQueryHandler(IModeRepository modeRepository) => _modeRepository = modeRepository;
+        public GetAllModesQueryHandler(IReadOnlyContext context) => _context = context;
 
         public async Task<Result<IEnumerable<ModeDto>>> Handle(GetAllModesQuery request, CancellationToken cancellationToken)
         {
-            var modes = await _modeRepository.GetAllAsync();
+            var modes = await (from m in _context.QuerySet<Mode>()
+                select m).ToListAsync(cancellationToken);
             return new SuccessResult<IEnumerable<ModeDto>>(modes.Select(mode => new ModeDto(mode.Id, mode.Title)));
         }
     }
