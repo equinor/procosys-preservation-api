bb69ca7c6cad52f60cfdb31bb553642adf4ed13b
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-15 13:16:08 +0100

    Added Step.Title to Model

--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/JourneyAggregate/Step.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/JourneyAggregate/Step.cs
@@ -8,12 +8,14 @@ namespace Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate
 {
     public class Step : SchemaEntityBase, ICreationAuditable, IModificationAuditable
     {
+        public const int TitleLengthMin = 1;
+        public const int TitleLengthMax = 64;
         protected Step()
             : base(null)
         {
         }
 
-        public Step(string schema, Mode mode, Responsible responsible)
+        public Step(string schema, string title, Mode mode, Responsible responsible)
             : base(schema)
         {
             if (mode == null)
@@ -36,14 +38,16 @@ namespace Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate
                 throw new ArgumentException($"Can't relate item in {responsible.Schema} to item in {schema}");
             }
 
+            Title = title;
             SortKey = 0;
             ModeId = mode.Id;
             ResponsibleId = responsible.Id;
         }
 
+        public string Title { get; private set; }
         public int ModeId { get; private set; }
         public int ResponsibleId { get; private set; }
-        public int SortKey { get; set; }  // sortKey will be set correct in later PBI when impl UI for Add, MoveUp and MoveDown of Steps in Journey
+        public int SortKey { get; set; }  // sortKey will be set correct in later PBI when impl UI for Add, MoveUp and MoveDown of Steps in Step
         public bool IsVoided { get; private set; }
 
         public DateTime CreatedAtUtc { get; private set; }
