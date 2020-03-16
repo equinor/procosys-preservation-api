b764ada1fd396bdd7362609f8493e79180427104
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-16 19:16:55 +0100

    migration

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200316181225_TagStorageArea.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class TagStorageArea : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StorageArea",
+                table: "Tags",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StorageArea",
+                table: "Tags");
+        }
+    }
+}
