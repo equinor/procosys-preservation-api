769cf1526d0f7a3d51aad41686f917a13b73f9bb
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-03-18 11:57:59 +0100

    72144 Endpoint for getting unique filter values for responsible codes (#130)
    * Get Filter values for unique responsibles
    
    * tests
    
    * rename

---
+++ src/Equinor.Procosys.Preservation.Query/GetUniqueTagResponsibles/GetUniqueTagResponsiblesQuery.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Query.GetUniqueTagResponsibles
+{
+    public class GetUniqueTagResponsiblesQuery : IRequest<Result<List<ResponsibleDto>>>
+    {
+        public GetUniqueTagResponsiblesQuery(string projectName) => ProjectName = projectName;
+
+        public string ProjectName { get; }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Query.Tests/GetUniqueTagResponsibles/GetUniqueTagResponsiblesQueryTests.cs
@@ -0,0 +1,17 @@
+﻿using Equinor.Procosys.Preservation.Query.GetUniqueTagResponsibles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Query.Tests.GetUniqueTagResponsibles
+{
+    [TestClass]
+    public class GetUniqueTagResponsiblesQueryTests
+    {
+        [TestMethod]
+        public void Constructor_SetsProperties()
+        {
+            var dut = new GetUniqueTagResponsiblesQuery("PX");
+            
+            Assert.AreEqual("PX", dut.ProjectName);
+        }
+    }
+}
