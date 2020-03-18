247b971c7e79bc8936b0e642dd5019c011b01ca9
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-18 15:12:14 +0100

    test query

---
+++ src/tests/Equinor.Procosys.Preservation.Query.Tests/TagFunctionAggregate/GetTagFunctionQueryQueryHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.TagFunctionAggregate;
+using Equinor.Procosys.Preservation.Infrastructure;
+using Equinor.Procosys.Preservation.Query.TagFunctionAggregate;
+using Equinor.Procosys.Preservation.Test.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Query.Tests.TagFunctionAggregate
+{
+    [TestClass]
+    public class GetTagFunctionQueryQueryHandlerTests : ReadOnlyTestsBase
+    {
+        private TagFunction _tf;
+
+        protected override void SetupNewDatabase(DbContextOptions<PreservationContext> dbContextOptions)
+        {
+            using (var context = new PreservationContext(dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                _tf = AddTagFunction(context, "TFC", "RC");
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetTagFunctionQuery_KnownTagFunction_ShouldReturnTagFunctionWithAllPropertiesSet()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var dut = new GetTagFunctionQueryHandler(context);
+                var result = await dut.Handle(new GetTagFunctionQuery(_tf.Code, _tf.RegisterCode), default);
+
+                var tagFunction = result.Data;
+
+                Assert.IsNotNull(tagFunction);
+                Assert.AreEqual(_tf.Code, tagFunction.Code);
+                Assert.AreEqual(_tf.Description, tagFunction.Description);
+                Assert.AreEqual(_tf.RegisterCode, tagFunction.RegisterCode);
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetTagFunctionQuery_UnknownTagFunction_ShouldReturnNull()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var dut = new GetTagFunctionQueryHandler(context);
+                var result = await dut.Handle(new GetTagFunctionQuery("XX", _tf.RegisterCode), default);
+                Assert.IsNull(result.Data);
+            }
+        }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Query.Tests/TagFunctionAggregate/GetTagFunctionQueryTests.cs
@@ -0,0 +1,17 @@
+﻿using Equinor.Procosys.Preservation.Query.TagFunctionAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Query.Tests.TagFunctionAggregate
+{
+    [TestClass]
+    public class GetTagFunctionQueryTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldSetProperties()
+        {
+            var dut = new GetTagFunctionQuery("A", "B");
+            Assert.AreEqual("A", dut.TagFunctionCode);
+            Assert.AreEqual("B", dut.RegisterCode);
+        }
+    }
+}
