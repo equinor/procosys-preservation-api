ff78b05fc9625a7cdccf79076464f7d909ca2cfb
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-23 11:53:38 +0100

    UpdateRequirementsCommand, Validator and Handler

---
+++ src/Equinor.Procosys.Preservation.Command/TagFunctionCommands/UpdateRequirements/UpdateRequirementsCommandHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.TagFunctionAggregate;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Command.TagFunctionCommands.UpdateRequirements
+{
+    public class UpdateRequirementsCommandHandler : IRequestHandler<UpdateRequirementsCommand, Result<Unit>>
+    {
+        private readonly ITagFunctionRepository _tagFunctionRepository;
+        private readonly IRequirementTypeRepository _requirementTypeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPlantProvider _plantProvider;
+
+        public UpdateRequirementsCommandHandler(
+            ITagFunctionRepository tagFunctionRepository,
+            IRequirementTypeRepository requirementTypeRepository,
+            IUnitOfWork unitOfWork,
+            IPlantProvider plantProvider)
+        {
+            _tagFunctionRepository = tagFunctionRepository;
+            _requirementTypeRepository = requirementTypeRepository;
+            _unitOfWork = unitOfWork;
+            _plantProvider = plantProvider;
+        }
+
+        public async Task<Result<Unit>> Handle(UpdateRequirementsCommand request, CancellationToken cancellationToken)
+        {
+            var tagFunction = await _tagFunctionRepository.GetByNameAsync(request.TagFunctionCode, request.RegisterCode);
+
+            if (tagFunction == null)
+            {
+                // todo Need to get TagFunction Description from Main
+                tagFunction = new TagFunction(_plantProvider.Plant, request.TagFunctionCode, "ToDo", request.RegisterCode);
+                _tagFunctionRepository.Add(tagFunction);
+            }
+            else
+            {
+                RemoveChangedOrRemovedRequirements(tagFunction, request.Requirements.ToList());
+            }
+
+            var reqDefIds = request.Requirements.Select(r => r.RequirementDefinitionId).ToList();
+            var reqDefs =
+                await _requirementTypeRepository.GetRequirementDefinitionsByIdsAsync(reqDefIds);
+
+            foreach (var requirement in request.Requirements)
+            {
+                if (tagFunction.Requirements.All(r => r.RequirementDefinitionId != requirement.RequirementDefinitionId))
+                {
+                    var reqDef = reqDefs.Single(rd => rd.Id == requirement.RequirementDefinitionId);
+
+                    tagFunction.AddRequirement(new TagFunctionRequirement(_plantProvider.Plant, requirement.IntervalWeeks,
+                        reqDef));
+                }
+            }
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new SuccessResult<Unit>(Unit.Value);
+        }
+
+        private void RemoveChangedOrRemovedRequirements(TagFunction existingTagFunction, IList<Requirement> updatedRequirements)
+        {
+            var tagFunctionRequirements = existingTagFunction.Requirements;
+
+            for (var i = tagFunctionRequirements.Count-1; i >= 0; i++)
+            {
+                var existingRequirement = tagFunctionRequirements.ElementAt(i);
+                var updatedRequirement = updatedRequirements.FirstOrDefault(r =>
+                    r.RequirementDefinitionId == existingRequirement.RequirementDefinitionId);
+
+                if (updatedRequirement == null || existingRequirement.IntervalWeeks != updatedRequirement.IntervalWeeks)
+                {
+                    existingTagFunction.RemoveRequirement(existingRequirement);
+                }
+            }
+        }
+    }
+}
--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/TagFunctionAggregate/ITagFunctionRepository.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/TagFunctionAggregate/ITagFunctionRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace Equinor.Procosys.Preservation.Domain.AggregateModels.TagFunctionAggregate
+﻿using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Preservation.Domain.AggregateModels.TagFunctionAggregate
 {
     public interface ITagFunctionRepository : IRepository<TagFunction>
     {
+        Task<TagFunction> GetByNameAsync(string tagFunctionCode, string registerCode);
     }
 }
