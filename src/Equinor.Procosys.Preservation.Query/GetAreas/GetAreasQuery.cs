b4cacc2c49e96987513c82f0717ef4e953413e0c
Lykke Larsen <norl@statoil.com>
2020-03-30 15:22:24 +0200

    add plant header to Areas controller

--- src/Equinor.Procosys.Preservation.Query/GetAreas/GetAreasQuery.cs
+++ src/Equinor.Procosys.Preservation.Query/GetAreas/GetAreasQuery.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MediatR;
 using ServiceResult;
 
@@ -6,5 +7,8 @@ namespace Equinor.Procosys.Preservation.Query.GetAreas
 {
     public class GetAreasQuery : IRequest<Result<List<AreaDto>>>
     {
+        public GetAreasQuery(string plant) => Plant = plant ?? throw new ArgumentNullException(nameof(plant));
+
+        public string Plant { get; }
     }
 }
