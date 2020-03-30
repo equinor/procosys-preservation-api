8e72f49e283352b8a262e6619df3ae4bfe8368bb
Lykke Larsen <norl@statoil.com>
2020-03-30 16:53:47 +0200

    add plant header to Responsibles controller

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/Responsibles/ResponsiblesController.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Responsibles/ResponsiblesController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.Query.ResponsibleAggregate;
+using Equinor.Procosys.Preservation.WebApi.Misc;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ServiceResult.ApiExtensions;
@@ -16,9 +19,13 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.Responsible
         public ResponsiblesController(IMediator mediator) => _mediator = mediator;
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ResponsibleDto>>> GetAllResponsibles()
+        public async Task<ActionResult<IEnumerable<ResponsibleDto>>> GetAllResponsibles(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant)
         {
-            var result = await _mediator.Send(new GetAllResponsiblesQuery());
+            var result = await _mediator.Send(new GetAllResponsiblesQuery(plant));
             return this.FromResult(result);
         }
     }
