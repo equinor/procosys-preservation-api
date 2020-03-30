4261fa9fd5065eab8c2d058a42f2bbdd97a16dac
Lykke Larsen <norl@statoil.com>
2020-03-30 16:46:17 +0200

    add plant header to RequirementTypes controller

--- src/Equinor.Procosys.Preservation.Query/RequirementTypeAggregate/GetAllRequirementTypesQuery.cs
+++ src/Equinor.Procosys.Preservation.Query/RequirementTypeAggregate/GetAllRequirementTypesQuery.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MediatR;
 using ServiceResult;
 
@@ -6,8 +7,13 @@ namespace Equinor.Procosys.Preservation.Query.RequirementTypeAggregate
 {
     public class GetAllRequirementTypesQuery : IRequest<Result<IEnumerable<RequirementTypeDto>>>
     {
-        public GetAllRequirementTypesQuery(bool includeVoided) => IncludeVoided = includeVoided;
+        public GetAllRequirementTypesQuery(string plant, bool includeVoided)
+        {
+            Plant = plant ?? throw new ArgumentNullException(nameof(plant));
+            IncludeVoided = includeVoided;
+        }
 
+        public string Plant { get; }
         public bool IncludeVoided { get; }
     }
 }
--- src/Equinor.Procosys.Preservation.Query/RequirementTypeAggregate/GetRequirementTypeByIdQuery.cs
+++ src/Equinor.Procosys.Preservation.Query/RequirementTypeAggregate/GetRequirementTypeByIdQuery.cs
@@ -1,12 +1,18 @@
-﻿using MediatR;
+﻿using System;
+using MediatR;
 using ServiceResult;
 
 namespace Equinor.Procosys.Preservation.Query.RequirementTypeAggregate
 {
     public class GetRequirementTypeByIdQuery : IRequest<Result<RequirementTypeDto>>
     {
-        public GetRequirementTypeByIdQuery(int id) => Id = id;
+        public GetRequirementTypeByIdQuery(string plant, int id)
+        {
+            Plant = plant ?? throw new ArgumentNullException(nameof(plant));
+            Id = id;
+        }
 
+        public string Plant { get; }
         public int Id { get; }
     }
 }
