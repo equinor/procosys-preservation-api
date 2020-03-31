b321b05fc7241003d54307b440b01a9ad67581a6
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-03-31 16:36:04 +0200

    ICacheManager

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Caching/CacheManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Caching
+{
+    public class CacheManager : ICacheManager
+    {
+        readonly IMemoryCache _cache;
+
+        public CacheManager() => _cache = new MemoryCache(new MemoryCacheOptions());
+
+        public T Get<T>(string key) where T : class => _cache.Get(key) as T;
+
+        public T GetOrCreate<T>(string key, Func<T> fetch, CacheDuration duration, long expiration) where T : class
+        {
+            var instance = Get<T>(key);
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            instance = fetch.Invoke();
+            Add(key, instance, duration, expiration);
+            return instance;
+        }
+
+        private void Add<T>(string key, T instance, CacheDuration duration, long expiration) where T : class
+        {
+            if (instance == null)
+            {
+                return;
+            }
+
+            _cache.Set(key, instance, DateTime.UtcNow.Add(GetExpirationTime(duration, expiration)));
+        }
+
+        static TimeSpan GetExpirationTime(CacheDuration duration, long expiration)
+        {
+            switch (duration)
+            {
+                case CacheDuration.Hours:
+                    return TimeSpan.FromHours(expiration);
+                case CacheDuration.Minutes:
+                    return TimeSpan.FromMinutes(expiration);
+                default:
+                    return TimeSpan.FromSeconds(expiration);
+            }
+        }
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Caching/ICacheManager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Caching
+{
+    public interface ICacheManager
+    {
+        T GetOrCreate<T>(string key, Func<T> fetch, CacheDuration duration, long expiration) where T : class;
+    }
+}
