2f826fecc02770286c06572d913689f707464ee0
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-04-01 14:14:15 +0200

    add plant header to Areas controller

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/Areas/AreasController.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Areas/AreasController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.Query.GetAreas;
+using Equinor.Procosys.Preservation.WebApi.Misc;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ServiceResult.ApiExtensions;
@@ -16,7 +19,11 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.Areas
         public AreasController(IMediator mediator) => _mediator = mediator;
 
         [HttpGet]
-        public async Task<ActionResult<List<AreaDto>>> GetAreas()
+        public async Task<ActionResult<List<AreaDto>>> GetAreas(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant)
         {
             var result = await _mediator.Send(new GetAreasQuery());
             return this.FromResult(result);
