ded044783c16f448ba9d62a2c93e66aba144c923
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-04-01 13:27:58 +0200

    71187 Mainapi EndPoint getting permission + cashing (#148)
    * IPermissionApiService.GetPermissionsAsync
    
    * ICacheManager
    
    * Bindings
    
    * IPermissionService.GetPermissionsForCurrentUserAsync
    
    * Tests
    
    * minor
    
    * codereview
    
    * Take oid as inparam to IPermissionService.GetPermissionsForUserOid

---
+++ src/Equinor.Procosys.Preservation.WebApi/Services/IPermissionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Preservation.WebApi.Services
+{
+    public interface IPermissionService
+    {
+        Task<IList<string>> GetPermissionsForUserOid(Guid userOid);
+    }
+}
