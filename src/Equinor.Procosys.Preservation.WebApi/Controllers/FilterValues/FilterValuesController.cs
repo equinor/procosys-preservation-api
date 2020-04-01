7b4e9560d957d9a36fc289465e88f265b87e4085
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-04-01 14:21:52 +0200

    add plant header to FilterValues controller

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/FilterValues/FilterValuesController.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/FilterValues/FilterValuesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.Query.GetUniqueTagAreas;
 using Equinor.Procosys.Preservation.Query.GetUniqueTagDisciplines;
 using Equinor.Procosys.Preservation.Query.GetUniqueTagFunctions;
@@ -7,6 +9,7 @@ using Equinor.Procosys.Preservation.Query.GetUniqueTagJourneys;
 using Equinor.Procosys.Preservation.Query.GetUniqueTagModes;
 using Equinor.Procosys.Preservation.Query.GetUniqueTagRequirementTypes;
 using Equinor.Procosys.Preservation.Query.GetUniqueTagResponsibles;
+using Equinor.Procosys.Preservation.WebApi.Misc;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ServiceResult.ApiExtensions;
@@ -22,49 +25,84 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.FilterValues
         public FilterValuesController(IMediator mediator) => _mediator = mediator;
 
         [HttpGet("RequirementTypes")]
-        public async Task<ActionResult<List<RequirementTypeDto>>> GetRequirementTypes([FromQuery] string projectName)
+        public async Task<ActionResult<List<RequirementTypeDto>>> GetRequirementTypes(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagRequirementTypesQuery(projectName));
             return this.FromResult(result);
         }
 
         [HttpGet("Responsibles")]
-        public async Task<ActionResult<List<ResponsibleDto>>> GetResponsibles([FromQuery] string projectName)
+        public async Task<ActionResult<List<ResponsibleDto>>> GetResponsibles(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagResponsiblesQuery(projectName));
             return this.FromResult(result);
         }
 
         [HttpGet("Journeys")]
-        public async Task<ActionResult<List<JourneyDto>>> GetJourneys([FromQuery] string projectName)
+        public async Task<ActionResult<List<JourneyDto>>> GetJourneys(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagJourneysQuery(projectName));
             return this.FromResult(result);
         }
 
         [HttpGet("Modes")]
-        public async Task<ActionResult<List<ModeDto>>> GetModes([FromQuery] string projectName)
+        public async Task<ActionResult<List<ModeDto>>> GetModes(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagModesQuery(projectName));
             return this.FromResult(result);
         }
 
         [HttpGet("Areas")]
-        public async Task<ActionResult<List<AreaDto>>> GetAreas([FromQuery] string projectName)
+        public async Task<ActionResult<List<AreaDto>>> GetAreas(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagAreasQuery(projectName));
             return this.FromResult(result);
         }
 
         [HttpGet("TagFunctions")]
-        public async Task<ActionResult<List<TagFunctionCodeDto>>> GetTagFunctions([FromQuery] string projectName)
+        public async Task<ActionResult<List<TagFunctionCodeDto>>> GetTagFunctions(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagFunctionsQuery(projectName));
             return this.FromResult(result);
         }
 
         [HttpGet("Disciplines")]
-        public async Task<ActionResult<List<DisciplineDto>>> GetTagDisciplines([FromQuery] string projectName)
+        public async Task<ActionResult<List<DisciplineDto>>> GetTagDisciplines(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(Constants.Plant.MaxLength, MinimumLength = Constants.Plant.MinLength)]
+            string plant,
+            [FromQuery] string projectName)
         {
             var result = await _mediator.Send(new GetUniqueTagDisciplinesQuery(projectName));
             return this.FromResult(result);
