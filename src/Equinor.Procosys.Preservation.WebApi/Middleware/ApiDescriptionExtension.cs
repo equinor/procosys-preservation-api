7aa49a7647544c7cb00f2bea163589e2f67acb67
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-04-02 08:55:24 +0200

    Created an IOperationFilter AddRoleDocumentation which generates documentation in swagger: Created doc is for each endpoint with Autorize attr. Doc say: 401: User is not authenticated 403: User does not have the required permissions (PermA | PermB) & (PermC)

---
+++ src/Equinor.Procosys.Preservation.WebApi/Middleware/ApiDescriptionExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Equinor.Procosys.Preservation.WebApi.Middleware
+{
+    public static class ApiDescriptionExtension
+    {
+        public static AuthorizeAttribute[] GetAuthorizeAttributes(this ApiDescription apiDescription)
+        {
+            var hasAuthAttribute = apiDescription.ActionDescriptor.EndpointMetadata
+                //.Select(filterInfo => filterInfo.Instance)
+                .Where(i => i.GetType() == typeof(AuthorizeAttribute) || i.GetType().BaseType == typeof(AuthorizeAttribute))
+                .Cast<AuthorizeAttribute>();
+
+            return hasAuthAttribute as AuthorizeAttribute[] ?? hasAuthAttribute.ToArray();
+        }
+    }
+}
