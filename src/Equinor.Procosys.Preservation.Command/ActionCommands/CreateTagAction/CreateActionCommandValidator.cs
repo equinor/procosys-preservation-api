c04dddb409aa20f943d1d4a47b996865fdb52a1a
Lykke Larsen <norl@statoil.com>
2020-04-06 13:48:31 +0200

    add validators

---
+++ src/Equinor.Procosys.Preservation.Command/ActionCommands/CreateTagAction/CreateActionCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.Validators.ProjectValidators;
+using Equinor.Procosys.Preservation.Command.Validators.TagValidators;
+using FluentValidation;
+
+namespace Equinor.Procosys.Preservation.Command.ActionCommands.CreateTagAction
+{
+    public class CreateActionCommandValidator : AbstractValidator<CreateActionCommand>
+    {
+        public CreateActionCommandValidator(
+            IProjectValidator projectValidator,
+            ITagValidator tagValidator)
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(command => command)
+                .MustAsync((command, token) => NotBeAClosedProjectForTagAsync(command.TagId, token))
+                .WithMessage(command => $"Project for tag is closed! Tag={command.TagId}")
+                .MustAsync((command, token) => BeAnExistingTagAsync(command.TagId, token))
+                .WithMessage((x, id) => $"Tag doesn't exists! Tag={id}")
+                .MustAsync((command, token) => NotBeAVoidedTagAsync(command.TagId, token))
+                .WithMessage((x, id) => $"Tag is voided! Tag={id}");
+
+            async Task<bool> NotBeAClosedProjectForTagAsync(int tagId, CancellationToken token)
+                => !await projectValidator.IsClosedForTagAsync(tagId, token);
+            async Task<bool> BeAnExistingTagAsync(int tagId, CancellationToken token)
+                => await tagValidator.ExistsAsync(tagId, token);
+            async Task<bool> NotBeAVoidedTagAsync(int tagId, CancellationToken token)
+                => !await tagValidator.IsVoidedAsync(tagId, token);
+        }
+    }
+}
