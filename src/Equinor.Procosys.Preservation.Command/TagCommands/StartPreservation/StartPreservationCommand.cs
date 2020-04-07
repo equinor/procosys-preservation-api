ddff56cbd297511d7f815c3d55af5e7a88492280
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-04-07 13:25:10 +0200

    Added ITagRequest to bulk-command. TagId-getter get first TagId in list (assume that all tags in same project). Refactoctored calidators for bulk-command and verify all YagIds in same project

--- src/Equinor.Procosys.Preservation.Command/TagCommands/StartPreservation/StartPreservationCommand.cs
+++ src/Equinor.Procosys.Preservation.Command/TagCommands/StartPreservation/StartPreservationCommand.cs
@@ -1,14 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Equinor.Procosys.Preservation.Domain;
 using MediatR;
 using ServiceResult;
 
 namespace Equinor.Procosys.Preservation.Command.TagCommands.StartPreservation
 {
-    public class StartPreservationCommand : IRequest<Result<Unit>>
+    public class StartPreservationCommand : IRequest<Result<Unit>>, ITagRequest
     {
         public StartPreservationCommand(IEnumerable<int> tagIds)
             => TagIds = tagIds ?? new List<int>();
 
         public IEnumerable<int> TagIds { get; }
+        
+        public int TagId
+        {
+            get
+            {
+                if (!TagIds.Any())
+                {
+                    throw new Exception($"At least 1 {nameof(TagIds)} must be given!");
+                }
+
+                return TagIds.First();
+            }
+        }
     }
 }
