93dd3d6bc4b0eb02731eaedcfed4e7aa9e7853b4
Lykke Larsen <norl@statoil.com>
2020-04-14 17:23:59 +0200

    add tests

---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/ActionCommands/UpdateAction/UpdateActionCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Equinor.Procosys.Preservation.Command.ActionCommands.UpdateAction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.ActionCommands.UpdateAction
+{
+    [TestClass]
+    public class UpdateActionCommandTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldSetProperties()
+        {
+            var dueTimeUtc = new DateTime(2020, 1, 1, 1, 1, 1, DateTimeKind.Utc);
+            var dut = new UpdateActionCommand(2, 1, "Title", "Description", dueTimeUtc);
+
+            Assert.AreEqual(2, dut.TagId);
+            Assert.AreEqual(1, dut.ActionId);
+            Assert.AreEqual("Title", dut.Title);
+            Assert.AreEqual("Description", dut.Description);
+            Assert.AreEqual(dueTimeUtc, dut.DueTimeUtc);
+        }
+    }
+}
