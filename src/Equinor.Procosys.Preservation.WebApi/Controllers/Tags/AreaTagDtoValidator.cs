d351788d3d706d17a0b1b522ef78e9bc66c002b3
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-04-23 14:26:28 +0200

    Bug1: Endpoint didn't have plant as required param Bug2: Query didn't have IProjectRequest interface (which cause security on Project) Improvement: Validator -> swagger show which params are required

---
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Tags/AreaTagDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Equinor.Procosys.Preservation.WebApi.Controllers.Tags
+{
+    public class AreaTagDtoValidator : AbstractValidator<AreaTagDto>
+    {
+        public AreaTagDtoValidator()
+        {
+            RuleFor(x => x).NotNull();
+
+            RuleFor(x => x.ProjectName).NotNull();
+
+            RuleFor(x => x.AreaTagType).NotNull();
+
+            RuleFor(x => x.DisciplineCode).NotNull();
+        }
+    }
+}
