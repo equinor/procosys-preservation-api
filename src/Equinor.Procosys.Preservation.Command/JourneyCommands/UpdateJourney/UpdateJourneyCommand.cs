095ece036dd88369feaf4be9bb82ccad7f0e393b
Demissie Aredo <deare@equinor.com>
2020-04-23 12:16:20 +0200

    Removed blank line

--- src/Equinor.Procosys.Preservation.Command/JourneyCommands/UpdateJourney/UpdateJourneyCommand.cs
+++ src/Equinor.Procosys.Preservation.Command/JourneyCommands/UpdateJourney/UpdateJourneyCommand.cs
@@ -12,6 +12,5 @@ namespace Equinor.Procosys.Preservation.Command.JourneyCommands.UpdateJourney
         }
         public int JourneyId { get; set;}
         public string Title { get; set; }
-
     }
 }
