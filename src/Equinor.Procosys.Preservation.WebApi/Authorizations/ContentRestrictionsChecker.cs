7f0f51e52ab90b2e9e09e070473283cc3f67f69e
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-04-23 08:07:27 +0200

    minor

--- src/Equinor.Procosys.Preservation.WebApi/Authorizations/ContentRestrictionsChecker.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Authorizations/ContentRestrictionsChecker.cs
@@ -13,8 +13,8 @@ namespace Equinor.Procosys.Preservation.WebApi.Authorizations
         public bool HasCurrentUserAnyRestrictions()
         {
             var userDataClaimWithContentRestriction = _currentUserProvider.CurrentUser.Claims.GetContentRestrictionClaims();
-            return userDataClaimWithContentRestriction.Count > 1 && userDataClaimWithContentRestriction.First().Value !=
-                   ClaimsTransformation.NoRestrictions;
+            return userDataClaimWithContentRestriction.Count > 1 &&
+                   userDataClaimWithContentRestriction.First().Value != ClaimsTransformation.NoRestrictions;
         }
 
         public bool HasCurrentUserExplicitAccessToContent(string responsibleCode)
