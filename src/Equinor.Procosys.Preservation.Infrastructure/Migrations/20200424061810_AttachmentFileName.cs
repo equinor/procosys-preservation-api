5c22467ee22ed9cab699485158ed57062de925ff
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-04-24 08:18:36 +0200

    AttachmentFileName migration

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200424061810_AttachmentFileName.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class AttachmentFileName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "Attachments",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "Attachments");
+        }
+    }
+}
