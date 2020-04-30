dad2f35ee98decae7577d4147bc8d1b66ff0496b
Demissie Bediye Aredo <deare@equinor.com>
2020-04-30 08:18:02 +0200

    73348 unvoid tag endpoint (#191)
    * Unvoid tag endpoint
    
    * Added tests
    
    * Fixed PR comments

---
+++ src/Equinor.Procosys.Preservation.Command/TagCommands/UnvoidTag/UnvoidTagCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.Validators.ProjectValidators;
+using Equinor.Procosys.Preservation.Command.Validators.TagValidators;
+using FluentValidation;
+
+namespace Equinor.Procosys.Preservation.Command.TagCommands.UnvoidTag
+{
+    public class UnvoidTagCommandValidator : AbstractValidator<UnvoidTagCommand>
+    {
+        public UnvoidTagCommandValidator(
+            IProjectValidator projectValidator,
+            ITagValidator tagValidator)
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(command => command)
+                .MustAsync((command, token) => NotBeAClosedProjectForTagAsync(command.TagId, token))
+                .WithMessage(command => $"Project for tag is closed! Tag={command.TagId}")
+                .MustAsync((command, token) => BeAnExistingTagAsync(command.TagId, token))
+                .WithMessage(command => $"Tag doesn't exist! Tag={command.TagId}")
+                .MustAsync((command, token) => BeAVoidedTagAsync(command.TagId, token))
+                .WithMessage(command => $"Tag is not voided! Tag={command.TagId}");
+
+            async Task<bool> NotBeAClosedProjectForTagAsync(int tagId, CancellationToken token)
+                => !await projectValidator.IsClosedForTagAsync(tagId, token);
+            async Task<bool> BeAnExistingTagAsync(int tagId, CancellationToken token)
+                => await tagValidator.ExistsAsync(tagId, token);
+            async Task<bool> BeAVoidedTagAsync(int tagId, CancellationToken token)
+                => await tagValidator.IsVoidedAsync(tagId, token);
+        }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/TagCommands/UnvoidTag/UnvoidTagCommandHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.TagCommands.UnvoidTag;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.TagCommands.UnvoidTag
+{
+    [TestClass]
+    public class UnvoidTagCommandHandlerTests : CommandHandlerTestsBase
+    {
+        private Tag _tag;
+        private UnvoidTagCommand _command;
+        private UnvoidTagCommandHandler _dut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var tagId = 2;
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+
+            var stepMock = new Mock<Step>();
+            stepMock.SetupGet(s => s.Plant).Returns(TestPlant);
+
+            var rdMock = new Mock<RequirementDefinition>();
+            rdMock.SetupGet(rd => rd.Plant).Returns(TestPlant);
+
+            var requirement = new TagRequirement(TestPlant, 2, rdMock.Object);
+            _tag = new Tag(TestPlant, TagType.Standard, "", "", stepMock.Object,
+                new List<TagRequirement> {requirement});
+
+            projectRepositoryMock
+                .Setup(r => r.GetTagByTagIdAsync(tagId))
+                .Returns(Task.FromResult(_tag));
+
+            _command = new UnvoidTagCommand(tagId);
+
+            _dut = new UnvoidTagCommandHandler(
+                projectRepositoryMock.Object,
+                UnitOfWorkMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandlingUnvoidTagCommand_ShouldUnvoidTag()
+        {
+            // Arrange
+            _tag.Void();
+            Assert.IsTrue(_tag.IsVoided);
+
+            // Act
+            var result = await _dut.Handle(_command, default);
+
+            Assert.AreEqual(0, result.Errors.Count);
+            Assert.IsFalse(_tag.IsVoided);
+        }
+
+        [TestMethod]
+        public async Task HandlingUnvoidTagCommand_ShouldSave()
+        {
+            await _dut.Handle(_command, default);
+            UnitOfWorkMock.Verify(u => u.SaveChangesAsync(default), Times.Once);
+        }
+    }
+}
