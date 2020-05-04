c049460428ebe9247376d63dde9fc585a21ca6a5
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2020-05-04 11:35:08 +0200

    K8s deployment (#195)
    * Added deploy to k8s in pipeline. Added possibility to read key vault configuration on startup.
    
    * Fixed wrong names in pipeline
    
    * Added missing NuGet reference in project file

--- src/Equinor.Procosys.Preservation.WebApi/Program.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Equinor.Procosys.Preservation.WebApi.Misc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Equinor.Procosys.Preservation.WebApi
@@ -13,6 +15,15 @@ namespace Equinor.Procosys.Preservation.WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var kvSettings = new KeyVaultSettings();
+                    config.Build().GetSection("KeyVault").Bind(kvSettings);
+                    if (kvSettings.Enabled)
+                    {
+                        config.AddAzureKeyVault(kvSettings.Uri, kvSettings.ClientId, kvSettings.ClientSecret);
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseKestrel(options => options.AddServerHeader = false);
