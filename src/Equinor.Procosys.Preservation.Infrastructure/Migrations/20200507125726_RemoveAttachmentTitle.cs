9fbc09d3624867d83dcb09a13c0bf27d4f7723d7
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-07 14:58:08 +0200

    migration RemoveAttachmentTitle

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200507125726_RemoveAttachmentTitle.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class RemoveAttachmentTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Attachments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Attachments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
