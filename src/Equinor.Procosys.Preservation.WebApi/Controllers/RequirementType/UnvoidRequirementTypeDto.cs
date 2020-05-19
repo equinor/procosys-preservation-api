e9c07b99e1d905268c16b8cd0aa247c034e9b2ce
Demissie Bediye Aredo <deare@equinor.com>
2020-05-19 13:27:06 +0200

    73448 API requirement definition endpoints void / unvoid (#235)
    * added void / unvoid endpoints for Requirement Definition
    
    * created tests
    
    * minor fixes
    
    * Fixed comments on PR

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/RequirementType/UnvoidRequirementTypeDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Equinor.Procosys.Preservation.WebApi.Controllers.RequirementType
-{
-    public class UnvoidRequirementTypeDto
-    {
-        public  string RowVersion { get; set; }
-    }
-}
