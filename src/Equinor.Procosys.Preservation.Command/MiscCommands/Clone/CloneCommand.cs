cceb71f51d59edaf8b75127f9752e23eb514aab8
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-27 15:41:35 +0200

    Clone Modes

---
+++ src/Equinor.Procosys.Preservation.Command/MiscCommands/Clone/CloneCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Command.MiscCommands.Clone
+{
+    public class CloneCommand : IRequest<Result<Unit>>
+    {
+        public CloneCommand(string sourcePlant) => SourcePlant = sourcePlant;
+
+        public string SourcePlant { get; }
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.Command/MiscCommands/Clone/CloneCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ModeAggregate;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Command.MiscCommands.Clone
+{
+    public class CloneCommandHandler : IRequestHandler<CloneCommand, Result<Unit>>
+    {
+        private readonly IPlantProvider _plantProvider;
+        private readonly IModeRepository _modeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CloneCommandHandler(IPlantProvider plantProvider, IModeRepository modeRepository, IUnitOfWork unitOfWork)
+        {
+            _plantProvider = plantProvider;
+            _modeRepository = modeRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<Unit>> Handle(CloneCommand request, CancellationToken cancellationToken)
+        {
+            var targetPlant = _plantProvider.Plant;
+
+            await CloneModes(request.SourcePlant, targetPlant);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return new SuccessResult<Unit>(Unit.Value);
+        }
+
+        private async Task CloneModes(string sourcePlant, string targetPlant)
+        {
+            _plantProvider.SetTemporaryPlant(sourcePlant);
+            var sourceModes = await _modeRepository.GetAllAsync();
+            _plantProvider.ReleaseTemporaryPlant();
+
+            var targetModes = await _modeRepository.GetAllAsync();
+
+            foreach (var sourceMode in sourceModes)
+            {
+                if (targetModes.SingleOrDefault(t => t.Title == sourceMode.Title) == null)
+                {
+                    var targetMode = new Mode(targetPlant, sourceMode.Title);
+                    _modeRepository.Add(targetMode);
+                }
+            }
+        }
+    }
+}
