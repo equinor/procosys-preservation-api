05bd5acc3eb15256b3fd3a5c4103309716942b27
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-05-27 11:51:38 +0200

    Endpoints listing content of caches for user (#247)

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/Misc/CacheController.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Misc/CacheController.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.MainApi.Plant;
 using Equinor.Procosys.Preservation.WebApi.Caches;
@@ -24,7 +26,7 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.Misc
         }
 
         [Authorize]
-        [HttpGet("Clear")]
+        [HttpPut("Clear")]
         public void Clear(
             [FromHeader(Name = PlantProvider.PlantHeader)]
             [Required]
@@ -34,6 +36,41 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.Misc
             var currentUserOid = _currentUserProvider.GetCurrentUserOid();
             _plantCache.Clear(currentUserOid);
             _permissionCache.ClearAll(plant, currentUserOid);
+        }
+
+        [Authorize]
+        [HttpGet("Permissions")]
+        public async Task<IList<string>> GetPermissions(
+            [FromHeader(Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(PlantEntityBase.PlantLengthMax, MinimumLength = PlantEntityBase.PlantLengthMin)]
+            string plant)
+        {
+            var currentUserOid = _currentUserProvider.GetCurrentUserOid();
+            var permissions = await _permissionCache.GetPermissionsForUserAsync(plant, currentUserOid);
+            return permissions;
+        }
+
+        [Authorize]
+        [HttpGet("Projects")]
+        public async Task<IList<string>> GetProjects(
+            [FromHeader(Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(PlantEntityBase.PlantLengthMax, MinimumLength = PlantEntityBase.PlantLengthMin)]
+            string plant)
+        {
+            var currentUserOid = _currentUserProvider.GetCurrentUserOid();
+            var projects = await _permissionCache.GetProjectNamesForUserOidAsync(plant, currentUserOid);
+            return projects;
+        }
+
+        [Authorize]
+        [HttpGet("Plants")]
+        public async Task<IList<string>> GetPlants()
+        {
+            var currentUserOid = _currentUserProvider.GetCurrentUserOid();
+            var plants = await _plantCache.GetPlantIdsForUserOidAsync(currentUserOid);
+            return plants;
         }
     }
 }
