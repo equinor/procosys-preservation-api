aea286f81542250c794fe458aea262e8b412f077
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-28 17:06:05 +0200

    CloneCommandHandlerTests testing Clone of Modes

---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/CloneCommandHandlerTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.MiscCommands.Clone;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ModeAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ResponsibleAggregate;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.TagFunctionAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.MiscCommands.Clone
+{
+    [TestClass]
+    public class CloneCommandHandlerTests : CommandHandlerTestsBase
+    {
+        private readonly string _sourcePlant = "SOURCE";
+        private CloneCommand _command;
+        private CloneCommandHandler _dut;
+        private readonly PlantProvider _plantProvider = new PlantProvider(TestPlant);
+        private ModeRepository _modeRepository;
+        private readonly List<Mode> _sourceModes = new List<Mode>();
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _modeRepository = new ModeRepository(_plantProvider, _sourceModes);
+            _sourceModes.Add(new Mode(TestPlant, "ModeA"));
+
+            var responsibleRepositoryMock = new Mock<IResponsibleRepository>();
+            responsibleRepositoryMock
+                .Setup(r => r.GetAllAsync())
+                .Returns(Task.FromResult(new List<Responsible>()));
+            
+            var requirementTypeRepositoryMock = new Mock<IRequirementTypeRepository>();
+            requirementTypeRepositoryMock
+                .Setup(r => r.GetAllAsync())
+                .Returns(Task.FromResult(new List<RequirementType>()));
+
+            var tagFunctionRepositoryMock = new Mock<ITagFunctionRepository>();
+            tagFunctionRepositoryMock
+                .Setup(r => r.GetAllAsync())
+                .Returns(Task.FromResult(new List<TagFunction>()));
+
+            _command = new CloneCommand(_sourcePlant, TestPlant);
+            _dut = new CloneCommandHandler(
+                _plantProvider,
+                UnitOfWorkMock.Object,
+                _modeRepository,
+                responsibleRepositoryMock.Object,
+                requirementTypeRepositoryMock.Object,
+                tagFunctionRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandlingCloneCommand_ShouldCloneModes()
+        {
+            // Act
+            await _dut.Handle(_command, default);
+
+            // Assert
+            var modeAdded = _modeRepository.GetAllAsync().Result.Single();
+            Assert.IsNotNull(modeAdded);
+            Assert.AreEqual(TestPlant, modeAdded.Plant);
+            Assert.AreEqual(_sourceModes.First().Title, modeAdded.Title);
+        }
+
+        [TestMethod]
+        public async Task HandlingCloneCommand_ShouldSave()
+        {
+            // Act
+            await _dut.Handle(_command, default);
+
+            // Assert
+            UnitOfWorkMock.Verify(u => u.SaveChangesAsync(default), Times.Once);
+        }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/ModeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.ModeAggregate;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.MiscCommands.Clone
+{
+    internal class ModeRepository : IModeRepository
+    {
+        private readonly PlantProvider _plantProvider;
+        private readonly List<Mode> _sourceModes;
+        private readonly List<Mode> _targetModes = new List<Mode>();
+        private readonly string _targetPlant;
+
+        public ModeRepository(PlantProvider plantProvider, List<Mode> sourceModes)
+        {
+            _plantProvider = plantProvider;
+            _sourceModes = sourceModes;
+            _targetPlant = plantProvider.Plant;
+        }
+
+        public void Add(Mode item)
+        {
+            if (_plantProvider.Plant == _targetPlant)
+            {
+                _targetModes.Add(item);
+            }
+            else
+            {
+                _sourceModes.Add(item);
+            }
+        }
+
+        public Task<bool> Exists(int id) => throw new NotImplementedException();
+
+        public Task<Mode> GetByIdAsync(int id) => throw new NotImplementedException();
+
+        public Task<List<Mode>> GetByIdsAsync(IEnumerable<int> id) => throw new NotImplementedException();
+
+        public void Remove(Mode entity) => throw new NotImplementedException();
+
+        public Task<List<Mode>> GetAllAsync()
+        {
+            if (_plantProvider.Plant == _targetPlant)
+            {
+                return Task.FromResult(_targetModes);
+            }
+            return Task.FromResult(_sourceModes);
+        }
+    }
+}
