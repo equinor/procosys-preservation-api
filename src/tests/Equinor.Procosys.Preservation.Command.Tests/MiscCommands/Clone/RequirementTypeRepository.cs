af2b2f85b1d68eb21801ec190d2f46f06b181243
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-05-29 15:29:33 +0200

    Refactor to generic TestRepo

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/RequirementTypeRepository.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/RequirementTypeRepository.cs
@@ -7,44 +7,16 @@ using Equinor.Procosys.Preservation.Test.Common.ExtensionMethods;
 
 namespace Equinor.Procosys.Preservation.Command.Tests.MiscCommands.Clone
 {
-    internal class RequirementTypeRepository : IRequirementTypeRepository
+    internal class RequirementTypeRepository : TestRepository<RequirementType>, IRequirementTypeRepository
     {
-        private readonly PlantProvider _plantProvider;
-        private readonly List<RequirementType> _sourceRequirementTypes;
-        private readonly List<RequirementType> _targetRequirementTypes = new List<RequirementType>();
-        private readonly string _targetPlant;
-
         public RequirementTypeRepository(PlantProvider plantProvider, List<RequirementType> sourceRequirementTypes)
+            :base(plantProvider, sourceRequirementTypes)
         {
-            _plantProvider = plantProvider;
-            _sourceRequirementTypes = sourceRequirementTypes;
-            _targetPlant = plantProvider.Plant;
         }
-
-        public void Add(RequirementType item)
-        {
-            if (_plantProvider.Plant == _targetPlant)
-            {
-                _targetRequirementTypes.Add(item);
-            }
-            else
-            {
-                _sourceRequirementTypes.Add(item);
-            }
-        }
-
-        public Task<List<RequirementType>> GetAllAsync()
-        {
-            if (_plantProvider.Plant == _targetPlant)
-            {
-                return Task.FromResult(_targetRequirementTypes);
-            }
-            return Task.FromResult(_sourceRequirementTypes);
-        }
-
+        
         public void Save()
         {
-            var requirementDefinitions = _targetRequirementTypes
+            var requirementDefinitions = _targetItems
                 .SelectMany(rt => rt.RequirementDefinitions)
                 .ToList();
             var nextId = requirementDefinitions.Count;
@@ -54,12 +26,8 @@ namespace Equinor.Procosys.Preservation.Command.Tests.MiscCommands.Clone
             }
         }
 
-        public Task<bool> Exists(int id) => throw new NotImplementedException();
-        public Task<RequirementType> GetByIdAsync(int id) => throw new NotImplementedException();
-        public Task<List<RequirementType>> GetByIdsAsync(IEnumerable<int> id) => throw new NotImplementedException();
-        public void Remove(RequirementType entity) => throw new NotImplementedException();
-        public Task<RequirementType> GetByCodeAsync(string RequirementTypeCode) => throw new NotImplementedException();
         public Task<RequirementDefinition> GetRequirementDefinitionByIdAsync(int requirementDefinitionId) => throw new NotImplementedException();
+
         public Task<List<RequirementDefinition>> GetRequirementDefinitionsByIdsAsync(IList<int> requirementDefinitionIds) => throw new NotImplementedException();
     }
 }
--- src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/ResposibleRepository.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/ResposibleRepository.cs
@@ -5,45 +5,14 @@ using Equinor.Procosys.Preservation.Domain.AggregateModels.ResponsibleAggregate;
 
 namespace Equinor.Procosys.Preservation.Command.Tests.MiscCommands.Clone
 {
-    internal class ResponsibleRepository : IResponsibleRepository
+    internal class ResponsibleRepository : TestRepository<Responsible>, IResponsibleRepository
     {
-        private readonly PlantProvider _plantProvider;
-        private readonly List<Responsible> _sourceResponsibles;
-        private readonly List<Responsible> _targetResponsibles = new List<Responsible>();
-        private readonly string _targetPlant;
 
         public ResponsibleRepository(PlantProvider plantProvider, List<Responsible> sourceResponsibles)
+            :base(plantProvider, sourceResponsibles)
         {
-            _plantProvider = plantProvider;
-            _sourceResponsibles = sourceResponsibles;
-            _targetPlant = plantProvider.Plant;
         }
 
-        public void Add(Responsible item)
-        {
-            if (_plantProvider.Plant == _targetPlant)
-            {
-                _targetResponsibles.Add(item);
-            }
-            else
-            {
-                _sourceResponsibles.Add(item);
-            }
-        }
-
-        public Task<List<Responsible>> GetAllAsync()
-        {
-            if (_plantProvider.Plant == _targetPlant)
-            {
-                return Task.FromResult(_targetResponsibles);
-            }
-            return Task.FromResult(_sourceResponsibles);
-        }
-
-        public Task<bool> Exists(int id) => throw new NotImplementedException();
-        public Task<Responsible> GetByIdAsync(int id) => throw new NotImplementedException();
-        public Task<List<Responsible>> GetByIdsAsync(IEnumerable<int> id) => throw new NotImplementedException();
-        public void Remove(Responsible entity) => throw new NotImplementedException();
         public Task<Responsible> GetByCodeAsync(string responsibleCode) => throw new NotImplementedException();
     }
 }
