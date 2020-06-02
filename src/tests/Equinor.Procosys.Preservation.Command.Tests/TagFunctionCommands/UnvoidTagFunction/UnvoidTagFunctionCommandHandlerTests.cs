907969e56dc890d61de0f438abfca8c5662861d5
Nora Lykke Strøm Larsen <norl@equinor.com>
2020-06-02 15:37:28 +0200

    74245 void unvoid tag function (#259)
    * void tagfunction
    
    * void tag function validator
    
    * typo fix
    
    * tests
    
    * create tag function validator
    
    * unvoid tag functions
    
    * unvoid tag function validator
    
    * unvoid tag function tests
    
    * nitpick
    
    Co-authored-by: Lykke Larsen <norl@statoil.com>

---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/TagFunctionCommands/UnvoidTagFunction/UnvoidTagFunctionCommandHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.TagFunctionCommands.UnvoidTagFunction;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.TagFunctionAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.TagFunctionCommands.UnvoidTagFunction
+{
+    [TestClass]
+    public class UnvoidTagFunctionCommandHandlerTests : CommandHandlerTestsBase
+    {
+        private TagFunction _tagFunction;
+        private UnvoidTagFunctionCommand _command;
+        private UnvoidTagFunctionCommandHandler _dut;
+        private readonly string _rowVersion = "AAAAAAAAABA=";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Arrange
+            var tagFunctionRepositoryMock = new Mock<ITagFunctionRepository>();
+            const string TagFunctionCode = "TFC";
+            const string RegisterCode = "RC";
+
+            _tagFunction = new TagFunction(TestPlant, "ReqDefinitionTitle", "TagFunctionDescription", "MAIN_EQUIP");
+            _tagFunction.Void();
+
+            tagFunctionRepositoryMock.Setup(r => r.GetByCodesAsync(TagFunctionCode, RegisterCode))
+                .Returns(Task.FromResult(_tagFunction));
+
+            _command = new UnvoidTagFunctionCommand(TagFunctionCode, RegisterCode, _rowVersion);
+            _dut = new UnvoidTagFunctionCommandHandler(
+                tagFunctionRepositoryMock.Object,
+                UnitOfWorkMock.Object);
+        }
+
+        [TestMethod]
+        public async Task HandlingUnvoidTagFunctionCommand_ShouldUnvoidTagFunction()
+        {
+            // Arrange
+            Assert.IsTrue(_tagFunction.IsVoided);
+
+            // Act
+            var result = await _dut.Handle(_command, default);
+
+            // Assert
+            Assert.AreEqual(0, result.Errors.Count);
+            Assert.IsFalse(_tagFunction.IsVoided);
+        }
+
+        [TestMethod]
+        public async Task HandlingUnvoidTagFunctionCommand_ShouldSetAndReturnRowVersion()
+        {
+            // Act
+            var result = await _dut.Handle(_command, default);
+
+            // Assert
+            Assert.AreEqual(0, result.Errors.Count);
+            Assert.AreEqual(_rowVersion, result.Data);
+            Assert.AreEqual(_rowVersion, _tagFunction.RowVersion.ConvertToString());
+        }
+
+        [TestMethod]
+        public async Task HandlingUnvoidTagFunctionCommand_ShouldSave()
+        {
+            // Act
+            await _dut.Handle(_command, default);
+
+            // Assert
+            UnitOfWorkMock.Verify(u => u.SaveChangesAsync(default), Times.Once);
+        }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/TagFunctionCommands/UnvoidTagFunction/UnvoidTagFunctionCommandValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.TagFunctionCommands.UnvoidTagFunction;
+using Equinor.Procosys.Preservation.Command.Validators.TagFunctionValidators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.TagFunctionCommands.UnvoidTagFunction
+{
+    [TestClass]
+    public class UnvoidTagFunctionCommandValidatorTests
+    {
+        private Mock<ITagFunctionValidator> _tagFunctionValidatorMock;
+        private UnvoidTagFunctionCommand _command;
+        private UnvoidTagFunctionCommandValidator _dut;
+
+        private readonly string _tagFunctionCode = "TFC";
+
+        [TestInitialize]
+        public void Setup_OkState()
+        {
+            const string RegisterCode = "RC";
+
+            _tagFunctionValidatorMock = new Mock<ITagFunctionValidator>();
+            _tagFunctionValidatorMock.Setup(r => r.ExistsAsync(_tagFunctionCode, default)).Returns(Task.FromResult(true));
+            _tagFunctionValidatorMock.Setup(r => r.IsVoidedAsync(_tagFunctionCode, default))
+                .Returns(Task.FromResult(true));
+
+            _command = new UnvoidTagFunctionCommand(_tagFunctionCode, RegisterCode, null);
+            _dut = new UnvoidTagFunctionCommandValidator(_tagFunctionValidatorMock.Object);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldBeValid_WhenOkState()
+        {
+            var result = _dut.Validate(_command);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenTagFunctionNotExists()
+        {
+            _tagFunctionValidatorMock.Setup(r => r.ExistsAsync(_tagFunctionCode, default)).Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag function doesn't exist!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenTagFunctionIsNotVoided()
+        {
+            _tagFunctionValidatorMock.Setup(r => r.IsVoidedAsync(_tagFunctionCode, default)).Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag function is not voided!"));
+        }
+    }
+}
