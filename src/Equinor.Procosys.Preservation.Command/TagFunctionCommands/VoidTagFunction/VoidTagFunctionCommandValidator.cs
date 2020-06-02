b1d55c3b0079aed4002224c31f7c936bb230fee6
Lykke Larsen <norl@statoil.com>
2020-06-02 13:13:46 +0200

    void tag function validator

---
+++ src/Equinor.Procosys.Preservation.Command/TagFunctionCommands/VoidTagFunction/VoidTagFunctionCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.Validators.TagFunctionValidators;
+using FluentValidation;
+
+namespace Equinor.Procosys.Preservation.Command.TagFunctionCommands.VoidTagFunction
+{
+    public class VoidTagFunctionCommandValidator : AbstractValidator<VoidTagFunctionCommand>
+    {
+        public VoidTagFunctionCommandValidator(ITagFunctionValidator tagFunctionValidator)
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(command => command)
+                .MustAsync((command, token) => BeAnExistingTagFunctionAsync(command.TagFunctionCode, token))
+                .WithMessage(command => $"Tag function doesn't exist! TagFunction={command.TagFunctionCode}")
+                .MustAsync((command, token) => NotBeAVoidedTagFunctionAsync(command.TagFunctionCode, token))
+                .WithMessage(command => $"Tag function is already voided! TagFunction={command.TagFunctionCode}");
+
+            async Task<bool> BeAnExistingTagFunctionAsync(string tagFunctionCode, CancellationToken token)
+                => await tagFunctionValidator.ExistsAsync(tagFunctionCode, token);
+            async Task<bool> NotBeAVoidedTagFunctionAsync(string tagFunctionCode, CancellationToken token)
+                => !await tagFunctionValidator.IsVoidedAsync(tagFunctionCode, token);
+        }
+    }
+}
