b3b056e881421e8abf0969c140f96e45a72e1176
Demissie Bediye Aredo <deare@equinor.com>
2020-06-03 13:02:50 +0200

    74282 added ForSupplier column to Modes table (#261)
    * added ForSupplier column to Modes
    
    * Made ForSupplier column public

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200603100929_AddForSupplierInMode.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class AddForSupplierInMode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ForSupplier",
+                table: "Modes",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForSupplier",
+                table: "Modes");
+        }
+    }
+}
