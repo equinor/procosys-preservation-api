d57047d6a58f7effeb2de0c15a7a26ac533f2e67
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-06-04 12:36:25 +0200

    74312 db requirement usage in requirement def (#263)
    * Added RequirementUsage column. Constructor to set it will be in next PR (commented code here)
    
    * config
    
    * migration

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200604084929_RequirementUsage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class RequirementUsage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Usage",
+                table: "RequirementDefinitions",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "ForAll");
+
+            migrationBuilder.CreateCheckConstraint(
+                name: "constraint_reqdef_check_valid_usage",
+                table: "RequirementDefinitions",
+                sql: "Usage in ('ForAll','ForSuppliersOnly','ForOtherThanSuppliers')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_reqdef_check_valid_usage",
+                table: "RequirementDefinitions");
+
+            migrationBuilder.DropColumn(
+                name: "Usage",
+                table: "RequirementDefinitions");
+        }
+    }
+}
