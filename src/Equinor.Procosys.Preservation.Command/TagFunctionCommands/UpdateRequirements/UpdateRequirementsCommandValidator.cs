9229c38c007e71baccf0349fdc5add491d06b842
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-06-09 10:33:50 +0200

    Update UpdateRequirementsCommandValidator to handle Requirement Usage

--- src/Equinor.Procosys.Preservation.Command/TagFunctionCommands/UpdateRequirements/UpdateRequirementsCommandValidator.cs
+++ src/Equinor.Procosys.Preservation.Command/TagFunctionCommands/UpdateRequirements/UpdateRequirementsCommandValidator.cs
@@ -12,18 +12,23 @@ namespace Equinor.Procosys.Preservation.Command.TagFunctionCommands.UpdateRequir
         public UpdateRequirementsCommandValidator(IRequirementDefinitionValidator requirementDefinitionValidator)
         {
             CascadeMode = CascadeMode.StopOnFirstFailure;
-            
-            RuleFor(command => command.Requirements)
-                .Must(BeUniqueRequirements)
-                .WithMessage("Requirement definitions must be unique!");
 
-            RuleForEach(command => command.Requirements)
-                .MustAsync((_, req, __, token) => BeAnExistingRequirementDefinitionAsync(req, token))
-                .WithMessage((_, req) =>
-                    $"Requirement definition doesn't exists! Requirement={req.RequirementDefinitionId}")
-                .MustAsync((_, req, __, token) => NotBeAVoidedRequirementDefinitionAsync(req, token))
-                .WithMessage((_, req) =>
-                    $"Requirement definition is voided! Requirement={req.RequirementDefinitionId}");
+            When(command => command.Requirements.Any(), () =>
+            {
+                RuleFor(command => command.Requirements)
+                    .Must(BeUniqueRequirements)
+                    .WithMessage("Requirement definitions must be unique!")
+                    .MustAsync((_, requirements, token) => RequirementUsageIsForAllJourneysAsync(requirements, token))
+                    .WithMessage(command => "Requirements must include requirements to be used both for supplier and other than suppliers!");
+
+                RuleForEach(command => command.Requirements)
+                    .MustAsync((_, req, __, token) => BeAnExistingRequirementDefinitionAsync(req, token))
+                    .WithMessage((_, req) =>
+                        $"Requirement definition doesn't exists! Requirement={req.RequirementDefinitionId}")
+                    .MustAsync((_, req, __, token) => NotBeAVoidedRequirementDefinitionAsync(req, token))
+                    .WithMessage((_, req) =>
+                        $"Requirement definition is voided! Requirement={req.RequirementDefinitionId}");
+            });
 
             async Task<bool> BeAnExistingRequirementDefinitionAsync(RequirementForCommand requirement, CancellationToken token)
                 => await requirementDefinitionValidator.ExistsAsync(requirement.RequirementDefinitionId, token);
@@ -36,6 +41,12 @@ namespace Equinor.Procosys.Preservation.Command.TagFunctionCommands.UpdateRequir
                 var reqIds = requirements.Select(dto => dto.RequirementDefinitionId).ToList();
                 return reqIds.Distinct().Count() == reqIds.Count;
             }
+            
+            async Task<bool> RequirementUsageIsForAllJourneysAsync(IEnumerable<RequirementForCommand> requirements, CancellationToken token)
+            {
+                var reqIds = requirements.Select(dto => dto.RequirementDefinitionId).ToList();
+                return await requirementDefinitionValidator.UsageCoversBothForSupplierAndOtherAsync(reqIds, token);
+            }                        
         }
     }
 }
