74e33d7104d5d828d0d02ef0cba3afb9e750ecf7
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-06-10 12:08:38 +0200

    Migration of TagRequirements.Usage

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200610075621_TagRequirementUsage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class TagRequirementUsage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Usage",
+                table: "TagRequirements",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "ForAll");
+
+            migrationBuilder.CreateCheckConstraint(
+                name: "constraint_tagreq_check_valid_usage",
+                table: "TagRequirements",
+                sql: "Usage in ('ForAll','ForSuppliersOnly','ForOtherThanSuppliers')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_tagreq_check_valid_usage",
+                table: "TagRequirements");
+
+            migrationBuilder.DropColumn(
+                name: "Usage",
+                table: "TagRequirements");
+        }
+    }
+}
