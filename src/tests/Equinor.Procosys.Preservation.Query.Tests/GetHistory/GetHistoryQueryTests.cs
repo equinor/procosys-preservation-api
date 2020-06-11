f96dd0a873e918f504d70f5ae8a00e998841ecb0
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-06-11 15:37:06 +0200

    tests

---
+++ src/tests/Equinor.Procosys.Preservation.Query.Tests/GetHistory/GetHistoryQueryTests.cs
@@ -0,0 +1,19 @@
+﻿using Equinor.Procosys.Preservation.Query.GetHistory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Query.Tests.GetHistory
+{
+    [TestClass]
+    public class GetHistoryQueryTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldSetProperties()
+        {
+            // Act 
+            var dut = new GetHistoryQuery(1);
+
+            // Assert
+            Assert.AreEqual(1, dut.TagId);
+        }
+    }
+}
