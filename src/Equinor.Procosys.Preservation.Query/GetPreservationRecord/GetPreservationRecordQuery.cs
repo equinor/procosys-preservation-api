85663ba7ce99625f86b0ba7265dcdae03c31ebc8
Demissie Bediye Aredo <deare@equinor.com>
2020-06-12 08:46:47 +0200

    74322 api  get preservation record endpoint (#276)
    * added GetPreservationRecord endpoint
    
    * create query and handler
    
    * added GetPreservationRecord endpoint
    
    * modified GetPreservationQuery handler
    
    * added tests
    
    * added more tests
    
    * When getting all preservetionRecords for all periods for a requirement, skip period without records
    
    * codereview changes
    
    * revert change in TagController
    
    * minor after codereview
    
    Co-authored-by: Jan Inge Dalsbø <JDALSB@equinor.com>

---
+++ src/Equinor.Procosys.Preservation.Query/GetPreservationRecord/GetPreservationRecordQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Query.GetPreservationRecord
+{
+    public class GetPreservationRecordQuery : IRequest<Result<PreservationRecordDto>>, ITagQueryRequest
+    {
+        public GetPreservationRecordQuery(int tagId, int requirementId, int preservationRecordId)
+        {
+            TagId = tagId;
+            RequirementId = requirementId;
+            PreservationRecordId = preservationRecordId;
+        }
+
+        public int TagId { get; }
+        public int RequirementId { get; }
+        public int PreservationRecordId { get; }
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.Query/GetPreservationRecord/PreservationRecordDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Equinor.Procosys.Preservation.Query.GetPreservationRecord
+{
+    public class PreservationRecordDto
+    {
+        public PreservationRecordDto(
+            int id,
+            bool bulkPreserved)
+        {
+            Id = id;
+            BulkPreserved = bulkPreserved;
+        }
+
+        public int Id { get; }
+        public bool BulkPreserved { get; }
+    }
+}
