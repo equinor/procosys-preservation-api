0fe42bec548f5d1d4acf9a4093680da6a65f1d83
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-06-15 16:36:45 +0200

    add descriptions to event types

--- src/Equinor.Procosys.Preservation.Domain/AggregateModels/HistoryAggregate/EventType.cs
+++ src/Equinor.Procosys.Preservation.Domain/AggregateModels/HistoryAggregate/EventType.cs
@@ -1,22 +1,40 @@
-﻿namespace Equinor.Procosys.Preservation.Domain.AggregateModels.HistoryAggregate
+﻿using System.ComponentModel;
+
+namespace Equinor.Procosys.Preservation.Domain.AggregateModels.HistoryAggregate
 {
     public enum EventType
     {
+        [Description("Added requirement")]
         AddRequirement,
+        [Description("Deleted requirement")]
         DeleteRequirement,
+        [Description("Voided requirement")]
         VoidRequirement,
+        [Description("Unvoided requirement")]
         UnvoidRequirement,
+        [Description("Preserved requirement")]
         PreserveRequirement,
+        [Description("Voided tag")]
         VoidTag,
+        [Description("Unvoided tag")]
         UnvoidTag,
+        [Description("Created tag")]
         CreateTag,
+        [Description("Deleted tag")]
         DeleteTag,
+        [Description("Started preservation")]
         StartPreservation,
+        [Description("Completed preservation")]
         CompletePreservation,
+        [Description("Changed interval")]
         ChangeInterval,
+        [Description("Manually transferred")]
         ManualTransfer,
+        [Description("Automatically transferred")]
         AutomaticTransfer,
+        [Description("Added action")]
         AddAction,
+        [Description("Closed action")]
         CloseAction
     }
 }
