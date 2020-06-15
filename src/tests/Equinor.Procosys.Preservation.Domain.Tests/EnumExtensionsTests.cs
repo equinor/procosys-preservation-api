756956acd3327276022cc3744ece80da2a710385
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-06-15 16:46:19 +0200

    enum extension tests

---
+++ src/tests/Equinor.Procosys.Preservation.Domain.Tests/EnumExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.Domain.Tests
+{
+    [TestClass]
+    public class EnumExtensionsTests
+    {
+        private enum TestEnums
+        {
+            [System.ComponentModel.Description("Some enum description")]
+            SomeEnum
+        }
+
+        [TestMethod]
+        public void GetDescription_ShouldReturnDescription()
+        {
+            var expected = "Some enum description";
+            var actual = TestEnums.SomeEnum.GetDescription();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
