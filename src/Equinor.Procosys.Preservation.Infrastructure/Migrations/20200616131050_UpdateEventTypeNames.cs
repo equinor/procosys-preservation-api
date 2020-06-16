3b33ad4f4fd4bd6032c2d4c089abba538b5b7172
Lykke Larsen <norl@statoil.com>
2020-06-16 15:19:11 +0200

    migration for new eventtype names

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200616131050_UpdateEventTypeNames.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class UpdateEventTypeNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History");
+
+            migrationBuilder.CreateCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History",
+                sql: "EventType in ('RequirementAdded','RequirementDeleted','RequirementVoided','RequirementUnvoided','RequirementPreserved','TagVoided','TagUnvoided','TagCreated','TagDeleted','PreservationStarted','PreservationCompleted','IntervalChanged','TransferredManually','TransferredAutomatically','ActionAdded','ActionClosed')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History");
+
+            migrationBuilder.CreateCheckConstraint(
+                name: "constraint_history_check_valid_event_type",
+                table: "History",
+                sql: "EventType in ('AddRequirement','DeleteRequirement','VoidRequirement','UnvoidRequirement','PreserveRequirement','VoidTag','UnvoidTag','CreateTag','DeleteTag','StartPreservation','CompletePreservation','ChangeInterval','ManualTransfer','AutomaticTransfer','AddAction','CloseAction')");
+        }
+    }
+}
