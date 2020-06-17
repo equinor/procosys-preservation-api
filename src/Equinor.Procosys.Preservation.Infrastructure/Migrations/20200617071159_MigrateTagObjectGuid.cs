ae3f2c932f180681c428de7eeffbdb87752a6451
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-06-17 09:34:32 +0200

    migration (#284)

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200617071159_MigrateTagObjectGuid.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class MigrateTagObjectGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Update Tags Set ObjectGuid = NEWID() where ObjectGuid = 'D445579D-E8CA-49F4-80D7-59452EBD8D47'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
