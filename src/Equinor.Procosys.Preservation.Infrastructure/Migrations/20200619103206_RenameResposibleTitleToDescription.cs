b6959ea6f1f8b8e66333fa2c5ab5c0a175305fd2
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-06-19 12:44:01 +0200

    Migration

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200619103206_RenameResposibleTitleToDescription.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class RenameResposibleTitleToDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Responsibles_Plant_ASC",
+                table: "Responsibles");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Responsibles",
+                newName: "Description");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Responsibles_Plant_ASC",
+                table: "Responsibles",
+                column: "Plant")
+                .Annotation("SqlServer:Include", new[] { "CreatedAtUtc", "IsVoided", "ModifiedAtUtc", "Description" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Responsibles_Plant_ASC",
+                table: "Responsibles");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Responsibles",
+                newName: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Responsibles_Plant_ASC",
+                table: "Responsibles",
+                column: "Plant")
+                .Annotation("SqlServer:Include", new[] { "CreatedAtUtc", "IsVoided", "ModifiedAtUtc", "Title" });
+        }
+    }
+}
