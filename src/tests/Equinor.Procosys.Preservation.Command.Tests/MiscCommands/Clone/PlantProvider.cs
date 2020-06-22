f70e388c4178c1ae5fc76f46555b2610b62fe63d
Henning Bjørgo <HHBJ@equinor.com>
2020-06-22 08:57:39 +0200

    Code cosmetics.

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/PlantProvider.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/MiscCommands/Clone/PlantProvider.cs
@@ -5,19 +5,12 @@ namespace Equinor.Procosys.Preservation.Command.Tests.MiscCommands.Clone
 {
     public class PlantProvider : IPlantProvider, IPlantSetter
     {
-        public const string PlantHeader = "x-plant";
         private string _plant;
 
-        public PlantProvider(string plant)
-        {
-            _plant = plant;
-        }
+        public PlantProvider(string plant) => _plant = plant;
 
         public string Plant => _plant ?? throw new Exception("Could not determine current plant");
 
-        public void SetPlant(string plant)
-        {
-            _plant = plant;
-        }
+        public void SetPlant(string plant) => _plant = plant;
     }
 }
