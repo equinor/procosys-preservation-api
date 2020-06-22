4f7e87d88e89661c8001bba25321f53d9b1d8aa0
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-06-22 13:06:36 +0200

    IJourneyService

---
+++ src/Equinor.Procosys.Preservation.Domain/Services/IJourneyService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Preservation.Domain.Services
+{
+    public interface IJourneyService
+    {
+        Task<bool> IsJourneyInUseAsync(long journeyId, CancellationToken cancellationToken);
+    }
+}
