6692dc9eeef97ebd293b0fb6e2e68be3f0a480cd
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-06-22 08:14:30 +0200

    Migration

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200622061116_MakeTagDescNullable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class MakeTagDescNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tags",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tags",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
