24a531e767d78866041219f65373b03516227d7f
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-06-22 08:50:26 +0200

    74436 Sync responsibles and tag function data (#293)
    * Sync Resposibles endpoint + command + handler
    
    * unit tests
    
    * spelling error
    
    * Sync TF endpoint + command + handler + unit tests

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/Synchronize/SynchronizeController.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Synchronize/SynchronizeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Command.SyncCommands.SyncProjects;
+using Equinor.Procosys.Preservation.Command.SyncCommands.SyncResponsibles;
+using Equinor.Procosys.Preservation.Command.SyncCommands.SyncTagFunctions;
 using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.WebApi.Misc;
 using MediatR;
@@ -27,6 +29,30 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.Synchronize
             string plant)
         {
             var result = await _mediator.Send(new SyncProjectsCommand());
+            return this.FromResult(result);
+        }
+
+        [Authorize(Roles = Permissions.LIBRARY_PRESERVATION_WRITE)]
+        [HttpPut("Responsibles")]
+        public async Task<ActionResult> Responsibles(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(PlantEntityBase.PlantLengthMax, MinimumLength = PlantEntityBase.PlantLengthMin)]
+            string plant)
+        {
+            var result = await _mediator.Send(new SyncResponsiblesCommand());
+            return this.FromResult(result);
+        }
+
+        [Authorize(Roles = Permissions.LIBRARY_PRESERVATION_WRITE)]
+        [HttpPut("TagFunctions")]
+        public async Task<ActionResult> TagFunctions(
+            [FromHeader( Name = PlantProvider.PlantHeader)]
+            [Required]
+            [StringLength(PlantEntityBase.PlantLengthMax, MinimumLength = PlantEntityBase.PlantLengthMin)]
+            string plant)
+        {
+            var result = await _mediator.Send(new SyncTagFunctionsCommand());
             return this.FromResult(result);
         }
     }
