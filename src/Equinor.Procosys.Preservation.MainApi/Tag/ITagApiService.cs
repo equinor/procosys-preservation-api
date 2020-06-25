8eadc519fa9e348eef242577263c0bade695aa19
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-06-25 13:37:52 +0200

    Move paging logic from SyncProjectsCommandHandler.SyncTagData into MainApiTagService.GetTagDetailsAsync to reuse paging logic to reduce how long a url can be (max 2000)

--- src/Equinor.Procosys.Preservation.MainApi/Tag/ITagApiService.cs
+++ src/Equinor.Procosys.Preservation.MainApi/Tag/ITagApiService.cs
@@ -5,7 +5,7 @@ namespace Equinor.Procosys.Preservation.MainApi.Tag
 {
     public interface ITagApiService
     {
-        Task<IList<ProcosysTagDetails>> GetTagDetailsAsync(string plant, string projectName, IEnumerable<string> tagNos);
+        Task<IList<ProcosysTagDetails>> GetTagDetailsAsync(string plant, string projectName, IList<string> tagNos);
         Task<IList<ProcosysTagOverview>> SearchTagsByTagNoAsync(string plant, string projectName, string startsWithTagNo);
         Task<IList<ProcosysPreservedTag>> GetPreservedTagsAsync(string plant, string projectName);
         Task<IList<ProcosysTagOverview>> SearchTagsByTagFunctionsAsync(string plant, string projectName, IList<string> tagFunctionCodeRegisterCodePairs);
