9abb66a68cfb6f28ba9fc743633ff53c268cfc7c
Pål Eie <PAEIE@equinor.com>
2020-06-26 11:22:36 +0200

    Updates after CR

--- src/Equinor.Procosys.Preservation.WebApi/Middleware/VerifyOidInDbMiddleware.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Middleware/VerifyOidInDbMiddleware.cs
@@ -1,39 +1,50 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.PersonAggregate;
 using Equinor.Procosys.Preservation.Infrastructure;
 using Equinor.Procosys.Preservation.WebApi.Authorizations;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace Equinor.Procosys.Preservation.WebApi.Middleware
 {
     public class VerifyOidInDbMiddleware
     {
-        public const string PlantHeader = "x-plant";
-
         private readonly RequestDelegate _next;
 
         public VerifyOidInDbMiddleware(RequestDelegate next) => _next = next;
 
-        public async Task InvokeAsync(HttpContext context, IHttpContextAccessor httpContextAccessor, PreservationContext preservationContext)
+        public async Task InvokeAsync(HttpContext context, PreservationContext preservationContext, ICurrentUserProvider currentUserProvider)
         {
-            var oid = httpContextAccessor.HttpContext.User.Claims.TryGetOid().Value;
-            var exists = preservationContext.Persons.Any(p => p.Oid.Equals(oid));
+            var exists = preservationContext.Persons.Any(p => p.Oid.Equals(currentUserProvider.GetCurrentUserOid()));
 
             if (!exists)
             {
-                var givenName = httpContextAccessor.HttpContext.User.Claims.TryGetGivenName();
-                var surName = httpContextAccessor.HttpContext.User.Claims.TryGetSurName();
+                var givenName = context.User.Claims.TryGetGivenName();
+                var surName = context.User.Claims.TryGetSurName();
 
                 if (string.IsNullOrWhiteSpace(givenName) || string.IsNullOrWhiteSpace(surName))
                 {
                     throw new Exception("Not able to find necessary claims to create user");
                 }
-                
-                var person = new Person(oid, givenName, surName);
-                preservationContext.Persons.Add(person);
-                preservationContext.SaveChanges();
+
+                var person = new Person(currentUserProvider.GetCurrentUserOid(), givenName, surName);
+
+                try
+                {
+                    preservationContext.Persons.Add(person);
+                    preservationContext.SaveChanges();
+                    
+                }
+                catch (Exception)
+                {
+                    // We have to do this silently as concurrency is a very likely problem.
+                    // For a user accessing preservation for the first time, there will probably be multiple
+                    // requests in parallel.
+                    preservationContext.Entry(person).State = EntityState.Detached;
+                }
             }
             
             // Call the next delegate/middleware in the pipeline
