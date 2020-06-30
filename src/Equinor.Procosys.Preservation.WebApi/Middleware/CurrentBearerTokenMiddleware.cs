bea0fd370b46d3906936e4e287fb112af03eefa1
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-06-30 09:23:03 +0200

    Bugfix - Handling of Authorization in middleware before user is authenticated (#315)
    * Don't try to decode Headers["Authorization"] before user is authenticated
    
    (cherry picked from commit 5006c65223febe6d0c76f32e3e84fb9336a72016)
    
    * Don't panic with exception in CurrentUserMiddleware before user is authenticated
    
    (cherry picked from commit a2399dc9f8739b48ce068f1ad1ffe5e4c78ca753)

--- src/Equinor.Procosys.Preservation.WebApi/Middleware/CurrentBearerTokenMiddleware.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Middleware/CurrentBearerTokenMiddleware.cs
@@ -13,9 +13,13 @@ namespace Equinor.Procosys.Preservation.WebApi.Middleware
         public async Task InvokeAsync(HttpContext context, IHttpContextAccessor httpContextAccessor, IBearerTokenSetter bearerTokenSetter)
         {
             var authorizationHeader = httpContextAccessor.HttpContext.Request.Headers["Authorization"];
-            var token = authorizationHeader.ToString().Split(' ')[1];
+            var tokens = authorizationHeader.ToString()?.Split(' ');
 
-            bearerTokenSetter.SetBearerToken(token);
+            if (tokens != null && tokens.Length > 1)
+            {
+                var token = tokens[1];
+                bearerTokenSetter.SetBearerToken(token);
+            }
 
             // Call the next delegate/middleware in the pipeline
             await _next(context);
