09dc957debcebe99214456fde999f3fec28cb7ef
Elisabeth Bratli (ELIBRA) <ELIBRA@equinor.com>
2020-07-08 09:07:17 +0200

    Delete requirement type tests

---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/RequirementTypeCommands/DeleteRequirementType/DeleteRequirementTypeCommandValidatorTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.RequirementTypeCommands.DeleteRequirementType;
+using Equinor.Procosys.Preservation.Command.Validators.RequirementTypeValidators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.RequirementTypeCommands.DeleteRequirementType
+{
+    [TestClass]
+    public class DeleteRequirementTypeCommandValidatorTests
+    {
+        private DeleteRequirementTypeCommandValidator _dut;
+        private Mock<IRequirementTypeValidator> _requirementTypeValidatorMock;
+        private DeleteRequirementTypeCommand _command;
+
+        private int _id = 1;
+
+        [TestInitialize]
+        public void Setup_OkState()
+        {
+            _requirementTypeValidatorMock = new Mock<IRequirementTypeValidator>();
+            _requirementTypeValidatorMock.Setup(r => r.ExistsAsync(_id, default)).Returns(Task.FromResult(true));
+            _requirementTypeValidatorMock.Setup(r => r.IsVoidedAsync(_id, default)).Returns(Task.FromResult(true));
+            _command = new DeleteRequirementTypeCommand(_id, null);
+
+            _dut = new DeleteRequirementTypeCommandValidator(_requirementTypeValidatorMock.Object);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldBeValid_WhenOkState()
+        {
+            var result = _dut.Validate(_command);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenRequirementTypeNotExists()
+        {
+            _requirementTypeValidatorMock.Setup(r => r.ExistsAsync(_id, default)).Returns(Task.FromResult(false));
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Requirement type doesn't exist!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenRequirementTypeNotVoided()
+        {
+            _requirementTypeValidatorMock.Setup(r => r.IsVoidedAsync(_id, default)).Returns(Task.FromResult(false));
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Requirement type is not voided!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenRequirementTypeHasDefinitions()
+        {
+            _requirementTypeValidatorMock.Setup(r => r.RequirementDefinitionExistsAsync(_id, default)).Returns(Task.FromResult(true));
+            
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Requirement type has requirement definitions!"));
+        }
+    }
+}
