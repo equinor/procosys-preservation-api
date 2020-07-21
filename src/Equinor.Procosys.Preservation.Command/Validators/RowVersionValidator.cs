73fa3cee0729854f4cfa9103d0e377cbb87ea206
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-07-21 12:47:16 +0200

    add row version validator

---
+++ src/Equinor.Procosys.Preservation.Command/Validators/RowVersionValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Equinor.Procosys.Preservation.Command.Validators
+{
+    public class RowVersionValidator : IRowVersionValidator
+    {
+        public async Task<bool> IsValid(string rowVersion, CancellationToken cancellationToken) 
+            => TryConvertBase64StringToByteArray(rowVersion, out _);
+
+        private static bool TryConvertBase64StringToByteArray(string input, out byte[] output)
+            {
+                output = null;
+                try
+                {
+                    output = Convert.FromBase64String(input);
+                    return true;
+                }
+                catch (FormatException)
+                {
+                    return false;
+                }
+            }
+    }
+}
