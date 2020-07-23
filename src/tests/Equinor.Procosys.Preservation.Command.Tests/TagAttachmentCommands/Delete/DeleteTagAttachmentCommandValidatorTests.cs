6798d205280e63cac5902bbe8ac04d9c045afa3c
Lykke Larsen <norl@statoil.com>
2020-07-23 13:15:11 +0200

    delete tag attachment command validator and test

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/TagAttachmentCommands/Delete/DeleteTagAttachmentCommandValidatorTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/TagAttachmentCommands/Delete/DeleteTagAttachmentCommandValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Command.TagAttachmentCommands.Delete;
+using Equinor.Procosys.Preservation.Command.Validators;
 using Equinor.Procosys.Preservation.Command.Validators.AttachmentValidators;
 using Equinor.Procosys.Preservation.Command.Validators.ProjectValidators;
 using Equinor.Procosys.Preservation.Command.Validators.TagValidators;
@@ -15,14 +16,19 @@ namespace Equinor.Procosys.Preservation.Command.Tests.TagAttachmentCommands.Dele
         private Mock<IProjectValidator> _projectValidatorMock;
         private Mock<ITagValidator> _tagValidatorMock;
         private Mock<IAttachmentValidator> _attachmentValidatorMock;
+        private Mock<IRowVersionValidator> _rowVersionValidatorMock;
         private DeleteTagAttachmentCommand _command;
+
+        private int _tagId = 2;
+        private int _attachmentId = 3;
+        private string _rowVersion = "AAAAAAAAJ00=";
 
         [TestInitialize]
         public void Setup_OkState()
         {
             _projectValidatorMock = new Mock<IProjectValidator>();
 
-            _command = new DeleteTagAttachmentCommand(2, 3, null);
+            _command = new DeleteTagAttachmentCommand(_tagId, _attachmentId, _rowVersion);
 
             _tagValidatorMock = new Mock<ITagValidator>();
             _tagValidatorMock.Setup(r => r.ExistsAsync(_command.TagId, default)).Returns(Task.FromResult(true));
@@ -30,10 +36,14 @@ namespace Equinor.Procosys.Preservation.Command.Tests.TagAttachmentCommands.Dele
             _attachmentValidatorMock = new Mock<IAttachmentValidator>();
             _attachmentValidatorMock.Setup(r => r.ExistsAsync(_command.AttachmentId, default)).Returns(Task.FromResult(true));
 
+            _rowVersionValidatorMock = new Mock<IRowVersionValidator>();
+            _rowVersionValidatorMock.Setup(r => r.IsValid(_rowVersion, default)).Returns(Task.FromResult(true));
+
             _dut = new DeleteTagAttachmentCommandValidator(
                 _projectValidatorMock.Object,
                 _tagValidatorMock.Object,
-                _attachmentValidatorMock.Object);
+                _attachmentValidatorMock.Object,
+                _rowVersionValidatorMock.Object);
         }
 
         [TestMethod]
@@ -90,6 +100,21 @@ namespace Equinor.Procosys.Preservation.Command.Tests.TagAttachmentCommands.Dele
             Assert.IsFalse(result.IsValid);
             Assert.AreEqual(1, result.Errors.Count);
             Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Tag is voided!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenInvalidRowVersion()
+        {
+            const string invalidRowVersion = "String";
+
+            var command = new DeleteTagAttachmentCommand(_tagId, _attachmentId, invalidRowVersion);
+            _rowVersionValidatorMock.Setup(r => r.IsValid(invalidRowVersion, default)).Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Not a valid RowVersion!"));
         }
     }
 }
