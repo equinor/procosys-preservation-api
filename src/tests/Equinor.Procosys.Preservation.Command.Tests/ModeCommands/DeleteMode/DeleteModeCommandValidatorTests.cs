737d88f99bdcaf98fb9960aff37735c38c452245
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-07-23 08:51:42 +0200

    delete mode command validator and test

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/ModeCommands/DeleteMode/DeleteModeCommandValidatorTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/ModeCommands/DeleteMode/DeleteModeCommandValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Command.ModeCommands.DeleteMode;
+using Equinor.Procosys.Preservation.Command.Validators;
 using Equinor.Procosys.Preservation.Command.Validators.ModeValidators;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -11,9 +12,11 @@ namespace Equinor.Procosys.Preservation.Command.Tests.ModeCommands.DeleteMode
     {
         private DeleteModeCommandValidator _dut;
         private Mock<IModeValidator> _modeValidatorMock;
+        private Mock<IRowVersionValidator> _rowVersionValidatorMock;
         private DeleteModeCommand _command;
 
         private int _id = 1;
+        private string _rowVersion = "AAAAAAAAJ00=";
 
         [TestInitialize]
         public void Setup_OkState()
@@ -21,9 +24,13 @@ namespace Equinor.Procosys.Preservation.Command.Tests.ModeCommands.DeleteMode
             _modeValidatorMock = new Mock<IModeValidator>();
             _modeValidatorMock.Setup(r => r.ExistsAsync(_id, default)).Returns(Task.FromResult(true));
             _modeValidatorMock.Setup(r => r.IsVoidedAsync(_id, default)).Returns(Task.FromResult(true));
-            _command = new DeleteModeCommand(_id, null);
+            
+            _rowVersionValidatorMock = new Mock<IRowVersionValidator>();
+            _rowVersionValidatorMock.Setup(r => r.IsValid(_rowVersion, default)).Returns(Task.FromResult(true));
+            
+            _command = new DeleteModeCommand(_id, _rowVersion);
 
-            _dut = new DeleteModeCommandValidator(_modeValidatorMock.Object);
+            _dut = new DeleteModeCommandValidator(_modeValidatorMock.Object, _rowVersionValidatorMock.Object);
         }
 
         [TestMethod]
@@ -68,6 +75,21 @@ namespace Equinor.Procosys.Preservation.Command.Tests.ModeCommands.DeleteMode
             Assert.IsFalse(result.IsValid);
             Assert.AreEqual(1, result.Errors.Count);
             Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Mode is used in step(s)!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenInvalidRowVersion()
+        {
+            const string invalidRowVersion = "String";
+
+            var command = new DeleteModeCommand(_id, invalidRowVersion);
+            _rowVersionValidatorMock.Setup(r => r.IsValid(invalidRowVersion, default)).Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Not a valid RowVersion!"));
         }
     }
 }
