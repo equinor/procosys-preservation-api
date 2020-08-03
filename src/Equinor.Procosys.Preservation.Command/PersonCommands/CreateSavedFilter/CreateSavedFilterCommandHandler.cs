d84429fac6dbd57d5170088c3c228a134949f883
Lykke Larsen <norl@statoil.com>
2020-08-03 16:17:12 +0200

    endpoint create new saved filter

---
+++ src/Equinor.Procosys.Preservation.Command/PersonCommands/CreateSavedFilter/CreateSavedFilterCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.PersonCommands.CreateSavedFilter;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.PersonAggregate;
+using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Command.ActionCommands.CreateAction
+{
+    public class CreateSavedFilterCommandHandler : IRequestHandler<CreateSavedFilterCommand, Result<int>>
+    {
+        private readonly IPersonRepository _personRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPlantProvider _plantProvider;
+        private readonly ICurrentUserProvider _currentUserProvider;
+
+        public CreateSavedFilterCommandHandler(
+            IPersonRepository personRepository,
+            IUnitOfWork unitOfWork,
+            IPlantProvider plantProvider,
+            ICurrentUserProvider currentUserProvider)
+        {
+            _personRepository = personRepository;
+            _unitOfWork = unitOfWork;
+            _plantProvider = plantProvider;
+            _currentUserProvider = currentUserProvider;
+        }
+
+        public async Task<Result<int>> Handle(CreateSavedFilterCommand request, CancellationToken cancellationToken)
+        {
+            var currentUserOid = _currentUserProvider.GetCurrentUserOid();
+            var person = await _personRepository.GetByOidAsync(currentUserOid);
+
+            var savedFilter = new SavedFilter(_plantProvider.Plant, request.Title, request.Criteria);
+            person.AddSavedFilter(savedFilter);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return new SuccessResult<int>(savedFilter.Id);
+        }
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Persons/CreateSavedFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Equinor.Procosys.Preservation.WebApi.Controllers.Persons
+{
+    public class CreateSavedFilterDto
+    {
+        public string Title { get; set; }
+        public string Criteria { get; set; }
+    }
+}
