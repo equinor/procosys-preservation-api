e4cf4cea5299a61043ec378e3c8e926830248a4f
Nora Lykke Strøm Larsen <NORL@equinor.com>
2020-08-04 14:54:07 +0200

    tests

---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/PersonCommands/CreateSavedFilter/CreateSavedFilterCommandValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.PersonCommands.CreateSavedFilter;
+using Equinor.Procosys.Preservation.Command.Validators.SavedFilterValidators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.PersonCommands.CreateSavedFilter
+{
+    [TestClass]
+    public class CreateSavedFilterCommandValidatorTests
+    {
+        private CreateSavedFilterCommand _command;
+        private CreateSavedFilterCommandValidator _dut;
+        private Mock<ISavedFilterValidator> _savedFilterValidatorMock;
+
+        private readonly string _title = "Title";
+
+        [TestInitialize]
+        public void Setup_OkState()
+        {
+            const string Criteria = "Criteria";
+
+            _savedFilterValidatorMock = new Mock<ISavedFilterValidator>();
+            _command = new CreateSavedFilterCommand(_title, Criteria);
+            _dut = new CreateSavedFilterCommandValidator(_savedFilterValidatorMock.Object);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldBeValid_WhenOkState()
+        {
+            var result = _dut.Validate(_command);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenSavedFilterWithSameTitleForPersonAlreadyExists()
+        {
+            _savedFilterValidatorMock.Setup(r => r.ExistsWithSameTitleForPersonAsync(_title, default)).Returns(Task.FromResult(true));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("A saved filter with this title already exists!"));
+        }
+    }
+}
---
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/Validators/SavedFilterValidatorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.Validators.SavedFilterValidators;
+using Equinor.Procosys.Preservation.Domain;
+using Equinor.Procosys.Preservation.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.PersonAggregate;
+using Equinor.Procosys.Preservation.Test.Common;
+using Moq;
+
+namespace Equinor.Procosys.Preservation.Command.Tests.Validators
+{
+    [TestClass]
+    public class SavedFilterValidatorTests : ReadOnlyTestsBase
+    {
+        private Mock<IPersonRepository> _personRepositoryMock;
+        private Mock<ICurrentUserProvider> _currentUserProviderMock;
+        private Guid _personOid;
+        private SavedFilterValidator _dut;
+
+        private readonly string _title = "title";
+
+        protected override void SetupNewDatabase(DbContextOptions<PreservationContext> dbContextOptions)
+        {
+            using (var context = new PreservationContext(dbContextOptions, _plantProvider, _eventDispatcher,
+                _currentUserProvider))
+            {
+                const string Criteria = "criteria";
+                _personOid = new Guid();
+
+                var person = AddPerson(context, _personOid, "Current", "User");
+                var savedFilter = new SavedFilter(TestPlant, _title, Criteria);
+                
+                person.AddSavedFilter(savedFilter);
+                context.SaveChangesAsync().Wait();
+
+                _personRepositoryMock = new Mock<IPersonRepository>();
+                _personRepositoryMock
+                    .Setup(p => p.GetByOidAsync(It.Is<Guid>(x => x == _personOid)))
+                    .Returns(Task.FromResult(person));
+
+                _currentUserProviderMock = new Mock<ICurrentUserProvider>();
+                _currentUserProviderMock
+                    .Setup(x => x.GetCurrentUserOid())
+                    .Returns(_personOid);
+            }
+        }
+
+        [TestMethod]
+        public async Task IsValid_UnknownTitle_ReturnsFalse()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher,
+                _currentUserProvider))
+            {
+                _dut = new SavedFilterValidator(context, _personRepositoryMock.Object, _currentUserProviderMock.Object);
+                var result = await _dut.ExistsWithSameTitleForPersonAsync("xxx", default);
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public async Task IsValid_KnownTitle_ReturnsTrue()
+        {
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher,
+                _currentUserProvider))
+            {
+                _dut = new SavedFilterValidator(context, _personRepositoryMock.Object, _currentUserProviderMock.Object);
+                var result = await _dut.ExistsWithSameTitleForPersonAsync(_title, default);
+
+                Assert.IsTrue(result);
+            }
+        }
+    }
+}
