478301304b4a7180a7ec75bd7df4ee1a054a3ce2
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2020-08-04 07:23:34 +0200

    Added migartion so existing RequirementTypes get correct icon from its Code (#347)

---
+++ src/Equinor.Procosys.Preservation.Infrastructure/Migrations/20200803093445_MigrateRequirementTypeIcon.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Migrations
+{
+    public partial class MigrateRequirementTypeIcon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Area' where Code = 'Area'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Battery' where Code = 'Charging'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Bearings' where Code = 'Grease'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Heating' where Code = 'Heating'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Electrical' where Code = 'IR Test'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Nitrogen' where Code = 'Nitrogen'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Pressure' where Code = 'Oil Level'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Power' where Code = 'Powered'");
+            migrationBuilder.Sql("update RequirementTypes set Icon = 'Rotate' where Code = 'Rotation'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
