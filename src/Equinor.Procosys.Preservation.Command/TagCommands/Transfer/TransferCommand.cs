a06bbc960a61eb41b2b6879fe866ceecf499371c
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-10 12:55:09 +0200

    Secure TransferCommand

--- src/Equinor.Procosys.Preservation.Command/TagCommands/Transfer/TransferCommand.cs
+++ src/Equinor.Procosys.Preservation.Command/TagCommands/Transfer/TransferCommand.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using MediatR;
 using ServiceResult;
 
 namespace Equinor.Procosys.Preservation.Command.TagCommands.Transfer
 {
-    public class TransferCommand : IRequest<Result<IEnumerable<IdAndRowVersion>>>
+    public class TransferCommand : IRequest<Result<IEnumerable<IdAndRowVersion>>>, ITagCommandRequest
     {
         public TransferCommand(IEnumerable<IdAndRowVersion> tags) => Tags = tags ?? new List<IdAndRowVersion>();
 
         public IEnumerable<IdAndRowVersion> Tags { get; }
+        
+        public int TagId
+        {
+            get
+            {
+                if (!Tags.Any())
+                {
+                    throw new Exception($"At least 1 {nameof(Tags)} must be given!");
+                }
+
+                return Tags.First().Id;
+            }
+        }
     }
 }
