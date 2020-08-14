f385e8a573882350bc6ecca253e246bb7a02ceda
Lykke Larsen <norl@statoil.com>
2020-08-14 10:56:03 +0200

    validator

--- src/Equinor.Procosys.Preservation.Command/Validators/SavedFilterValidators/SavedFilterValidator.cs
+++ src/Equinor.Procosys.Preservation.Command/Validators/SavedFilterValidators/SavedFilterValidator.cs
@@ -35,6 +35,18 @@ namespace Equinor.Procosys.Preservation.Command.Validators.SavedFilterValidators
                 select s).AnyAsync(token);
         }
 
+        public async Task<bool> ExistsWithSameTitleForPersonInProjectAsync(int savedFilterId, string title,
+            CancellationToken token)
+        {
+            var projectName = await (from s in _context.QuerySet<SavedFilter>()
+                join p in _context.QuerySet<Person>() on EF.Property<int>(s, "PersonId") equals p.Id
+                join pr in _context.QuerySet<Project>() on s.ProjectId equals pr.Id
+                where s.Id == savedFilterId
+                select pr.Name).SingleOrDefaultAsync(token);
+
+            return await ExistsWithSameTitleForPersonInProjectAsync(title, projectName, token);
+        }
+
         public async Task<bool> ExistsAsync(int savedFilterId, CancellationToken token) =>
             await (from sf in _context.QuerySet<SavedFilter>()
                 where sf.Id == savedFilterId
