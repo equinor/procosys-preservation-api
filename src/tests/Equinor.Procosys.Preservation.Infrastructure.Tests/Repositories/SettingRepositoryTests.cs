403468b48b92ca80868e228e7be44961d1336cda
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-19 08:32:03 +0200

    unit test repo

---
+++ src/tests/Equinor.Procosys.Preservation.Infrastructure.Tests/Repositories/SettingRepositoryTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Domain.AggregateModels.SettingAggregate;
+using Equinor.Procosys.Preservation.Infrastructure.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockQueryable.Moq;
+
+namespace Equinor.Procosys.Preservation.Infrastructure.Tests.Repositories
+{
+    [TestClass]
+    public class SettingRepositoryTests : RepositoryTestBase
+    {
+        private readonly string _settingCode = "A";
+        private SettingRepository _dut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var setting = new Setting(TestPlant, _settingCode);
+
+            var settings = new List<Setting> {setting};
+
+            var dbSetMock = settings.AsQueryable().BuildMockDbSet();
+
+            ContextHelper
+                .ContextMock
+                .Setup(x => x.Settings)
+                .Returns(dbSetMock.Object);
+
+            _dut = new SettingRepository(ContextHelper.ContextMock.Object);
+        }
+
+        [TestMethod]
+        public async Task GetByCode_ReturnsSetting_WhenSettingExists()
+        {
+            var result = await _dut.GetByCodeAsync(_settingCode);
+
+            // Assert
+            Assert.AreEqual(_settingCode, result.Code);
+        }
+
+        [TestMethod]
+        public async Task GetByCode_ReturnsNull_WhenSettingNotExists()
+        {
+            var result = await _dut.GetByCodeAsync("XYZ");
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
