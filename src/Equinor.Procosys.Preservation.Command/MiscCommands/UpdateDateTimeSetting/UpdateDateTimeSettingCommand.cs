c5a05f43d45fdb2fc7d286ab45d67346422a852b
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-20 10:25:39 +0200

    bugfix in add setting

--- src/Equinor.Procosys.Preservation.Command/MiscCommands/UpdateDateTimeSetting/UpdateDateTimeSettingCommand.cs
+++ src/Equinor.Procosys.Preservation.Command/MiscCommands/UpdateDateTimeSetting/UpdateDateTimeSettingCommand.cs
@@ -2,11 +2,11 @@
 using MediatR;
 using ServiceResult;
 
-namespace Equinor.Procosys.Preservation.Command.MiscCommands.DateTimeSetting
+namespace Equinor.Procosys.Preservation.Command.MiscCommands.UpdateDateTimeSetting
 {
-    public class DateTimeSettingCommand : IRequest<Result<Unit>>
+    public class UpdateDateTimeSettingCommand : IRequest<Result<Unit>>
     {
-        public DateTimeSettingCommand(string settingCode, DateTime value)
+        public UpdateDateTimeSettingCommand(string settingCode, DateTime value)
         {
             SettingCode = settingCode;
             Value = value;
--- src/Equinor.Procosys.Preservation.Command/MiscCommands/UpdateDateTimeSetting/UpdateDateTimeSettingCommandHandler.cs
+++ src/Equinor.Procosys.Preservation.Command/MiscCommands/UpdateDateTimeSetting/UpdateDateTimeSettingCommandHandler.cs
@@ -5,32 +5,33 @@ using Equinor.Procosys.Preservation.Domain.AggregateModels.SettingAggregate;
 using MediatR;
 using ServiceResult;
 
-namespace Equinor.Procosys.Preservation.Command.MiscCommands.DateTimeSetting
+namespace Equinor.Procosys.Preservation.Command.MiscCommands.UpdateDateTimeSetting
 {
-    public class DateTimeSettingCommandHandler : IRequestHandler<DateTimeSettingCommand, Result<Unit>>
+    public class UpdateDateTimeSettingCommandHandler : IRequestHandler<UpdateDateTimeSettingCommand, Result<Unit>>
     {
         private readonly ISettingRepository _settingRepository;
         private readonly IUnitOfWork _unitOfWork;
         private readonly IPlantProvider _plantProvider;
 
-        // todo unit test
-        public DateTimeSettingCommandHandler(ISettingRepository settingRepository, IUnitOfWork unitOfWork, IPlantProvider plantProvider)
+        public UpdateDateTimeSettingCommandHandler(ISettingRepository settingRepository, IUnitOfWork unitOfWork, IPlantProvider plantProvider)
         {
             _settingRepository = settingRepository;
             _unitOfWork = unitOfWork;
             _plantProvider = plantProvider;
         }
 
-        public async Task<Result<Unit>> Handle(DateTimeSettingCommand request, CancellationToken cancellationToken)
+        public async Task<Result<Unit>> Handle(UpdateDateTimeSettingCommand request, CancellationToken cancellationToken)
         {
             var setting = await _settingRepository.GetByCodeAsync(request.SettingCode);
 
             if (setting == null)
             {
                 setting = new Setting(_plantProvider.Plant, request.SettingCode);
+                _settingRepository.Add(setting);
             }
 
             setting.SetDateTime(request.Value);
+
             await _unitOfWork.SaveChangesAsync(cancellationToken);
 
             return new SuccessResult<Unit>(Unit.Value);
