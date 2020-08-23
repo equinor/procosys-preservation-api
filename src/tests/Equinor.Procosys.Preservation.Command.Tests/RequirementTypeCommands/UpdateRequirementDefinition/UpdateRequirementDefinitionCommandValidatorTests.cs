09079b35cabf6ca14a696ee0b3257be07959cdfb
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-23 14:55:30 +0200

    unit test new rules in UpdateRequirementDefinitionCommandValidator

--- src/tests/Equinor.Procosys.Preservation.Command.Tests/RequirementTypeCommands/UpdateRequirementDefinition/UpdateRequirementDefinitionCommandValidatorTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Command.Tests/RequirementTypeCommands/UpdateRequirementDefinition/UpdateRequirementDefinitionCommandValidatorTests.cs
@@ -23,17 +23,25 @@ namespace Equinor.Procosys.Preservation.Command.Tests.RequirementTypeCommands.Up
         private UpdateRequirementDefinitionCommandValidator _dut;
         private readonly int _requirementTypeId = 2;
         private readonly int _requirementDefinitionId = 3;
+        private readonly int _updateFieldId = 4;
         private readonly int _sortKey = 10;
         private readonly string _title = "Title test";
         private RequirementUsage _usage = RequirementUsage.ForAll;
         private int _defaultIntervalWeeks = 4;
         private string _rowVersion = "AAAAAAAAABA=";
-        private readonly IList<UpdateFieldsForCommand> _updatedFields = new List<UpdateFieldsForCommand>();
-        private readonly IList<FieldsForCommand> _newFields = new List<FieldsForCommand>();
+        private IList<UpdateFieldsForCommand> _updatedFields;
+        private IList<FieldsForCommand> _newFields;
+        private readonly FieldType _fieldType = FieldType.CheckBox;
 
         [TestInitialize]
         public void Setup_OkState()
         {
+            _updatedFields = new List<UpdateFieldsForCommand>
+            {
+                new UpdateFieldsForCommand(_updateFieldId, "L", _fieldType, 1, false, null)
+            };
+            _newFields = new List<FieldsForCommand>();
+
             _reqTypeValidatorMock = new Mock<IRequirementTypeValidator>();
             _reqTypeValidatorMock.Setup(r => r.ExistsAsync(_requirementTypeId, default)).Returns(Task.FromResult(true));
             _reqTypeValidatorMock
@@ -41,8 +49,13 @@ namespace Equinor.Procosys.Preservation.Command.Tests.RequirementTypeCommands.Up
                 .Returns(Task.FromResult(true));
 
             _reqDefinitionValidatorMock = new Mock<IRequirementDefinitionValidator>();
+            _reqDefinitionValidatorMock
+                .Setup(r => r.AllExcludedFieldsIsVoidedAsync(_requirementDefinitionId, new List<int> {_updateFieldId}, default))
+                .Returns(Task.FromResult(true));
 
             _fieldValidatorMock = new Mock<IFieldValidator>();
+            _fieldValidatorMock.Setup(f => f.ExistsAsync(_updateFieldId, default)).Returns(Task.FromResult(true));
+            _fieldValidatorMock.Setup(f => f.VerifyFieldTypeAsync(_updateFieldId, _fieldType, default)).Returns(Task.FromResult(true));
 
             _command = new UpdateRequirementDefinitionCommand(
                 _requirementTypeId, 
@@ -136,6 +149,58 @@ namespace Equinor.Procosys.Preservation.Command.Tests.RequirementTypeCommands.Up
             Assert.IsFalse(result.IsValid);
             Assert.AreEqual(1, result.Errors.Count);
             Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("A requirement definition with this title already exists on the requirement type"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenFieldToUpdateChangeType()
+        {
+            _fieldValidatorMock.Setup(f => f.ExistsAsync(_updateFieldId, default)).Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Field doesn't exist!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenFieldToUpdateNotExists()
+        {
+            _fieldValidatorMock.Setup(f => f.VerifyFieldTypeAsync(_updateFieldId, _fieldType, default)).Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Cannot change field type on existing fields!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenFieldToDeleteIsNotVoided()
+        {
+            _reqDefinitionValidatorMock
+                .Setup(r => r.AllExcludedFieldsIsVoidedAsync(_requirementDefinitionId, new List<int> {_updateFieldId}, default))
+                .Returns(Task.FromResult(false));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Fields to be deleted must be voided!"));
+        }
+
+        [TestMethod]
+        public void Validate_ShouldFail_WhenFieldToDeleteIsInUse()
+        {
+            _reqDefinitionValidatorMock
+                .Setup(r => r.AnyExcludedFieldsIsInUseAsync(_requirementDefinitionId, new List<int> {_updateFieldId}, default))
+                .Returns(Task.FromResult(true));
+
+            var result = _dut.Validate(_command);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(1, result.Errors.Count);
+            Assert.IsTrue(result.Errors[0].ErrorMessage.StartsWith("Fields to be deleted can not be in use!"));
         }
     }
 }
