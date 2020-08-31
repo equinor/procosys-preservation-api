34f0f57b2386d0f484add81c7e8b08bb13ab3153
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-08-31 14:09:08 +0200

    Added missing tests in JourneyRepositoryTests

--- src/tests/Equinor.Procosys.Preservation.Infrastructure.Tests/Repositories/JourneyRepositoryTests.cs
+++ src/tests/Equinor.Procosys.Preservation.Infrastructure.Tests/Repositories/JourneyRepositoryTests.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.JourneyAggregate;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.ModeAggregate;
 using Equinor.Procosys.Preservation.Domain.AggregateModels.ResponsibleAggregate;
 using Equinor.Procosys.Preservation.Infrastructure.Repositories;
+using Equinor.Procosys.Preservation.Test.Common.ExtensionMethods;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MockQueryable.Moq;
@@ -15,12 +17,14 @@ namespace Equinor.Procosys.Preservation.Infrastructure.Tests.Repositories
     [TestClass]
     public class JourneyRepositoryTests : RepositoryTestBase
     {
+        private const int JourneyId = 5;
         private const int StepId = 51;
         private List<Journey> _journeys;
         private Mock<DbSet<Journey>> _dbSetMock;
 
-        // todo write more unit tests. See ModeRepositoryTests
         private JourneyRepository _dut;
+        private Step _step;
+        private Journey _journey;
 
         [TestInitialize]
         public void Setup()
@@ -31,17 +35,17 @@ namespace Equinor.Procosys.Preservation.Infrastructure.Tests.Repositories
             var responsibleMock = new Mock<Responsible>();
             responsibleMock.SetupGet(x => x.Plant).Returns(TestPlant);
 
-            var stepMock1 = new Mock<Step>();
-            stepMock1.SetupGet(s => s.Id).Returns(StepId);
-            stepMock1.SetupGet(s => s.Plant).Returns(TestPlant);
+            _step = new Step(TestPlant, "S", modeMock.Object, responsibleMock.Object);
+            _step.SetProtectedIdForTesting(StepId);
 
-            var journey = new Journey(TestPlant, "J");
+            _journey = new Journey(TestPlant, "J");
+            _journey.SetProtectedIdForTesting(5);
 
-            journey.AddStep(stepMock1.Object);
+            _journey.AddStep(_step);
             
             _journeys = new List<Journey>
             {
-                journey
+                _journey
             };
             
             _dbSetMock = _journeys.AsQueryable().BuildMockDbSet();
@@ -69,5 +73,53 @@ namespace Equinor.Procosys.Preservation.Infrastructure.Tests.Repositories
 
             Assert.IsNull(result);
         }
+
+        [TestMethod]
+        public async Task GetJourneysByStepIdsAsync_KnownId_ShouldReturnStep()
+        {
+            var result = await _dut.GetJourneysByStepIdsAsync(new List<int>{StepId});
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(JourneyId, result.Single().Id);
+        }
+
+        [TestMethod]
+        public async Task GetJourneysByStepIdsAsync_UnknownId_ShouldReturnEmptyList()
+        {
+            var result = await _dut.GetJourneysByStepIdsAsync(new List<int> {6355});
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetJourneysWithAutoTransferStepsAsync_MatchingAutoTransferMethod_ShouldReturnJourneys()
+        {
+            // Arrange
+            _step.AutoTransferMethod = AutoTransferMethod.OnRfccSign;
+
+            // Act
+            var result = await _dut.GetJourneysWithAutoTransferStepsAsync(AutoTransferMethod.OnRfccSign);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(JourneyId, result.Single().Id);
+        }
+
+        [TestMethod]
+        public void Remove_WhenJourneyIsVoided_ShouldCallRemoveForJourney()
+        {
+            // Arrange
+            _journey.IsVoided = true;
+
+            // Act
+            _dut.Remove(_journey);
+
+            // Arrange
+            _dbSetMock.Verify(s => s.Remove(_journey), Times.Once);
+        }
+
+        [TestMethod]
+        public void Remove_WhenJourneyIsNotVoided_ShouldThrowException()
+            => Assert.ThrowsException<Exception>(() => _dut.Remove(_journey));
     }
 }
