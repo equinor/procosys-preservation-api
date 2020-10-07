85de930405ef454d417055e1bd89f3879fceff81
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-07 14:55:21 +0200

    Extend message written to History for IntervalChangedEvent

--- src/Equinor.Procosys.Preservation.Domain/Events/IntervalChangedEvent.cs
+++ src/Equinor.Procosys.Preservation.Domain/Events/IntervalChangedEvent.cs
@@ -8,16 +8,19 @@ namespace Equinor.Procosys.Preservation.Domain.Events
         public IntervalChangedEvent(
             string plant,
             Guid objectGuid,
+            int requirementDefinitionId,
             int fromInterval,
             int toInterval)
         {
             Plant = plant;
             ObjectGuid = objectGuid;
+            RequirementDefinitionId = requirementDefinitionId;
             FromInterval = fromInterval;
             ToInterval = toInterval;
         }
         public string Plant { get; }
         public Guid ObjectGuid { get; }
+        public int RequirementDefinitionId { get; }
         public int FromInterval { get; }
         public int ToInterval { get; }
     }
