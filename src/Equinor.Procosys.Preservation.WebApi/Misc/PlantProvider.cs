541d5ee0a3eb771dc6ab7070acccc625dc8c76de
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-09 10:40:41 +0200

    PlantProvider now allow thatr Plant is not set

--- src/Equinor.Procosys.Preservation.WebApi/Misc/PlantProvider.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Misc/PlantProvider.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using Equinor.Procosys.Preservation.Domain;
+﻿using Equinor.Procosys.Preservation.Domain;
 
 namespace Equinor.Procosys.Preservation.WebApi.Misc
 {
     public class PlantProvider : IPlantProvider, IPlantSetter
     {
-        private string _plant;
+        public string Plant { get; private set; }
 
-        public string Plant => _plant ?? throw new Exception("Could not determine current plant");
-
-        public void SetPlant(string plant) => _plant = plant;
+        public void SetPlant(string plant) => Plant = plant;
     }
 }
