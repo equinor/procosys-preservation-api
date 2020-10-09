36d8615af2b189458ac7e69b443dc87bf818b903
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-09 10:45:46 +0200

    Services calling Main api do not need to check if it is a valida plant now. Done in startup pipeline

--- src/Equinor.Procosys.Preservation.MainApi/Responsible/MainApiResposibleService.cs
+++ src/Equinor.Procosys.Preservation.MainApi/Responsible/MainApiResposibleService.cs
@@ -2,7 +2,6 @@
 using System.Net;
 using System.Threading.Tasks;
 using Equinor.Procosys.Preservation.MainApi.Client;
-using Equinor.Procosys.Preservation.MainApi.Plant;
 using Microsoft.Extensions.Options;
 
 namespace Equinor.Procosys.Preservation.MainApi.Responsible
@@ -12,25 +11,17 @@ namespace Equinor.Procosys.Preservation.MainApi.Responsible
         private readonly string _apiVersion;
         private readonly Uri _baseAddress;
         private readonly IBearerTokenApiClient _mainApiClient;
-        private readonly IPlantCache _plantCache;
 
         public MainApiResponsibleService(IBearerTokenApiClient mainApiClient,
-            IPlantCache plantCache,
             IOptionsMonitor<MainApiOptions> options)
         {
             _mainApiClient = mainApiClient;
-            _plantCache = plantCache;
             _apiVersion = options.CurrentValue.ApiVersion;
             _baseAddress = new Uri(options.CurrentValue.BaseAddress);
         }
 
         public async Task<ProcosysResponsible> TryGetResponsibleAsync(string plant, string code)
         {
-            if (!await _plantCache.IsValidPlantForCurrentUserAsync(plant))
-            {
-                throw new ArgumentException($"Invalid plant: {plant}");
-            }
-
             var url = $"{_baseAddress}Library/Responsible" +
                       $"?plantId={plant}" +
                       $"&code={WebUtility.UrlEncode(code)}" +
