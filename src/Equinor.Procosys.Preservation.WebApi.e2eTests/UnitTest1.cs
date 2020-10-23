e9aec032e6c37c7d1485a684847914ad2d3b8f82
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-23 14:52:05 +0200

    Add e2eTests project. Try to run tests in specific projects

---
+++ src/Equinor.Procosys.Preservation.WebApi.e2eTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.Procosys.Preservation.WebApi.e2eTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() => Assert.IsTrue(true);
+    }
+}
