daab209b5dc62ed81586e997cb96f387e60a261b
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-26 14:51:04 +0100

    Expand TagsController with endpoint for Reschedule

---
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Tags/RescheduleTagsDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Equinor.Procosys.Preservation.WebApi.Controllers.Tags
+{
+    public class RescheduleTagsDtoValidator : AbstractValidator<RescheduleTagsDto>
+    {
+        public RescheduleTagsDtoValidator()
+        {
+            RuleFor(x => x).NotNull().WithMessage($"{nameof(RescheduleTagsDto)} can't be null");
+
+            RuleFor(x => x.TagDtos)
+                .NotNull()
+                .WithMessage($"{nameof(RescheduleTagsDto.TagDtos)} can't be null");
+        }
+    }
+}
