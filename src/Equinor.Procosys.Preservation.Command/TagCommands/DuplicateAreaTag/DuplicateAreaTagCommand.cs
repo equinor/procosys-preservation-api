03b5c385a0389a3712b93704e1bc911907c76ad7
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-11-04 15:56:05 +0100

    DuplicateAreaTagCommand

---
+++ src/Equinor.Procosys.Preservation.Command/TagCommands/DuplicateAreaTag/DuplicateAreaTagCommand.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using ServiceResult;
+
+namespace Equinor.Procosys.Preservation.Command.TagCommands.DuplicateAreaTag
+{
+    public class DuplicateAreaTagCommand : IRequest<Result<int>>, ITagCommandRequest
+    {
+        public DuplicateAreaTagCommand(
+            int tagId,
+            string disciplineCode,
+            string areaCode,
+            string tagNoSuffix,
+            string description,
+            string remark,
+            string storageArea)
+        {
+            TagId = tagId;
+            DisciplineCode = disciplineCode;
+            AreaCode = areaCode;
+            TagNoSuffix = tagNoSuffix;
+            Description = description;
+            Remark = remark;
+            StorageArea = storageArea;
+        }
+
+        public int TagId { get; }
+        public string DisciplineCode { get; }
+        public string AreaCode { get; }
+        public string TagNoSuffix { get; }
+        public string Description { get; }
+        public string Remark { get; }
+        public string StorageArea { get; }
+    }
+}
---
+++ src/Equinor.Procosys.Preservation.Command/TagCommands/DuplicateAreaTag/DuplicateAreaTagCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.Procosys.Preservation.Command.Validators.ProjectValidators;
+using Equinor.Procosys.Preservation.Command.Validators.TagValidators;
+using FluentValidation;
+
+namespace Equinor.Procosys.Preservation.Command.TagCommands.DuplicateAreaTag
+{
+    public class DuplicateAreaTagCommandValidator : AbstractValidator<DuplicateAreaTagCommand>
+    {
+        public DuplicateAreaTagCommandValidator(ITagValidator tagValidator, IProjectValidator projectValidator)
+        {
+            CascadeMode = CascadeMode.Stop;
+
+            RuleFor(command => command)
+                .MustAsync((command, token) => NotBeAClosedProjectForTagAsync(command.TagId, token))
+                .WithMessage(command => $"Project for tag is closed! Tag={command.TagId}")
+                .MustAsync((command, token) => BeAnExistingTagAsync(command.TagId, token))
+                .WithMessage(command => $"Tag doesn't exist! Tag={command.TagId}");
+
+            async Task<bool> NotBeAClosedProjectForTagAsync(int tagId, CancellationToken token)
+                => !await projectValidator.IsClosedForTagAsync(tagId, token);
+            async Task<bool> BeAnExistingTagAsync(int tagId, CancellationToken token)
+                => await tagValidator.ExistsAsync(tagId, token);
+        }
+    }
+}
