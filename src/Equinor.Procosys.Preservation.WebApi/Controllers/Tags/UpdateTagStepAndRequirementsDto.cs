3cbe814ae96b454937402f78717f80ad2cda7831
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-11-12 14:24:38 +0100

    Modified UpdateTagStepAndRequirementsDto so lists are initialted if client don't send so

--- src/Equinor.Procosys.Preservation.WebApi/Controllers/Tags/UpdateTagStepAndRequirementsDto.cs
+++ src/Equinor.Procosys.Preservation.WebApi/Controllers/Tags/UpdateTagStepAndRequirementsDto.cs
@@ -6,8 +6,8 @@ namespace Equinor.Procosys.Preservation.WebApi.Controllers.Tags
     {
         public string Description { get; set; }
         public int StepId { get; set; }
-        public IList<TagRequirementDto> NewRequirements { get; set; }
-        public IList<UpdatedTagRequirementDto> UpdatedRequirements { get; set; }
+        public IList<TagRequirementDto> NewRequirements { get; set; } = new List<TagRequirementDto>();
+        public IList<UpdatedTagRequirementDto> UpdatedRequirements { get; set; } = new List<UpdatedTagRequirementDto>();
         public string RowVersion { get; set; }
     }
 }
