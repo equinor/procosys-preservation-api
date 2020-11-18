2d5d97f24398e34a7795fb64451991b4dc788e40
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-11-18 17:32:05 +0100

    positive int tests for ReqDef operations

--- src/tests/Equinor.Procosys.Preservation.WebApi.IntegrationTests/RequirementTypes/RequirementTypesControllerTestsHelper.cs
+++ src/tests/Equinor.Procosys.Preservation.WebApi.IntegrationTests/RequirementTypes/RequirementTypesControllerTestsHelper.cs
@@ -30,6 +30,34 @@ namespace Equinor.Procosys.Preservation.WebApi.IntegrationTests.RequirementTypes
             var content = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<RequirementTypeDto>>(content);
         }
+        
+        public static async Task<int> CreateRequirementDefinitionAsync(
+            HttpClient client,
+            int reqTypeId,
+            string title,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+        {
+            var bodyPayload = new
+            {
+                title,
+                sortKey = 10,
+                defaultIntervalWeeks = 4
+            };
+
+            var serializePayload = JsonConvert.SerializeObject(bodyPayload);
+            var content = new StringContent(serializePayload, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync($"{_route}/{reqTypeId}/RequirementDefinitions", content);
+            await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return -1;
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<int>(jsonString);
+        }
 
         public static async Task<string> UpdateRequirementDefinitionAsync(
             HttpClient client,
@@ -52,6 +80,85 @@ namespace Equinor.Procosys.Preservation.WebApi.IntegrationTests.RequirementTypes
             var serializePayload = JsonConvert.SerializeObject(bodyPayload);
             var content = new StringContent(serializePayload, Encoding.UTF8, "application/json");
             var response = await client.PutAsync($"{_route}/{requirementTypeId}/RequirementDefinitions/{requirementDefinitionId}", content);
+            await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public static async Task<string> VoidRequirementDefinitionAsync(
+            HttpClient client,
+            int reqTypeId,
+            int reqDefId,
+            string rowVersion,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+            => await VoidUnvoidRequirementDefinitionAsync(client,
+                reqTypeId,
+                reqDefId,
+                rowVersion,
+                "Void",
+                expectedStatusCode,
+                expectedMessageOnBadRequest);
+
+        public static async Task<string> UnvoidRequirementDefinitionAsync(
+            HttpClient client,
+            int reqTypeId,
+            int reqDefId,
+            string rowVersion,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+            => await VoidUnvoidRequirementDefinitionAsync(client,
+                reqTypeId,
+                reqDefId,
+                rowVersion,
+                "Unvoid",
+                expectedStatusCode,
+                expectedMessageOnBadRequest);
+
+        public static async Task DeleteRequirementDefinitionAsync(
+            HttpClient client,
+            int reqTypeId,
+            int reqDefId,
+            string rowVersion,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+        {
+            var bodyPayload = new
+            {
+                rowVersion
+            };
+            var serializePayload = JsonConvert.SerializeObject(bodyPayload);
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"{_route}/{reqTypeId}/RequirementDefinitions/{reqDefId}")
+            {
+                Content = new StringContent(serializePayload, Encoding.UTF8, "application/json")
+            };
+
+            var response = await client.SendAsync(request);
+            await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+        }
+
+        private static async Task<string> VoidUnvoidRequirementDefinitionAsync(
+            HttpClient client,
+            int reqTypeId,
+            int reqDefId,
+            string rowVersion,
+            string action,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+        {
+            var bodyPayload = new
+            {
+                rowVersion
+            };
+
+            var serializePayload = JsonConvert.SerializeObject(bodyPayload);
+            var content = new StringContent(serializePayload, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync($"{_route}/{reqTypeId}/RequirementDefinitions/{reqDefId}/{action}", content);
             await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
 
             if (response.StatusCode != HttpStatusCode.OK)
