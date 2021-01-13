2ac78d2674583d7553d4a2426401741068a6d334
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-01-13 11:47:31 +0100

    Added FirstName/LastName to TestProfile and simplified IntegrationTestAuthHandler.

---
+++ src/tests/Equinor.Procosys.Preservation.WebApi.IntegrationTests/PersonDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Equinor.Procosys.Preservation.WebApi.IntegrationTests
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
