1e80d09d45ad3004b9ef605f57e903a813dd30c9
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-06-02 16:07:23 +0200

    Added Action int.tests for CrossPlant controller
    (cherry picked from commit eb95f8fbb4c3441c2010c9913d3b23ba449f9aa2)

---
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests.Misc
+{
+    [TestClass]
+    public class CrossPlantControllerTests : CrossPlantControllerTestsBase
+    {
+        [TestMethod]
+        public async Task GetAllActions_AsCrossPlantUser_ShouldGetActions()
+        {
+            // Act
+            var actionDtos = await CrossPlantControllerTestsHelper.GetAllActionsAsync(UserType.CrossPlantUser);
+
+            // Assert
+            Assert.IsNotNull(actionDtos);
+            Assert.IsTrue(actionDtos.Count > 0);
+            AssertClosedActionWithAttachments(
+                actionDtos.SingleOrDefault(a => a.Id == ActionIdUnderTest_WithAttachments_Closed_InPlantA),
+                KnownPlantData.PlantA,
+                KnownPlantData.PlantATitle);
+            AssertClosedActionWithAttachments(
+                actionDtos.SingleOrDefault(a => a.Id == ActionIdUnderTest_WithAttachments_Closed_InPlantB),
+                KnownPlantData.PlantB,
+                KnownPlantData.PlantBTitle);
+        }
+
+        private void AssertClosedActionWithAttachments(ActionDto actionDto, string plantId, string plantTitle)
+        {
+            Assert.IsNotNull(actionDto);
+            Assert.AreEqual(actionDto.PlantId, plantId);
+            Assert.AreEqual(actionDto.PlantTitle, plantTitle);
+            Assert.AreEqual(actionDto.ProjectName, KnownTestData.ProjectName);
+            Assert.AreEqual(actionDto.ProjectDescription, KnownTestData.ProjectDescription);
+            Assert.IsTrue(actionDto.Title.StartsWith(KnownTestData.Action));
+            Assert.IsTrue(actionDto.IsClosed);
+            Assert.IsNotNull(actionDto.TagNo);
+            Assert.IsTrue(actionDto.Id > 0);
+            Assert.IsTrue(actionDto.TagId > 0);
+            Assert.IsTrue(actionDto.AttachmentCount > 0);
+        }
+    }
+}
---
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerTestsBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests.Misc
+{
+    [TestClass]
+    public class CrossPlantControllerTestsBase : TestBase
+    {
+        protected int ActionIdUnderTest_WithAttachments_Closed_InPlantA;
+        protected int ActionIdUnderTest_WithAttachments_Closed_InPlantB;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            ActionIdUnderTest_WithAttachments_Closed_InPlantA
+                = TestFactory.Instance.SeededData[KnownPlantData.PlantA].ActionId_ForActionWithAttachments_Closed;
+            ActionIdUnderTest_WithAttachments_Closed_InPlantB
+                = TestFactory.Instance.SeededData[KnownPlantData.PlantB].ActionId_ForActionWithAttachments_Closed;
+        }
+    }
+}
