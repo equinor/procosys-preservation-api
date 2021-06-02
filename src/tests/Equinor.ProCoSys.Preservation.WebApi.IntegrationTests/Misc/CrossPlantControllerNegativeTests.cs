eb95f8fbb4c3441c2010c9913d3b23ba449f9aa2
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-06-02 16:07:23 +0200

    Added Action int.tests for CrossPlant controller

---
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerNegativeTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests.Misc
+{
+    [TestClass]
+    public class CrossPlantControllerNegativeTests : CrossPlantControllerTestsBase
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+        
+        #region GetAllActions
+        [TestMethod]
+        public async Task GetAllActions_AsAnonymous_ShouldReturnUnauthorized()
+            => await CrossPlantControllerTestsHelper.GetAllActionsAsync(
+                UserType.Anonymous,
+                HttpStatusCode.Unauthorized);
+
+        [TestMethod]
+        public async Task GetAllActions_AsHacker_ShouldReturnForbidden_WhenUnknownPlant()
+            => await CrossPlantControllerTestsHelper.GetAllActionsAsync(
+                UserType.Hacker, 
+                HttpStatusCode.Forbidden,
+                "is not a valid plant");
+
+        [TestMethod]
+        public async Task GetAllActions_AsAdmin_ShouldReturnForbidden_WhenUnknownPlant()
+            => await CrossPlantControllerTestsHelper.GetAllActionsAsync(
+                UserType.LibraryAdmin, 
+                HttpStatusCode.Forbidden,
+                "is not a valid plant");
+
+        [TestMethod]
+        public async Task GetAllActions_AsHacker_ShouldReturnForbidden_WhenPermissionMissing()
+            => await CrossPlantControllerTestsHelper.GetAllActionsAsync(
+                UserType.Hacker, 
+                HttpStatusCode.Forbidden);
+
+        [TestMethod]
+        public async Task GetAllActions_AsAdmin_ShouldReturnForbidden_WhenPermissionMissing()
+            => await CrossPlantControllerTestsHelper.GetAllActionsAsync(
+                UserType.LibraryAdmin, 
+                HttpStatusCode.Forbidden);
+
+        #endregion
+    }
+}
---
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerTestsHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests.Misc
+{
+    public static class CrossPlantControllerTestsHelper
+    {
+        private const string _route = "CrossPlant";
+
+        public static async Task<List<ActionDto>> GetAllActionsAsync(
+            UserType userType,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+        {
+            var response = await TestFactory.Instance.GetHttpClient(userType, null).GetAsync($"{_route}/Actions");
+
+            await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+
+            if (expectedStatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<ActionDto>>(jsonString);
+        }
+    }
+}
