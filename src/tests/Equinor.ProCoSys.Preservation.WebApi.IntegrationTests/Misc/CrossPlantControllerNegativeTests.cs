aead8cf07fc9d42758862971ee5cf0041b74559e
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-06-05 19:18:19 +0200

    Integration tests for

--- src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerNegativeTests.cs
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerNegativeTests.cs
@@ -7,6 +7,39 @@ namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests.Misc
     [TestClass]
     public class CrossPlantControllerNegativeTests : CrossPlantControllerTestsBase
     {
+        #region GetAllTags
+        [TestMethod]
+        public async Task GetTags_AsAnonymous_ShouldReturnUnauthorized()
+            => await CrossPlantControllerTestsHelper.GetTagsAsync(
+                UserType.Anonymous,
+                HttpStatusCode.Unauthorized);
+
+        [TestMethod]
+        public async Task GetTags_AsHacker_ShouldReturnForbidden()
+            => await CrossPlantControllerTestsHelper.GetTagsAsync(
+                UserType.Hacker, 
+                HttpStatusCode.Forbidden);
+
+        [TestMethod]
+        public async Task GetTags_AsAdmin_ShouldReturnForbidden()
+            => await CrossPlantControllerTestsHelper.GetTagsAsync(
+                UserType.LibraryAdmin, 
+                HttpStatusCode.Forbidden);
+
+        [TestMethod]
+        public async Task GetTags_AsPreserver_ShouldReturnForbidden()
+            => await CrossPlantControllerTestsHelper.GetTagsAsync(
+                UserType.Preserver, 
+                HttpStatusCode.Forbidden);
+
+        [TestMethod]
+        public async Task GetTags_AsPlanner_ShouldReturnForbidden()
+            => await CrossPlantControllerTestsHelper.GetTagsAsync(
+                UserType.Planner, 
+                HttpStatusCode.Forbidden);
+
+        #endregion
+
         #region GetAllActions
         [TestMethod]
         public async Task GetActions_AsAnonymous_ShouldReturnUnauthorized()
--- src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerTestsHelper.cs
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/Misc/CrossPlantControllerTestsHelper.cs
@@ -26,5 +26,23 @@ namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests.Misc
             var jsonString = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<ActionDto>>(jsonString);
         }
+
+        public static async Task<List<TagDto>> GetTagsAsync(
+            UserType userType,
+            HttpStatusCode expectedStatusCode = HttpStatusCode.OK,
+            string expectedMessageOnBadRequest = null)
+        {
+            var response = await TestFactory.Instance.GetHttpClient(userType, null).GetAsync($"{_route}/Tags");
+
+            await TestsHelper.AssertResponseAsync(response, expectedStatusCode, expectedMessageOnBadRequest);
+
+            if (expectedStatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<TagDto>>(jsonString);
+        }
     }
 }
