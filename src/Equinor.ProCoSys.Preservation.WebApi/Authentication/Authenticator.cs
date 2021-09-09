4dedd103a587ed8535e2ed5983f4a310e63f3663
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-09-09 08:16:46 +0200

    Logstatement for debugging

--- src/Equinor.ProCoSys.Preservation.WebApi/Authentication/Authenticator.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/Authentication/Authenticator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Equinor.ProCoSys.Preservation.MainApi.Client;
 using Equinor.ProCoSys.Preservation.WebApi.Misc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Identity.Client;
 
@@ -11,12 +12,17 @@ namespace Equinor.ProCoSys.Preservation.WebApi.Authentication
     public class Authenticator : IBearerTokenProvider, IBearerTokenSetter, IApplicationAuthenticator
     {
         private readonly IOptions<AuthenticatorOptions> _options;
+        private readonly ILogger<BearerTokenApiClient> _logger;
         private bool _canUseOnBehalfOf;
         private string _requestToken;
         private string _onBehalfOfUserToken;
         private string _applicationToken;
 
-        public Authenticator(IOptions<AuthenticatorOptions> options) => _options = options;
+        public Authenticator(IOptions<AuthenticatorOptions> options, ILogger<BearerTokenApiClient> logger)
+        {
+            _options = options;
+            _logger = logger;
+        }
 
         public void SetBearerToken(string token, bool isUserToken = true)
         {
@@ -30,11 +36,15 @@ namespace Equinor.ProCoSys.Preservation.WebApi.Authentication
             {
                 if (_onBehalfOfUserToken == null)
                 {
+                    var apiSecret = _options.Value.PreservationApiSecret;
+                    var secret = $"{apiSecret.Substring(0, 3)}***{apiSecret.Substring(apiSecret.Length - 3, 3)}";
+                    _logger.LogInformation($"Getting onbehalf of token using {secret} for {_options.Value.PreservationApiClientId}");
                     var app = CreateConfidentialPreservationClient();
 
                     var tokenResult = await app
                         .AcquireTokenOnBehalfOf(new List<string> { _options.Value.MainApiScope }, new UserAssertion(_requestToken))
                         .ExecuteAsync();
+                    _logger.LogInformation("Got onbehalf of token");
 
                     _onBehalfOfUserToken = tokenResult.AccessToken;
                 }
