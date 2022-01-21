6f5a667066d46e40ef9f6dbce71a4678aa4fd12b
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-01-21 08:40:34 +0100

    Remove TagDeleted event

---
+++ src/Equinor.ProCoSys.Preservation.Infrastructure/Migrations/20220121073945_removeTagDeletedEvent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Preservation.Infrastructure.Migrations
+{
+    public partial class removeTagDeletedEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_History_constraint_history_check_valid_event_type",
+                table: "History");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_History_constraint_history_check_valid_event_type",
+                table: "History",
+                sql: "EventType in ('RequirementAdded','RequirementDeleted','RequirementVoided','RequirementUnvoided','RequirementPreserved','TagVoided','TagUnvoided','TagCreated','PreservationStarted','PreservationCompleted','IntervalChanged','JourneyChanged','StepChanged','TransferredManually','TransferredAutomatically','ActionAdded','ActionClosed','Rescheduled','UndoPreservationStarted')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_History_constraint_history_check_valid_event_type",
+                table: "History");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_History_constraint_history_check_valid_event_type",
+                table: "History",
+                sql: "EventType in ('RequirementAdded','RequirementDeleted','RequirementVoided','RequirementUnvoided','RequirementPreserved','TagVoided','TagUnvoided','TagCreated','TagDeleted','PreservationStarted','PreservationCompleted','IntervalChanged','JourneyChanged','StepChanged','TransferredManually','TransferredAutomatically','ActionAdded','ActionClosed','Rescheduled','UndoPreservationStarted')");
+        }
+    }
+}
