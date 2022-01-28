8caad36d169220ca81d1b98bd070e06564ce1c40
Kristoffer Steen <KRST@equinor.com>
2022-01-28 11:17:21 +0100

    80882: handle incoming certificate accepted from bus instead of poll

---
+++ src/Equinor.ProCoSys.Preservation.WebApi/Synchronization/ICertificateEventProcessorService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Equinor.ProCoSys.Preservation.WebApi.Synchronization
+{
+    public interface ICertificateEventProcessorService
+    {
+        Task ProcessCertificateEvent(string messageJson);
+    }
+}
