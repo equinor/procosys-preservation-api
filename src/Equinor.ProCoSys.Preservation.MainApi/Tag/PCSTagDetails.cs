4b0a29868c2bd2309d16810a98525ace7aacb7bd
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-12-01 10:43:40 +0100

    Add ProCoSysGuid for Tag, McPkg and CommPkg to TagDetails DTO used when getting from Main

--- src/Equinor.ProCoSys.Preservation.MainApi/Tag/PCSTagDetails.cs
+++ src/Equinor.ProCoSys.Preservation.MainApi/Tag/PCSTagDetails.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace Equinor.ProCoSys.Preservation.MainApi.Tag
 {
@@ -6,14 +7,17 @@ namespace Equinor.ProCoSys.Preservation.MainApi.Tag
     public class PCSTagDetails
     {
         public long Id { get; set; }
+        public Guid ProCoSysGuid { get; set; }
         public string AreaCode { get; set; }
         public string AreaDescription { get; set; }
         public string CallOffNo { get; set; }
         public string CommPkgNo { get; set; }
+        public Guid? CommPkgProCoSysGuid { get; set; }
         public string Description { get; set; }
         public string DisciplineCode { get; set; }
         public string DisciplineDescription { get; set; }
         public string McPkgNo { get; set; }
+        public Guid? McPkgProCoSysGuid { get; set; }
         public string ProjectDescription { get; set; }
         public string PurchaseOrderNo { get; set; }
         public string RegisterCode { get; set; }
