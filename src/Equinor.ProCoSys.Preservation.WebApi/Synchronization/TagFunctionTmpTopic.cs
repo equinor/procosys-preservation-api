bc8ac640e2fc038cfb86352973be088b1b861723
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-12-12 11:33:45 +0100

    Temporary topics with string as ProCoSysGuids

---
+++ src/Equinor.ProCoSys.Preservation.WebApi/Synchronization/TagFunctionTmpTopic.cs
@@ -0,0 +1,61 @@
+﻿namespace Equinor.ProCoSys.Preservation.WebApi.Synchronization
+{
+    public class TagFunctionTmpTopic
+    {
+        public const string TopicName = "tagfunction";
+
+        public string Plant
+        {
+            get;
+            set;
+        }
+
+        public string ProCoSysGuid
+        {
+            get;
+            set;
+        }
+
+        public string Behavior
+        {
+            get;
+            set;
+        }
+
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        public string CodeOld
+        {
+            get;
+            set;
+        }
+
+        public string RegisterCode
+        {
+            get;
+            set;
+        }
+
+        public string RegisterCodeOld
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public bool IsVoided
+        {
+            get;
+            set;
+        }
+    }
+}
