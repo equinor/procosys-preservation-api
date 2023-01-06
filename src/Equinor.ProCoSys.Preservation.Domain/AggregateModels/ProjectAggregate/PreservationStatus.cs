70e40fa815e4e147c1808b11d01b6a934db98109
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-01-06 08:01:52 +0100

    Added new PreservationStatus InService. Updated rules in tag according to new state, and added unit tests for these

--- src/Equinor.ProCoSys.Preservation.Domain/AggregateModels/ProjectAggregate/PreservationStatus.cs
+++ src/Equinor.ProCoSys.Preservation.Domain/AggregateModels/ProjectAggregate/PreservationStatus.cs
@@ -4,6 +4,7 @@
     {
         NotStarted,
         Active,
-        Completed
+        Completed,
+        InService
     }
 }
---
+++ src/Equinor.ProCoSys.Preservation.Domain/Events/PreservationSetInServiceEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MediatR;
+
+namespace Equinor.ProCoSys.Preservation.Domain.Events
+{
+    public class PreservationSetInServiceEvent : INotification
+    {
+        public PreservationSetInServiceEvent(
+            string plant,
+            Guid objectGuid)
+        {
+            Plant = plant;
+            ObjectGuid = objectGuid;
+        }
+        public string Plant { get; }
+        public Guid ObjectGuid { get; }
+    }
+}
