524caeacad2cfd967164949bff8db44894526ea6
Christian Alejandro Hormazabal Martin <chorm@equinor.com>
2023-03-08 11:28:20 +0100

    nuget packages

--- src/Equinor.ProCoSys.Preservation.WebApi/Behaviors/CheckValidProjectBehavior.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/Behaviors/CheckValidProjectBehavior.cs
@@ -17,7 +17,7 @@ namespace Equinor.ProCoSys.Preservation.WebApi.Behaviors
             _projectChecker = projectChecker;
         }
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
             var typeName = request.GetGenericTypeName();
 
--- src/Equinor.ProCoSys.Preservation.WebApi/DiModules/MediatorModule.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/DiModules/MediatorModule.cs
@@ -11,11 +11,14 @@ namespace Equinor.ProCoSys.Preservation.WebApi.DIModules
     {
         public static void AddMediatrModules(this IServiceCollection services)
         {
-            services.AddMediatR(
-                typeof(MediatorModule).GetTypeInfo().Assembly,
-                typeof(ICommandMarker).GetTypeInfo().Assembly,
-                typeof(IQueryMarker).GetTypeInfo().Assembly
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssemblies(
+                    typeof(ICommandMarker).GetTypeInfo().Assembly,
+                    typeof(MediatorModule).GetTypeInfo().Assembly,
+                    typeof(IQueryMarker).GetTypeInfo().Assembly
+                )
             );
+
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CheckValidProjectBehavior<,>));
