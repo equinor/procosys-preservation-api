08df9f28aa3a33a5d81a6afb39816e60f5ab6d3f
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-06-05 11:28:40 +0200

    Migrate tag Guids

---
+++ src/Equinor.ProCoSys.Preservation.Infrastructure/Migrations/20230605084137_MigrateTagGuid.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Preservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateTagGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Tags set Guid = ObjectGuid");
+            migrationBuilder.Sql("update Tags set Guid = ProCoSysGuid where ProCoSysGuid is not null");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
