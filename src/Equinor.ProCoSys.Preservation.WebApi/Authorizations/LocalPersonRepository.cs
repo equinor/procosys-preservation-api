817e4f653bcec7a25aab97afe832e76e22780252
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-06-05 13:32:19 +0200

    use IReadOnlyContext in LocalPersonRepository

--- src/Equinor.ProCoSys.Preservation.WebApi/Authorizations/LocalPersonRepository.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/Authorizations/LocalPersonRepository.cs
@@ -2,16 +2,23 @@
 using Equinor.ProCoSys.Preservation.Domain.AggregateModels.PersonAggregate;
 using System.Threading.Tasks;
 using System;
+using System.Linq;
+using Equinor.ProCoSys.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace Equinor.ProCoSys.Preservation.WebApi.Authorizations;
 
 public class LocalPersonRepository : ILocalPersonRepository
 {
-    private readonly IPersonRepository _personRepository;
+    private readonly IReadOnlyContext _context;
 
-    public LocalPersonRepository(IPersonRepository personRepository)
-        => _personRepository = personRepository;
+    public LocalPersonRepository(IReadOnlyContext context) => _context = context;
 
     public async Task<bool> ExistsAsync(Guid userOid)
-        => await _personRepository.GetByOidAsync(userOid) != null;
+    {
+        var exists = await (from person in _context.QuerySet<Person>()
+            where person.Guid == userOid
+            select person).AnyAsync();
+        return exists;
+    }
 }
