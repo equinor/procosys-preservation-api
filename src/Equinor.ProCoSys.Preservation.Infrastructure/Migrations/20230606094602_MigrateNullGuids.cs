8ad54b556779adc0cef828ae18a91a7a46b1d43b
Jan Inge Dalsbø <19545528+janid1967@users.noreply.github.com>
2023-06-07 07:00:59 +0200

    Extra migration for null-guids (#935)

---
+++ src/Equinor.ProCoSys.Preservation.Infrastructure/Migrations/20230606094602_MigrateNullGuids.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Preservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateNullGuids : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update Tags set Guid = ObjectGuid where ProCoSysGuid is null or ProCoSysGuid = '00000000-0000-0000-0000-000000000000'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
