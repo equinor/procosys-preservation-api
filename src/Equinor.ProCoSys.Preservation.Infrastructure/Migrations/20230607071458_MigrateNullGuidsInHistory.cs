4c78d4b2b9ce7204d1eef04be4adb497c84f9a65
Jan Inge Dalsbø <JDALSB@equinor.com>
2023-06-07 09:23:26 +0200

    Also migrate History records where new SourceGuid is still null guid after previous migrations.

---
+++ src/Equinor.ProCoSys.Preservation.Infrastructure/Migrations/20230607071458_MigrateNullGuidsInHistory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Preservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateNullGuidsInHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Also migrate History records where new SourceGuid is still null guid after previous migrations.
+            migrationBuilder.Sql("update History set SourceGuid = ObjectGuid where SourceGuid = '00000000-0000-0000-0000-000000000000'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
