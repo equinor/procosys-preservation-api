99b13aacff87294e01e24847b294b5cd57dde6f6
haavard-hoijord <hhoij@equinor.com>
2024-07-02 11:27:04 +0200

    RequirementDefinition event handlers

---
+++ src/Equinor.ProCoSys.Preservation.Command/EventHandlers/IntegrationEvents/RequirementDefinitionAddedEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Preservation.Command.EventPublishers;
+using Equinor.ProCoSys.Preservation.Domain.Events;
+using MediatR;
+
+namespace Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents;
+
+public class RequirementDefinitionAddedEventHandler : INotificationHandler<RequirementDefinitionAddedEvent>
+{
+    private readonly IIntegrationEventPublisher _integrationEventPublisher;
+    private readonly ICreateEventHelper _createEventHelper;
+
+    public RequirementDefinitionAddedEventHandler(IIntegrationEventPublisher integrationEventPublisher, ICreateEventHelper createEventHelper)
+    {
+        _integrationEventPublisher = integrationEventPublisher;
+        _createEventHelper = createEventHelper;
+    }
+
+    public async Task Handle(RequirementDefinitionAddedEvent notification, CancellationToken cancellationToken)
+    {
+        var addedEvent = await _createEventHelper.CreateRequirementDefinitionEvent(notification.RequirementDefinition);
+        await _integrationEventPublisher.PublishAsync(addedEvent, cancellationToken);
+    }
+}
---
+++ src/Equinor.ProCoSys.Preservation.Command/EventHandlers/IntegrationEvents/RequirementDefinitionDeletedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Preservation.Command.EventPublishers;
+using Equinor.ProCoSys.Preservation.Command.Events;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.ProCoSys.Preservation.Domain.Events;
+using MediatR;
+
+namespace Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents;
+
+public class RequirementDefinitionDeletedEventHandler  : INotificationHandler<RequirementDefinitionDeletedEvent>
+{
+    private readonly IIntegrationEventPublisher _integrationEventPublisher;
+    public RequirementDefinitionDeletedEventHandler(IIntegrationEventPublisher integrationEventPublisher) => _integrationEventPublisher = integrationEventPublisher;
+
+    public async Task Handle(RequirementDefinitionDeletedEvent notification, CancellationToken cancellationToken)
+    {
+        var deleteEvent = new DeleteEvent(notification.RequirementDefinition.Guid, notification.RequirementDefinition.Plant, "");
+        await _integrationEventPublisher.PublishAsync(deleteEvent, cancellationToken);
+    }
+}
