979c767167b745a7b02bdaa13d61ba6c0537323b
Richard Dungal <richard.dungal@bouvet.no>
2024-10-23 11:58:30 +0200

    Tag added domain event refactor

--- src/Equinor.ProCoSys.Preservation.Command/EventHandlers/HistoryEvents/ActionAddedEventHandler.cs
+++ src/Equinor.ProCoSys.Preservation.Command/EventHandlers/HistoryEvents/ActionAddedEventHandler.cs
@@ -4,26 +4,20 @@ using Equinor.ProCoSys.Preservation.Domain.AggregateModels.HistoryAggregate;
 using MediatR;
 using Equinor.ProCoSys.Preservation.Domain.Events;
 using Equinor.ProCoSys.Common.Misc;
-using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
 
 namespace Equinor.ProCoSys.Preservation.Command.EventHandlers.HistoryEvents
 {
-    public class ActionAddedEventHandler : INotificationHandler<ActionAddedEvent>
+    public class ActionAddedEventHandler : INotificationHandler<TagActionAddedEvent>
     {
         private readonly IHistoryRepository _historyRepository;
-        private readonly IProjectRepository _projectRepository;
 
-        public ActionAddedEventHandler(IHistoryRepository historyRepository, IProjectRepository projectRepository)
-        {
-            _historyRepository = historyRepository;
-            _projectRepository = projectRepository;
-        }
+        public ActionAddedEventHandler(IHistoryRepository historyRepository) => _historyRepository = historyRepository;
 
-        public Task Handle(ActionAddedEvent notification, CancellationToken cancellationToken)
+        public Task Handle(TagActionAddedEvent notification, CancellationToken cancellationToken)
         {
             var eventType = EventType.ActionAdded;
-            var description = $"{eventType.GetDescription()} - '{notification.Entity.Title}'";
-            var history = new History(notification.Plant, description, notification.SourceGuid, ObjectType.Tag, eventType);
+            var description = $"{eventType.GetDescription()} - '{notification.Action.Title}'";
+            var history = new History(notification.Action.Plant, description, notification.Entity.Guid, ObjectType.Tag, eventType);
             _historyRepository.Add(history);
             return Task.CompletedTask;
         }
