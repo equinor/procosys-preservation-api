2887a348879d6daf3b3a17807fecda85a95ca6b1
Richard Dungal <richard.dungal@bouvet.no>
2024-10-23 15:10:47 +0200

    RequirementDefinitionFieldAddedEventHandler

---
+++ src/Equinor.ProCoSys.Preservation.Command/EventHandlers/IntegrationEvents/RequirementDefinitionFieldAddedEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents.EventHelpers;
+using Equinor.ProCoSys.Preservation.Command.EventPublishers;
+using Equinor.ProCoSys.Preservation.Command.Events;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Equinor.ProCoSys.Preservation.Domain.Events;
+using MediatR;
+
+namespace Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents;
+
+public class RequirementDefinitionFieldAddedEventHandler : INotificationHandler<EntityAddedChildEntityEvent<RequirementDefinition, Field>>
+{
+    private readonly ICreateChildEventHelper<RequirementDefinition, Field, FieldEvent> _createTagEventHelper;
+    private readonly IIntegrationEventPublisher _integrationEventPublisher;
+
+    public RequirementDefinitionFieldAddedEventHandler(ICreateChildEventHelper<RequirementDefinition, Field, FieldEvent> createTagEventHelper, IIntegrationEventPublisher integrationEventPublisher)
+    {
+        _createTagEventHelper = createTagEventHelper;
+        _integrationEventPublisher = integrationEventPublisher;
+    }
+
+    public async Task Handle(EntityAddedChildEntityEvent<RequirementDefinition, Field> notification, CancellationToken cancellationToken)
+    {
+        var integrationEvent = await _createTagEventHelper.CreateEvent(notification.Entity, notification.ChildEntity);
+        await _integrationEventPublisher.PublishAsync(integrationEvent, cancellationToken);
+    }
+}
