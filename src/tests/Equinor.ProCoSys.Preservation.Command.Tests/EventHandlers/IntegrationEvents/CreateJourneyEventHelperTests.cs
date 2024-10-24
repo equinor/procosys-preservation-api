0cfc6992de2a3de297eed46f8a075c1b07f951c1
Richard Dungal <richard.dungal@bouvet.no>
2024-10-24 11:04:01 +0200

    CreateJourneyEventHelperTests

---
+++ src/tests/Equinor.ProCoSys.Preservation.Command.Tests/EventHandlers/IntegrationEvents/CreateJourneyEventHelperTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Common.Misc;
+using Equinor.ProCoSys.Common.Time;
+using Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents.EventHelpers;
+using Equinor.ProCoSys.Preservation.Command.Events;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ModeAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.PersonAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ResponsibleAggregate;
+using Equinor.ProCoSys.Preservation.Test.Common;
+using Equinor.ProCoSys.Preservation.Test.Common.ExtensionMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Action = Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate.Action;
+
+namespace Equinor.ProCoSys.Preservation.Command.Tests.EventHandlers.IntegrationEvents;
+
+[TestClass]
+public class CreateJourneyEventHelperTests
+{
+    private const string TestPlant = "PCS$PlantA";
+    private static DateTime TestTime => DateTime.Parse("2012-12-12T11:22:33Z").ToUniversalTime();
+    private static Guid TestGuid => new("11111111-1111-1111-1111-111111111111");
+
+    private Journey _journey;
+    private Person _person;
+    private CreateJourneyEventHelper _dut;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Arrange
+        var timeProvider = new ManualTimeProvider(TestTime);
+        TimeService.SetProvider(timeProvider);
+        
+        _journey = new Journey(TestPlant, "Test Title");
+        
+        _person = new Person(TestGuid, "Test", "Person");
+
+        var personRepositoryMock = new Mock<IPersonRepository>();
+        personRepositoryMock.Setup(x => x.GetReadOnlyByIdAsync(It.IsAny<int>())).ReturnsAsync(_person);
+        
+        _dut = new CreateJourneyEventHelper(personRepositoryMock.Object);
+    }
+
+    [DataTestMethod]
+    [DataRow(nameof(JourneyEvent.Plant), TestPlant)]
+    [DataRow(nameof(JourneyEvent.Title), "Test Title")]
+    [DataRow(nameof(JourneyEvent.IsVoided), false)]
+    public async Task CreateEvent_ShouldCreateActionEventExpectedValues(string property, object expected)
+    {
+        // Act
+        var integrationEvent = await _dut.CreateEvent(_journey);
+        var result = integrationEvent.GetType()
+            .GetProperties()
+            .Single(p => p.Name == property)
+            .GetValue(integrationEvent);
+
+        // Assert
+        Assert.AreEqual(result, expected);
+    }
+    
+    [TestMethod]
+    public async Task CreateEvent_ShouldCreateActionEventExpectedProCoSysGuidValue()
+    {
+        // Arrange
+        var expected = _journey.Guid;
+        
+        // Act
+        var integrationEvent = await _dut.CreateEvent(_journey);
+        var result = integrationEvent.ProCoSysGuid;
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public async Task CreateEvent_ShouldCreateActionEventExpectedCreatedByGuidValue()
+    {
+        // Arrange
+        _journey.SetCreated(_person);
+        
+        // Act
+        var integrationEvent = await _dut.CreateEvent(_journey);
+        var result = integrationEvent.CreatedByGuid;
+
+        // Assert
+        Assert.AreEqual(TestGuid, result);
+    }
+
+    [TestMethod]
+    public async Task CreateEvent_ShouldCreateActionEventWithExpectedCreatedAtUtcValue()
+    {
+        // Arrange
+        _journey.SetCreated(_person);
+
+        // Act
+        var integrationEvent = await _dut.CreateEvent(_journey);
+
+        // Assert
+        Assert.AreEqual(TestTime, integrationEvent.CreatedAtUtc);
+    }
+    
+    [TestMethod]
+    public async Task CreateEvent_ShouldCreateActionEventWithModifiedByGuid()
+    {
+        // Arrange
+        _journey.SetModified(_person);
+        
+        // Act
+        var integrationEvent = await _dut.CreateEvent(_journey);
+        var result = integrationEvent.ModifiedByGuid;
+
+        // Assert
+        Assert.AreEqual(TestGuid, result);
+    }
+
+    [TestMethod]
+    public async Task CreateEvent_ShouldCreateActionEventWithExpectedModifiedAtUtcValue()
+    {
+        // Arrange
+        _journey.SetModified(_person);
+
+        // Act
+        var integrationEvent = await _dut.CreateEvent(_journey);
+
+        // Assert
+        Assert.AreEqual(TestTime, integrationEvent.ModifiedAtUtc);
+    }
+}
