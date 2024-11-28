3c3f5b00178beca2681d1c50d65307e8a9efa692
Richard Thor Fridriksson Dungal <54837611+richard1927@users.noreply.github.com>
2024-11-28 11:59:51 +0100

    800 warning clarification (#984)

--- src/Equinor.ProCoSys.Preservation.WebApi/Synchronization/McPkgTmpTopic.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/Synchronization/McPkgTmpTopic.cs
@@ -4,6 +4,12 @@ using System.Collections.Generic;
 namespace Equinor.ProCoSys.Preservation.WebApi.Synchronization
 {
     [Obsolete("Revert to use Topic-classes from Equinor.ProCoSys.PcsServiceBus after ensuring real Guids on bus.")]
+    // Clarification: "real Guids on bus" refers to the ProCoSysGuid message property in the service bus messages.
+    // Currently (2024-11-28) they look something like this; EB38CCCAAE98D926E0532810000AC5B2,
+    // instead of a real Guid like 123e4567-e89b-12d3-a456-426614174000.
+    // The Equinor.ProCoSys.PcsServiceBus classes contain a ProCoSysGuid property of type Guid.
+    // When we try to deserialize the current ProCoSysGuid message property to a Guid, we get an exception.
+    // This Tmp class mirrors the properties of the original class, but with ProCoSysGuid as a string.
     public class McPkgTmpTopic
     {
         public string Plant
