f90bde0cf49b22f7f2fb658dfc665c75e29c7013
Richard Dungal <richard.dungal@bouvet.no>
2024-12-04 16:25:42 +0100

    tests

---
+++ src/tests/Equinor.ProCoSys.Preservation.Command.Tests/EventHandlers/IntegrationEvents/CreateTagDeletedEventHelperTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents.EventHelpers;
+using Equinor.ProCoSys.Preservation.Command.Events;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.ProCoSys.Preservation.Command.Tests.EventHandlers.IntegrationEvents;
+
+[TestClass]
+public class CreateTagDeletedEventHelperTests
+{
+    private const string TestPlant = "PCS$PlantA";
+    private const string TestProjectName = "Test Project";
+    private CreateTagDeleteEventHelper _dut;
+    private Tag _tag;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Arrange
+        var stepMock = new Mock<Step>();
+        stepMock.SetupGet(s => s.Plant).Returns(TestPlant);
+        
+        var tagRequirementMock = new Mock<TagRequirement>();
+        tagRequirementMock.SetupGet(r => r.Plant).Returns(TestPlant);
+        _tag = new Tag(TestPlant, TagType.Standard, Guid.NewGuid(), "Tag1", "", stepMock.Object,
+            new List<TagRequirement> { tagRequirementMock.Object });
+        
+        var project = new Project(TestPlant, TestProjectName, "Test Project Description", Guid.NewGuid());
+        project.AddTag(_tag);
+
+        var mockProjectRepository = new Mock<IProjectRepository>();
+        mockProjectRepository.Setup(x => x.GetProjectOnlyByTagGuidAsync(It.IsAny<Guid>())).ReturnsAsync(project);
+        
+        _dut = new CreateTagDeleteEventHelper(mockProjectRepository.Object);
+    }
+
+    [DataTestMethod]
+    [DataRow(nameof(RequirementTypeDeleteEvent.Plant), TestPlant)]
+    [DataRow(nameof(RequirementTypeDeleteEvent.ProjectName), TestProjectName)]
+    [DataRow(nameof(RequirementTypeDeleteEvent.Behavior), "delete")]
+    public async Task CreateEvent_ShouldCreateRequirementDefinitionEventExpectedValues(string property, object expected)
+    {
+        var deletionEvent = await _dut.CreateEvent(_tag);
+        var result = deletionEvent.GetType()
+            .GetProperties()
+            .Single(p => p.Name == property)
+            .GetValue(deletionEvent);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public async Task CreateEvent_ShouldCreateRequirementDefinitionEventWithExpectedProCoSysGuid()
+    {
+        // Arrange
+        var expected = _tag.Guid;
+        
+        // Act
+        var deletionEvent = await _dut.CreateEvent(_tag);
+        var result = deletionEvent.ProCoSysGuid;
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ src/tests/Equinor.ProCoSys.Preservation.Command.Tests/EventHandlers/IntegrationEvents/TagDeletedEventHandlerTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents;
+using Equinor.ProCoSys.Preservation.Command.EventHandlers.IntegrationEvents.EventHelpers;
+using Equinor.ProCoSys.Preservation.Command.EventPublishers;
+using Equinor.ProCoSys.Preservation.Command.Events;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.JourneyAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.ProCoSys.Preservation.Domain.Events;
+using Equinor.ProCoSys.Preservation.MessageContracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Equinor.ProCoSys.Preservation.Command.Tests.EventHandlers.IntegrationEvents;
+
+[TestClass]
+public class TagDeletedEventHandlerTests
+{
+    private const string TestPlant = "PCS$PlantA";
+    private TagDeletedEventHandler _dut;
+    private IIntegrationEvent _publishedEvent;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Arrange
+        
+        var mockCreateEventHelper = new Mock<ICreateEventHelper<Tag, TagDeleteEvent>>();
+        var emptyDeleteEvent = new TagDeleteEvent(Guid.Empty, string.Empty, string.Empty);
+        mockCreateEventHelper.Setup(x => x.CreateEvent(It.IsAny<Tag>())).ReturnsAsync(emptyDeleteEvent);
+        
+        var mockPublisher = new Mock<IIntegrationEventPublisher>();
+        mockPublisher.Setup(x => x.PublishAsync(It.IsAny<IIntegrationEvent>(), default))
+            .Callback<IIntegrationEvent, CancellationToken>((e, _) => _publishedEvent = e);
+
+        _publishedEvent = null;
+
+        _dut = new TagDeletedEventHandler(mockCreateEventHelper.Object, mockPublisher.Object);
+    }
+
+    [TestMethod]
+    public async Task Handle_ShouldSendIntegrationEvent()
+    {
+        // Arrange
+        var stepMock = new Mock<Step>();
+        stepMock.SetupGet(s => s.Plant).Returns(TestPlant);
+        
+        var tagRequirementMock = new Mock<TagRequirement>();
+        tagRequirementMock.SetupGet(r => r.Plant).Returns(TestPlant);
+        var tag = new Tag(TestPlant, TagType.Standard, Guid.NewGuid(), "Tag1", "", stepMock.Object,
+            new List<TagRequirement> { tagRequirementMock.Object });
+        
+        var domainEvent = new DeletedEvent<Tag>(tag);
+
+        // Act
+        await _dut.Handle(domainEvent, default);
+
+        // Assert
+        Assert.IsInstanceOfType<TagDeleteEvent>(_publishedEvent);
+    }
+}
