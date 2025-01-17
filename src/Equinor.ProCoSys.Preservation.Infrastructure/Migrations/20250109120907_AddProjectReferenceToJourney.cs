fd1bc0d39b0f0cc33d4a6560a10f1feda3e05bfb
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2025-01-17 10:02:21 +0100

    Add migration files

---
+++ src/Equinor.ProCoSys.Preservation.Infrastructure/Migrations/20250109120907_AddProjectReferenceToJourney.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Equinor.ProCoSys.Preservation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectReferenceToJourney : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectId",
+                table: "Journeys",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FieldType",
+                table: "FieldValues",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AttachmentType",
+                table: "Attachments",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journeys_ProjectId",
+                table: "Journeys",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Journeys_Projects_ProjectId",
+                table: "Journeys",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Journeys_Projects_ProjectId",
+                table: "Journeys");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Journeys_ProjectId",
+                table: "Journeys");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "Journeys");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FieldType",
+                table: "FieldValues",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AttachmentType",
+                table: "Attachments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
