56a30eb68a9eed4e87f3235c6f62b426e78e2c0c
Richard Dungal <richard.dungal@bouvet.no>
2025-01-27 20:06:45 +0100

    Removing redundant comment

--- src/Equinor.ProCoSys.Preservation.WebApi/ApplicationOptions.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/ApplicationOptions.cs
@@ -2,10 +2,6 @@
 
 namespace Equinor.ProCoSys.Preservation.WebApi
 {
-    /// <summary>
-    /// Options for the application. Read from application configuration via IOptionsMonitor.
-    /// "Mapped" to the generic IAuthenticatorOptions
-    /// </summary>
     public class ApplicationOptions
     {
         public Guid ObjectId { get; set; }
