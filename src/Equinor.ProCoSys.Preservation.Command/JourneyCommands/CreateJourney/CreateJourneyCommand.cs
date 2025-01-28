f6f7a6e97be68e8404098b1783598842d18c9729
Edward Brunton <42774600+EdwardBrunton@users.noreply.github.com>
2025-01-28 11:56:41 +0100

    pr comment

--- src/Equinor.ProCoSys.Preservation.Command/JourneyCommands/CreateJourney/CreateJourneyCommand.cs
+++ src/Equinor.ProCoSys.Preservation.Command/JourneyCommands/CreateJourney/CreateJourneyCommand.cs
@@ -6,6 +6,6 @@ namespace Equinor.ProCoSys.Preservation.Command.JourneyCommands.CreateJourney
     public class CreateJourneyCommand(string title, string projectName = null) : IRequest<Result<int>>
     {
         public string Title { get; } = title;
-        public string ProjectName { get; set; } = projectName;
+        public string ProjectName { get; } = projectName;
     }
 }
