29318a888ea6f8ed1a86d84d5152b1a28ddca575
Richard Dungal <richard.dungal@bouvet.no>
2025-01-30 21:29:45 +0100

    r

--- src/Equinor.ProCoSys.Preservation.WebApi/DiModules/ServiceBusConfig.cs
+++ src/Equinor.ProCoSys.Preservation.WebApi/DiModules/ServiceBusConfig.cs
@@ -11,28 +11,29 @@ public static class ServiceBusConfig
 {
     public static void ConfigureServiceBus(this WebApplicationBuilder builder)
     {
-        if (builder.IsServiceBusEnabled())
+        if (!builder.IsServiceBusEnabled())
         {
-            // Env variable used in radix. Configuration is added for easier use locally
-            // Url will be validated during startup of service bus integration and give a
-            // Uri exception if invalid.
- 
-            builder.Services.AddPcsServiceBusIntegration(options => options
-                .UseBusConnection(builder.GetConfig<string>("ConnectionStrings:ServiceBus"))
-                .WithLeaderElector(builder.GetLeaderElectorUri())
-                .WithRenewLeaseInterval(builder.GetConfig<int>("ServiceBus:LeaderElectorRenewLeaseInterval"))
-                .WithSubscription(PcsTopicConstants.Tag, "preservation_tag")
-                .WithSubscription(PcsTopicConstants.TagFunction, "preservation_tagfunction")
-                .WithSubscription(PcsTopicConstants.Project, "preservation_project")
-                .WithSubscription(PcsTopicConstants.CommPkg, "preservation_commpkg")
-                .WithSubscription(PcsTopicConstants.McPkg, "preservation_mcpkg")
-                .WithSubscription(PcsTopicConstants.Responsible, "preservation_responsible")
-                .WithSubscription(PcsTopicConstants.Certificate, "preservation_certificate")
-                //THIS METHOD SHOULD BE FALSE IN NORMAL OPERATION.
-                //ONLY SET TO TRUE WHEN A LARGE NUMBER OF MESSAGES HAVE FAILED AND ARE COPIED TO DEAD LETTER.
-                //WHEN SET TO TRUE, MESSAGES ARE READ FROM DEAD LETTER QUEUE INSTEAD OF NORMAL QUEUE
-                .WithReadFromDeadLetterQueue(builder.Configuration.GetValue("ServiceBus:ReadFromDeadLetterQueue", defaultValue: false))); 
+            return;
         }
+        
+        // Env variable used in radix. Configuration is added for easier use locally
+        // Url will be validated during startup of service bus integration and give a
+        // Uri exception if invalid.
+        builder.Services.AddPcsServiceBusIntegration(options => options
+            .UseBusConnection(builder.GetConfig<string>("ConnectionStrings:ServiceBus"))
+            .WithLeaderElector(builder.GetLeaderElectorUri())
+            .WithRenewLeaseInterval(builder.GetConfig<int>("ServiceBus:LeaderElectorRenewLeaseInterval"))
+            .WithSubscription(PcsTopicConstants.Tag, "preservation_tag")
+            .WithSubscription(PcsTopicConstants.TagFunction, "preservation_tagfunction")
+            .WithSubscription(PcsTopicConstants.Project, "preservation_project")
+            .WithSubscription(PcsTopicConstants.CommPkg, "preservation_commpkg")
+            .WithSubscription(PcsTopicConstants.McPkg, "preservation_mcpkg")
+            .WithSubscription(PcsTopicConstants.Responsible, "preservation_responsible")
+            .WithSubscription(PcsTopicConstants.Certificate, "preservation_certificate")
+            //THIS METHOD SHOULD BE FALSE IN NORMAL OPERATION.
+            //ONLY SET TO TRUE WHEN A LARGE NUMBER OF MESSAGES HAVE FAILED AND ARE COPIED TO DEAD LETTER.
+            //WHEN SET TO TRUE, MESSAGES ARE READ FROM DEAD LETTER QUEUE INSTEAD OF NORMAL QUEUE
+            .WithReadFromDeadLetterQueue(builder.Configuration.GetValue("ServiceBus:ReadFromDeadLetterQueue", defaultValue: false)));
     }
     
     private static bool IsServiceBusEnabled(this WebApplicationBuilder builder)
