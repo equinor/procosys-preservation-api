2d962df530ea831c4e03c5decc2b321f89f94326
Richard Dungal <richard.dungal@bouvet.no>
2025-01-30 21:17:58 +0100

    Integration test fix

--- src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/TestFactory.cs
+++ src/tests/Equinor.ProCoSys.Preservation.WebApi.IntegrationTests/TestFactory.cs
@@ -209,25 +209,11 @@ namespace Equinor.ProCoSys.Preservation.WebApi.IntegrationTests
         private void EnsureTestDatabaseDeletedAtTeardown(IServiceCollection services)
             => _teardownList.Add(() =>
             {
-                using (var dbContext = DatabaseContext(services))
-                {
-                    dbContext.Database.EnsureDeleted();
-                }
+                using var sp = services.BuildServiceProvider();
+                using var dbContext = sp.GetRequiredService<PreservationContext>();
+                
+                dbContext.Database.EnsureDeleted();
             });
-
-        private PreservationContext DatabaseContext(IServiceCollection services)
-        {
-            services.AddDbContext<PreservationContext>(options 
-                => options.UseSqlServer(_connectionString, o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
-
-            var sp = services.BuildServiceProvider();
-            _disposables.Add(sp);
-
-            var spScope = sp.CreateScope();
-            _disposables.Add(spScope);
-
-            return spScope.ServiceProvider.GetRequiredService<PreservationContext>();
-        }
 
         private string GetTestDbConnectionString(string projectDir)
         {
