01e53adbd6577d7f64d16e32d1ea9e9ee9084e8f
Richard Thor Fridriksson Dungal <54837611+richard1927@users.noreply.github.com>
2025-02-25 11:25:33 +0100

    1113 application insights identity auth (#1013)
    Using identity credentials to authenticate application insights connection

---
+++ src/Equinor.ProCoSys.Preservation.WebApi/DiModules/TelemetryConfig.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Core;
+using Equinor.ProCoSys.Common.Telemetry;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Equinor.ProCoSys.Preservation.WebApi.DiModules;
+
+public static class TelemetryConfig
+{
+    public static void ConfigureTelemetry(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        TokenCredential credential)
+    {
+        services.AddScoped<ITelemetryClient, ApplicationInsightsTelemetryClient>();
+        
+        services.AddApplicationInsightsTelemetry(options =>
+        {
+            options.ConnectionString = configuration.GetValue<string>("ApplicationInsights:ConnectionString");
+        });
+        
+        services.Configure<TelemetryConfiguration>(config =>
+        {
+            config.SetAzureTokenCredential(credential);
+        });
+    }
+}
