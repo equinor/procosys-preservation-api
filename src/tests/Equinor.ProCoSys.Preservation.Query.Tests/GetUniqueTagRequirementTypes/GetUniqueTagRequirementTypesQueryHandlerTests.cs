2488a41d72d3294e6c46614561d3105cac9ec4ed
Jan Inge Dalsbø <JDALSB@equinor.com>
2025-03-06 12:58:29 +0100

    Extra for 1210: Also exclude voided Tag Requirements from available Requirement types used in filter

--- src/tests/Equinor.ProCoSys.Preservation.Query.Tests/GetUniqueTagRequirementTypes/GetUniqueTagRequirementTypesQueryHandlerTests.cs
+++ src/tests/Equinor.ProCoSys.Preservation.Query.Tests/GetUniqueTagRequirementTypes/GetUniqueTagRequirementTypesQueryHandlerTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.ProjectAggregate;
+using Equinor.ProCoSys.Preservation.Domain.AggregateModels.RequirementTypeAggregate;
 using Equinor.ProCoSys.Preservation.Infrastructure;
 using Equinor.ProCoSys.Preservation.Query.GetUniqueTagRequirementTypes;
 using Equinor.ProCoSys.Preservation.Test.Common;
@@ -40,12 +42,21 @@ namespace Equinor.ProCoSys.Preservation.Query.Tests.GetUniqueTagRequirementTypes
         [TestMethod]
         public async Task HandleGetUniqueTagRequirementTypesQuery_ShouldReturnCorrectUniqueRequirementTypes()
         {
+            var typeCode = "XC";
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var newReqType = AddRequirementTypeWith1DefWithoutField(context, typeCode, "XT", RequirementTypeIcon.Other);
+                var tag = context.Tags.Include(t => t.Requirements).Single(t => t.Id == _testDataSet.Project1.Tags.First().Id);
+                tag.AddRequirement(new TagRequirement(_plantProvider.Plant, 1, newReqType.RequirementDefinitions.Single()));
+                context.SaveChangesAsync().Wait();
+            }
             using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
             {
                 var dut = new GetUniqueTagRequirementTypesQueryHandler(context);
                 
                 var result = await dut.Handle(_queryForProject1, default);
-                Assert.AreEqual(2, result.Data.Count);
+                Assert.AreEqual(3, result.Data.Count);
+                Assert.IsTrue(result.Data.Any(rt => rt.Code == typeCode));
                 Assert.IsTrue(result.Data.Any(rt => rt.Code == _testDataSet.ReqType1.Code));
                 Assert.IsTrue(result.Data.Any(rt => rt.Icon == _testDataSet.ReqType1.Icon));
                 Assert.IsTrue(result.Data.Any(rt => rt.Code == _testDataSet.ReqType2.Code));
@@ -55,6 +66,34 @@ namespace Equinor.ProCoSys.Preservation.Query.Tests.GetUniqueTagRequirementTypes
                 Assert.AreEqual(1, result.Data.Count);
                 Assert.IsTrue(result.Data.Any(rt => rt.Code == _testDataSet.ReqType1.Code));
                 Assert.IsTrue(result.Data.Any(rt => rt.Icon == _testDataSet.ReqType1.Icon));
+            }
+        }
+
+        [TestMethod]
+        public async Task HandleGetUniqueTagRequirementTypesQuery_ShouldNotRequirementTypes_ForVoidedTagRequirements()
+        {
+            var typeCode = "XC";
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var newReqType = AddRequirementTypeWith1DefWithoutField(context, typeCode, "XT", RequirementTypeIcon.Other);
+                var tag = context.Tags.Include(t => t.Requirements).Single(t => t.Id == _testDataSet.Project1.Tags.First().Id);
+                var tagRequirement =
+                    new TagRequirement(_plantProvider.Plant, 1, newReqType.RequirementDefinitions.Single())
+                    {
+                        IsVoided = true
+                    };
+                tag.AddRequirement(tagRequirement);
+                context.SaveChangesAsync().Wait();
+            }
+            using (var context = new PreservationContext(_dbContextOptions, _plantProvider, _eventDispatcher, _currentUserProvider))
+            {
+                var dut = new GetUniqueTagRequirementTypesQueryHandler(context);
+
+                var result = await dut.Handle(_queryForProject1, default);
+                Assert.AreEqual(2, result.Data.Count);
+                Assert.IsFalse(result.Data.Any(rt => rt.Code == typeCode));
+                Assert.IsTrue(result.Data.Any(rt => rt.Code == _testDataSet.ReqType1.Code));
+                Assert.IsTrue(result.Data.Any(rt => rt.Code == _testDataSet.ReqType2.Code));
             }
         }
 
